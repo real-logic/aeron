8f302cdbf856ad3a21d7f88f65fa4277c42b32c0
Todd L. Montgomery <todd.montgomery@kaazing.com>
2014-02-08 18:25:12 -0800

    added LICENSE. added initial maven setup. added gitignore.

---
+++ src/main/java/uk/co/real_logic/App.java
@@ -0,0 +1,13 @@
+package uk.co.real_logic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

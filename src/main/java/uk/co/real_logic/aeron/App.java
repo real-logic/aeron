d7bf0ff793cd46370efebd6cc33c52a8f8e51c53
Todd L. Montgomery <tmont@nard.net>
2014-02-19 21:26:18 -0800

    Added checkstyle to mvn build.

--- src/main/java/uk/co/real_logic/aeron/App.java
+++ src/main/java/uk/co/real_logic/aeron/App.java
@@ -4,10 +4,10 @@ package uk.co.real_logic.aeron;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
     }
 }
---
+++ src/main/java/uk/co/real_logic/aeron/util/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Created by tmont_000 on 2/19/14.
+ *
+ * Util package to hold ringbuffer and other bits.
+ */
+package uk.co.real_logic.aeron.util;

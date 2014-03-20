75c4b1b5e2468f523b76d2fc09f9691ef78f152f
Richard Warburton <richard.warburton@gmail.com>
2014-03-20 17:11:05 +0000

    add some provisional buffer exhaustion logic

---
+++ aeron-core/src/main/java/uk/co/real_logic/aeron/BufferExhaustionPredictor.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2014 Real Logic Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.real_logic.aeron;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Predicts when buffer exhaustion is going to happen for a given
+ * Term, so new term buffers can be mapped.
+ */
+public class BufferExhaustionPredictor
+{
+    // TODO: parameterize and adaptively tune this
+    private static final long minOkWindowToExhaustionInNanos = 100;
+
+    private static final int DATA_COLLECTION_WINDOW = 10;
+
+    private final long bufferSize;
+    // TODO: replace with a flyweighted, threadsafe, buffer
+    private final List<DataWriteRate> writeRates;
+
+    private long bufferConsumed;
+
+    private int writeCursor;
+
+    public BufferExhaustionPredictor(final long bufferSizeInBytes)
+    {
+        this.bufferSize = bufferSizeInBytes;
+
+        onBufferAllocated();
+        writeCursor = 0;
+        writeRates = IntStream.range(0, DATA_COLLECTION_WINDOW)
+                              .mapToObj(i -> new DataWriteRate())
+                              .collect(toList());
+    }
+
+    public void dataWritten(final long amountInBytes, final long nanoTime)
+    {
+        bufferConsumed += amountInBytes;
+
+        DataWriteRate writeRate = writeRates.get(writeCursor);
+        writeRate.nanoTime(nanoTime);
+        writeRate.amountInBytes(amountInBytes);
+        writeCursor = next(writeCursor);
+    }
+
+    public void onBufferAllocated() {
+        bufferConsumed = 0;
+    }
+
+    /**
+     * @return true if its time to request a new term buffer
+     */
+    public boolean predictExhaustion(final long currentTime)
+    {
+        long period = currentTime - startTime();
+
+        int readCursor = writeCursor;
+        long dataWriteTally = 0;
+        do
+        {
+            dataWriteTally += writeRates.get(readCursor).amountInBytes();
+
+            readCursor = next(readCursor);
+        }
+        while (readCursor != writeCursor);
+
+        double bytesWrittenPerNano = (double) dataWriteTally / period;
+        double remainingBytesInBuffer = bufferSize - bufferConsumed;
+        double nanosBeforeExhaustion = remainingBytesInBuffer / bytesWrittenPerNano;
+
+        return nanosBeforeExhaustion < minOkWindowToExhaustionInNanos;
+    }
+
+    private long startTime()
+    {
+        long time = writeRates.get(writeCursor).nanoTime();
+
+        if (time == 0)
+        {
+            return writeRates.get(0).nanoTime();
+        }
+
+        return time;
+    }
+
+    private int next(final int cursor)
+    {
+        return (cursor + 1) % DATA_COLLECTION_WINDOW;
+    }
+
+}
---
+++ aeron-core/src/test/java/uk/co/real_logic/aeron/BufferExhaustionPredictorTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2014 Real Logic Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.real_logic.aeron;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(Parameterized.class)
+public class BufferExhaustionPredictorTest
+{
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data()
+    {
+        Object[][] data = new Object[][]
+        {
+            { 10, new long[] {1, 1, 1}, false },
+            { 10, new long[] {30, 30, 30}, true },
+            { 100, new long[] {30, 10, 20}, true },
+        };
+        return Arrays.asList(data);
+    }
+
+    private final long period;
+    private final long[] amounts;
+    private final boolean result;
+
+    public BufferExhaustionPredictorTest(final long period, final long[] amounts, final boolean result)
+    {
+        this.period = period;
+        this.amounts = amounts;
+        this.result = result;
+    }
+
+    @Test
+    public void canPredictExhaustion()
+    {
+        BufferExhaustionPredictor predictor = new BufferExhaustionPredictor(100);
+        long currentTime = System.nanoTime();
+        long startTime = currentTime - period;
+        for (int i = 0; i < amounts.length; i++)
+        {
+            predictor.dataWritten(amounts[i], startTime + i);
+        }
+        assertThat(predictor.predictExhaustion(currentTime), is(result));
+    }
+
+}

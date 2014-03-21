660e884069c64fa05d8fe22a69e89c388440aea4
Richard Warburton <richard.warburton@gmail.com>
2014-03-21 11:34:21 +0000

    get AtomicBufferTest under 120 char line length

--- aeron-util/src/test/java/uk/co/real_logic/aeron/util/concurrent/AtomicBufferTest.java
+++ aeron-util/src/test/java/uk/co/real_logic/aeron/util/concurrent/AtomicBufferTest.java
@@ -521,7 +521,8 @@ public class AtomicBufferTest
         duplicateBuffer.position(INDEX);
         duplicateBuffer.put(testBytes);
 
-        final ByteBuffer dstBuffer = ((ByteBuffer)ByteBuffer.allocate(testBytes.length * 2).position(testBytes.length)).slice();
+        final ByteBuffer dstBuffer = ((ByteBuffer)ByteBuffer.allocate(testBytes.length * 2).position(testBytes.length))
+                                                            .slice();
 
         buffer.getBytes(INDEX, dstBuffer, testBytes.length);
 

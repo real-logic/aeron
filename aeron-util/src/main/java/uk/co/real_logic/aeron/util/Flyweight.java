8fe5fce2bf049395f524f58af0ae218d1554153e
Martin Thompson <mjpt777@gmail.com>
2014-03-31 16:17:53 +0100

    [Java:] Code style for consistency.

--- aeron-util/src/main/java/uk/co/real_logic/aeron/util/Flyweight.java
+++ aeron-util/src/main/java/uk/co/real_logic/aeron/util/Flyweight.java
@@ -51,6 +51,7 @@ public class Flyweight
     {
         this.atomicBuffer = buffer;
         this.offset = offset;
+
         return this;
     }
 
@@ -66,7 +67,7 @@ public class Flyweight
 
     protected int uint16Get(final int offset, final ByteOrder byteOrder)
     {
-        return (atomicBuffer.getShort(offset, byteOrder) & 0xFFFF);
+        return atomicBuffer.getShort(offset, byteOrder) & 0xFFFF;
     }
 
     protected void uint16Put(final int offset,
@@ -78,7 +79,7 @@ public class Flyweight
 
     protected long uint32Get(final int offset, final ByteOrder byteOrder)
     {
-        return (atomicBuffer.getInt(offset, byteOrder) & 0xFFFFFFFFL);
+        return atomicBuffer.getInt(offset, byteOrder) & 0xFFFFFFFFL;
     }
 
     protected void uint32Put(final int offset,
@@ -93,11 +94,13 @@ public class Flyweight
         final int length = atomicBuffer.getInt(offset);
         final long[] data = new long[length];
         int location = offset + SIZE_OF_INT;
+
         for (int i = 0; i < length; i++)
         {
             data[i] = uint32Get(location, byteOrder);
             location += SIZE_OF_LONG;
         }
+
         return data;
     }
 
@@ -105,32 +108,35 @@ public class Flyweight
                                  final long[] value,
                                  final ByteOrder byteOrder)
     {
-        int length = value.length;
+        final int length = value.length;
         atomicBuffer.putInt(offset, length, byteOrder);
         int location = offset + SIZE_OF_INT;
-        for (int i = 0; i < length; i++)
+
+        for (final long aValue : value)
         {
-            uint32Put(location, value[i], byteOrder);
+            uint32Put(location, aValue, byteOrder);
             location += SIZE_OF_LONG;
         }
+
         return SIZE_OF_INT + (length * BitUtil.SIZE_OF_LONG);
     }
 
     // TODO: consider efficiency for String encoding/decoding
     // TODO: is there a sensible error handling for getBytes/putBytes not reading/writing the current amount of data
-    public String stringGet(final int offset, ByteOrder byteOrder)
+    public String stringGet(final int offset, final ByteOrder byteOrder)
     {
-        int length = atomicBuffer.getInt(offset, byteOrder);
-        byte[] stringInBytes = new byte[length];
+        final int length = atomicBuffer.getInt(offset, byteOrder);
+        final byte[] stringInBytes = new byte[length];
         atomicBuffer.getBytes(offset + SIZE_OF_INT, stringInBytes);
+
         return new String(stringInBytes, StandardCharsets.UTF_8);
     }
 
-    public int stringPut(final int offset, String value, ByteOrder byteOrder)
+    public int stringPut(final int offset, final String value, final ByteOrder byteOrder)
     {
-        byte[] bytes = value.getBytes(StandardCharsets.UTF_8);
-        atomicBuffer.putInt(offset, bytes.length);
+        final byte[] bytes = value.getBytes(StandardCharsets.UTF_8);
+        atomicBuffer.putInt(offset, bytes.length, byteOrder);
+
         return SIZE_OF_INT + atomicBuffer.putBytes(offset + SIZE_OF_INT, bytes);
     }
-
 }

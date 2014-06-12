9ebc71b95efa9f07d3f2227e3b35c0329370663a
Todd L. Montgomery <todd.montgomery@kaazing.com>
2014-06-11 21:04:23 -0400

    [Java]: flush out Publish test case.

--- aeron-system-tests/src/test/java/uk/co/real_logic/aeron/PubUnicastTest.java
+++ aeron-system-tests/src/test/java/uk/co/real_logic/aeron/PubUnicastTest.java
@@ -20,12 +20,18 @@ import uk.co.real_logic.aeron.mediadriver.MediaDriver;
 import uk.co.real_logic.aeron.util.ConductorShmBuffers;
 import uk.co.real_logic.aeron.util.concurrent.AtomicBuffer;
 import uk.co.real_logic.aeron.util.protocol.DataHeaderFlyweight;
+import uk.co.real_logic.aeron.util.protocol.HeaderFlyweight;
 import uk.co.real_logic.aeron.util.protocol.StatusMessageFlyweight;
 
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertTrue;
 import static uk.co.real_logic.aeron.util.CommonConfiguration.ADMIN_DIR_NAME;
 import static uk.co.real_logic.aeron.util.CommonConfiguration.DIRS_DELETE_ON_EXIT_PROP_NAME;
@@ -37,12 +43,16 @@ public class PubUnicastTest
 {
     private static final String HOST = "localhost";
     private static final int PORT = 54321;
-    private static final Destination DESTINATION = new Destination("udp://" + HOST + ":" + PORT);
+    private static final int SRC_PORT = 54322;
+    private static final Destination DESTINATION = new Destination("udp://" + HOST + ":" + SRC_PORT +
+                                                                        "@" + HOST + ":" + PORT);
     private static final long CHANNEL_ID = 1L;
     private static final long SESSION_ID = 2L;
     private static final byte[] PAYLOAD = "Payload goes here!".getBytes();
 
     private final AtomicBuffer payload = new AtomicBuffer(ByteBuffer.allocate(PAYLOAD.length));
+
+    private final InetSocketAddress srcAddr = new InetSocketAddress(HOST, SRC_PORT);
 
     private Aeron producingClient;
     private MediaDriver driver;
@@ -53,6 +63,8 @@ public class PubUnicastTest
 
     private final DataHeaderFlyweight dataHeader = new DataHeaderFlyweight();
     private final StatusMessageFlyweight statusMessage = new StatusMessageFlyweight();
+
+    private ExecutorService executorService;
 
     @Before
     public void setupClientAndMediaDriver() throws Exception
@@ -73,6 +85,13 @@ public class PubUnicastTest
         channel = source.newChannel(CHANNEL_ID);
 
         payload.putBytes(0, PAYLOAD);
+
+        executorService = Executors.newFixedThreadPool(4);
+
+        executorService.execute(driver.conductor());
+        executorService.execute(driver.sender());
+        executorService.execute(driver.receiver());
+        executorService.execute(producingClient.conductor());
     }
 
     private Aeron.Context newAeronContext()
@@ -88,54 +107,77 @@ public class PubUnicastTest
         source.close();
         driver.close();
         driver.conductor().nioSelector().selectNowWithNoProcessing();
-    }
-
-    private void process(final int times) throws Exception
-    {
-        for (int i = 0; i < times; i++)
-        {
-            process();
-        }
-    }
-
-    private void process() throws Exception
-    {
-        producingClient.conductor().process();
-        driver.conductor().process();
-        driver.sender().process();
-        driver.receiver().process();
+        executorService.shutdown();
     }
 
     @Test
-    public void shouldSpinUpClientAndMediaDriver() throws Exception
-    {
-        process(3);
-    }
-
-    // TODO: get rid of these tests. Think they are redundant with good tests for media driver and client
-    // instead rely on PubAndSubUnicastTest for systems integration once we have broadcast buffer in
-
-    @Test
-    @Ignore("isn't finished yet")
-    public void shouldSendCorrectDataFrames() throws Exception
+    public void shouldSendCorrectlyFormedDataFrames() throws Exception
     {
         // let buffers get connected
-        process(1);
 
-        assertTrue(channel.offer(payload, 0, PAYLOAD.length));
+        // this will not be sent yet
+        while (!channel.offer(payload, 0, PAYLOAD.length))
+        {
+            Thread.sleep(10);
+        }
 
-        process(1);
-        // TODO: process to get buffers setup from setUp()
-        // TODO: assert receiving a correct 0 length data frame on senderChannel
-        // TODO: send SM to get client to send
-        // TODO: assert the received Data Frames are correctly formed
+        // sleep so we are sure some 0 length data has been sent
+        Thread.sleep(500);
+
+        final ByteBuffer rcvBuffer = ByteBuffer.allocate(DataHeaderFlyweight.HEADER_LENGTH + PAYLOAD.length);
+        rcvBuffer.clear();
+        final AtomicBuffer aBuffer = new AtomicBuffer(rcvBuffer);
+        long termId = 0;
+        int rcvedZeroLengthData = 0, rcvedDataFrames = 0;
+
+        // should only see 0 length data until SM is sent
+        InetSocketAddress addr;
+        while((addr = (InetSocketAddress)receiverChannel.receive(rcvBuffer)) != null)
+        {
+            dataHeader.wrap(aBuffer, 0);
+            assertThat(dataHeader.headerType(), is(HeaderFlyweight.HDR_TYPE_DATA));
+            assertThat(dataHeader.frameLength(), is(DataHeaderFlyweight.HEADER_LENGTH));
+            assertThat(dataHeader.channelId(), is(CHANNEL_ID));
+            assertThat(dataHeader.sessionId(), is(SESSION_ID));
+            assertThat(rcvBuffer.position(), is(DataHeaderFlyweight.HEADER_LENGTH));
+            termId = dataHeader.termId();
+            assertThat(addr, is(srcAddr));
+            rcvedZeroLengthData++;
+            rcvedDataFrames++;
+            rcvBuffer.clear();
+        }
+
+        assertThat(rcvedZeroLengthData, greaterThanOrEqualTo(1));
+
+        // send SM
+        sendSM(termId);
+
+        // sleep to make sure that the sender thread in the media driver has a chance to send data
+        Thread.sleep(100);
+
+        // assert the received Data Frames are correctly formed
+        rcvBuffer.clear();
+        while((addr = (InetSocketAddress)receiverChannel.receive(rcvBuffer)) != null)
+        {
+            dataHeader.wrap(aBuffer, 0);
+            assertThat(dataHeader.headerType(), is(HeaderFlyweight.HDR_TYPE_DATA));
+            assertThat(dataHeader.frameLength(), is(DataHeaderFlyweight.HEADER_LENGTH + PAYLOAD.length));
+            assertThat(dataHeader.channelId(), is(CHANNEL_ID));
+            assertThat(dataHeader.sessionId(), is(SESSION_ID));
+            assertThat(rcvBuffer.position(), is(DataHeaderFlyweight.HEADER_LENGTH + PAYLOAD.length));
+            assertThat(dataHeader.termId(), is(termId));
+            assertThat(addr, is(srcAddr));
+            rcvedDataFrames++;
+            rcvBuffer.clear();
+        }
+
+        assertThat(rcvedDataFrames - rcvedZeroLengthData, greaterThanOrEqualTo(1));
     }
 
     @Test
     @Ignore("isn't finished yet")
     public void publisherMessagesSubscriber() throws Exception
     {
-        process(5);
         // TODO: simple message send/read
     }
 
@@ -146,4 +188,25 @@ public class PubUnicastTest
         // TODO: send enough data to rollover a buffer
     }
 
+    private void sendSM(final long termId) throws Exception
+    {
+        final ByteBuffer smBuffer = ByteBuffer.allocate(StatusMessageFlyweight.HEADER_LENGTH);
+        statusMessage.wrap(new AtomicBuffer(smBuffer), 0);
+
+        statusMessage.receiverWindow(1000)
+            .highestContiguousTermOffset(0)
+            .termId(termId)
+            .channelId(CHANNEL_ID)
+            .sessionId(SESSION_ID)
+            .frameLength(StatusMessageFlyweight.HEADER_LENGTH)
+            .headerType(HeaderFlyweight.HDR_TYPE_SM)
+            .flags((short)0)
+            .version(HeaderFlyweight.CURRENT_VERSION);
+
+        smBuffer.position(0);
+        smBuffer.limit(StatusMessageFlyweight.HEADER_LENGTH);
+        final int byteSent = receiverChannel.send(smBuffer, srcAddr);
+
+        assertThat(byteSent, is(StatusMessageFlyweight.HEADER_LENGTH));
+    }
 }

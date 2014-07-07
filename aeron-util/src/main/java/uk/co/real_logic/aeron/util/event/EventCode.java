23df5d7bcf1d5f381f9d7fbd375327195c49b866
Richard Warburton <richard.warburton@gmail.com>
2014-07-07 18:14:09 +0100

    remove System.err prints from Receiver

--- aeron-util/src/main/java/uk/co/real_logic/aeron/util/event/EventCode.java
+++ aeron-util/src/main/java/uk/co/real_logic/aeron/util/event/EventCode.java
@@ -37,7 +37,8 @@ public enum EventCode
     UNKNOWN_HEADER_TYPE(12, EventCodec::dissectAsCommand),
 
     /** Probably means MULTICAST_DEFAULT_INTERFACE_PROP_NAME wasn't set properly */
-    COULD_NOT_FIND_INTERFACE(13, EventCodec::dissectAsString);
+    COULD_NOT_FIND_INTERFACE(13, EventCodec::dissectAsString),
+    COULD_NOT_FIND_FRAME_HANDLER_FOR_NEW_CONNECTED_SUBSCRIPTION(14, EventCodec::dissectAsString);
 
     private final static Int2ObjectHashMap<EventCode> mapOfIdToEventCode = new Int2ObjectHashMap<>();
 

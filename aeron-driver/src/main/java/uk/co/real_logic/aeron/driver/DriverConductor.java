5ee40a79ea3e0d42a5f3fbd8a06f9a2eb4ff50fc
Todd L. Montgomery <todd.montgomery@kaazing.com>
2014-07-29 16:15:11 -0700

    [Java]: remember to call close on connection when timing it out.

--- aeron-driver/src/main/java/uk/co/real_logic/aeron/driver/DriverConductor.java
+++ aeron-driver/src/main/java/uk/co/real_logic/aeron/driver/DriverConductor.java
@@ -684,6 +684,7 @@ public class DriverConductor extends Agent
                     }
 
                     connections.remove(connection);
+                    connection.close();
                 }
             }
         );

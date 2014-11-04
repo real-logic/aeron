ae4d2a60e9cf4dd80658202b73dfc405f065538d
Todd L. Montgomery <todd.montgomery@kaazing.com>
2014-11-04 09:47:52 -0800

    [Java]: update aeron-samples copyrights

--- aeron-samples/src/main/java/uk/co/real_logic/aeron/samples/raw/SendSelectReceiveUdpPing.java
+++ aeron-samples/src/main/java/uk/co/real_logic/aeron/samples/raw/SendSelectReceiveUdpPing.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package uk.co.real_logic.aeron.samples.raw;
 
 import org.HdrHistogram.Histogram;

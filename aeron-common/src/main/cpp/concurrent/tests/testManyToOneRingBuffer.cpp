9e1706986fdbbea8ceab0e7367e72579882230aa
Todd L. Montgomery <todd.montgomery@kaazing.com>
2014-11-12 13:34:57 -0800

    [C++]: some more tests for ManyToOneRingBuffer operational checks

--- aeron-common/src/main/cpp/concurrent/tests/testManyToOneRingBuffer.cpp
+++ aeron-common/src/main/cpp/concurrent/tests/testManyToOneRingBuffer.cpp
@@ -25,20 +25,27 @@
 
 using namespace aeron::common::concurrent::ringbuffer;
 using namespace aeron::common::concurrent;
+using namespace aeron::common;
 
-#define BUFFER_SZ (1024 + RingBufferDescriptor::TRAILER_LENGTH)
-#define ODD_BUFFER_SZ (1023 + RingBufferDescriptor::TRAILER_LENGTH)
+#define CAPACITY (1024)
+#define BUFFER_SZ (CAPACITY + RingBufferDescriptor::TRAILER_LENGTH)
+#define ODD_BUFFER_SZ ((CAPACITY - 1) + RingBufferDescriptor::TRAILER_LENGTH)
 
 typedef std::array<std::uint8_t, BUFFER_SZ> buffer_t;
 typedef std::array<std::uint8_t, ODD_BUFFER_SZ> odd_sized_buffer_t;
 
+constexpr static std::int32_t MSG_TYPE_ID = 101;
+constexpr static util::index_t HEAD_COUNTER_INDEX = 1024 + RingBufferDescriptor::HEAD_COUNTER_OFFSET;
+constexpr static util::index_t TAIL_COUNTER_INDEX = 1024 + RingBufferDescriptor::TAIL_COUNTER_OFFSET;
+
 class ManyToOneRingBufferTest : public testing::Test
 {
 public:
-    static const std::int32_t MSG_TYPE_ID = 101;
 
-    ManyToOneRingBufferTest()
-        : m_ab(&m_buffer[0], m_buffer.size()), m_ringBuffer(m_ab), m_srcAb(&m_srcBuffer[0], m_srcBuffer.size())
+    ManyToOneRingBufferTest() :
+        m_ab(&m_buffer[0],m_buffer.size()),
+        m_srcAb(&m_srcBuffer[0], m_srcBuffer.size()),
+        m_ringBuffer(m_ab)
     {
         clear();
     }
@@ -59,8 +66,8 @@ protected:
 
 TEST_F(ManyToOneRingBufferTest, shouldCalculateCapacityForBuffer)
 {
-    ASSERT_EQ(m_ab.getCapacity(), BUFFER_SZ);
-    ASSERT_EQ(m_ringBuffer.capacity(), BUFFER_SZ - RingBufferDescriptor::TRAILER_LENGTH);
+    EXPECT_EQ(m_ab.getCapacity(), BUFFER_SZ);
+    EXPECT_EQ(m_ringBuffer.capacity(), BUFFER_SZ - RingBufferDescriptor::TRAILER_LENGTH);
 }
 
 TEST_F(ManyToOneRingBufferTest, shouldThrowForCapacityNotPowerOfTwo)
@@ -73,7 +80,7 @@ TEST_F(ManyToOneRingBufferTest, shouldThrowForCapacityNotPowerOfTwo)
     ASSERT_THROW(
     {
         ManyToOneRingBuffer ringBuffer (ab);
-    }, aeron::common::util::IllegalArgumentException);
+    }, util::IllegalArgumentException);
 }
 
 TEST_F(ManyToOneRingBufferTest, shouldThrowWhenMaxMessageSizeExceeded)
@@ -81,5 +88,51 @@ TEST_F(ManyToOneRingBufferTest, shouldThrowWhenMaxMessageSizeExceeded)
     ASSERT_THROW(
     {
         m_ringBuffer.write(MSG_TYPE_ID, m_srcAb, 0, m_ringBuffer.maxMsgLength() + 1);
-    }, aeron::common::util::IllegalArgumentException);
+    }, util::IllegalArgumentException);
+}
+
+TEST_F(ManyToOneRingBufferTest, shouldWriteToEmptyBuffer)
+{
+    util::index_t tail = 0;
+    util::index_t tailIndex = 0;
+    util::index_t length = 8;
+    util::index_t srcIndex = 0;
+    util::index_t recordLength = util::BitUtil::align(length + RecordDescriptor::HEADER_LENGTH, RecordDescriptor::ALIGNMENT);
+
+    ASSERT_TRUE(m_ringBuffer.write(MSG_TYPE_ID, m_srcAb, srcIndex, length));
+
+    EXPECT_EQ(m_ab.getInt32(RecordDescriptor::lengthOffset(tailIndex)), recordLength);
+    EXPECT_EQ(m_ab.getInt32(RecordDescriptor::msgLengthOffset(tailIndex)), length);
+    EXPECT_EQ(m_ab.getInt32(RecordDescriptor::msgTypeOffset(tailIndex)), MSG_TYPE_ID);
+    EXPECT_EQ(m_ab.getInt64(TAIL_COUNTER_INDEX), tail + recordLength);
+}
+
+TEST_F(ManyToOneRingBufferTest, shouldRejectWriteWhenInsufficientSpace)
+{
+    util::index_t length = 100;
+    util::index_t head = 0;
+    util::index_t tail = head + (CAPACITY - util::BitUtil::align(length - RecordDescriptor::ALIGNMENT, RecordDescriptor::ALIGNMENT));
+    util::index_t srcIndex = 0;
+
+    m_ab.putInt64(HEAD_COUNTER_INDEX, head);
+    m_ab.putInt64(TAIL_COUNTER_INDEX, tail);
+
+    ASSERT_FALSE(m_ringBuffer.write(MSG_TYPE_ID, m_srcAb, srcIndex, length));
+
+    EXPECT_EQ(m_ab.getInt64(TAIL_COUNTER_INDEX), tail);
+}
+
+TEST_F(ManyToOneRingBufferTest, shouldRejectWriteWhenBufferFull)
+{
+    util::index_t length = 8;
+    util::index_t head = 0;
+    util::index_t tail = head + CAPACITY;
+    util::index_t srcIndex = 0;
+
+    m_ab.putInt64(HEAD_COUNTER_INDEX, head);
+    m_ab.putInt64(TAIL_COUNTER_INDEX, tail);
+
+    ASSERT_FALSE(m_ringBuffer.write(MSG_TYPE_ID, m_srcAb, srcIndex, length));
+
+    EXPECT_EQ(m_ab.getInt64(TAIL_COUNTER_INDEX), tail);
 }

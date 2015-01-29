32b38f6d6cebe5119d8f43376c4f4303635e61b2
phaynes <phaynes@threatmetrix.com>
2015-01-29 20:40:53 +1100

    Removed build cruft

--- cppbuild/aeron-client/src/main/cpp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/phaynes/devel/messaging/fork/Aeron/aeron-client/src/main/cpp
-# Build directory: /Users/phaynes/devel/messaging/fork/Aeron/cppbuild/aeron-client/src/main/cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

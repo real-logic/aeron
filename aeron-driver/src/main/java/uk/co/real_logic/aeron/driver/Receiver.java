1c2343623cce0d5a17fcd2d3fc9f2881c3259db2
Martin Thompson <mjpt777@gmail.com>
2015-03-07 13:29:04 +0000

    [Java] Code style.

--- aeron-driver/src/main/java/uk/co/real_logic/aeron/driver/Receiver.java
+++ aeron-driver/src/main/java/uk/co/real_logic/aeron/driver/Receiver.java
@@ -29,7 +29,7 @@ public class Receiver implements Agent
 {
     private final TransportPoller transportPoller;
     private final OneToOneConcurrentArrayQueue<ReceiverCmd> commandQueue;
-    private final Consumer<ReceiverCmd> onReceiverCmdFunc;
+    private final Consumer<ReceiverCmd> onReceiverCmdFunc = this::onReceiverCmd;
     private final AtomicCounter totalBytesReceived;
 
     public Receiver(final MediaDriver.Context ctx)
@@ -37,8 +37,6 @@ public class Receiver implements Agent
         this.transportPoller = ctx.receiverNioSelector();
         this.commandQueue = ctx.receiverCommandQueue();
         this.totalBytesReceived = ctx.systemCounters().bytesReceived();
-
-        onReceiverCmdFunc = this::onReceiverCmd;
     }
 
     public String roleName()

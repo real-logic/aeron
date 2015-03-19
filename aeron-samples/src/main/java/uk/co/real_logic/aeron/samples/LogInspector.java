d5d9c19ee1bceb1359151b66ccad4b93f99c310b
mjpt777 <mjpt777@gmail.com>
2015-03-19 11:04:42 +0000

    [Java] Update hex dump with upper case letters.

--- aeron-samples/src/main/java/uk/co/real_logic/aeron/samples/LogInspector.java
+++ aeron-samples/src/main/java/uk/co/real_logic/aeron/samples/LogInspector.java
@@ -113,7 +113,7 @@ public class LogInspector
         }
     }
 
-    private static final char[] HEX_ARRAY = "0123456789abcdef".toCharArray();
+    private static final char[] HEX_ARRAY = "0123456789ABCEDF".toCharArray();
 
     public static char[] bytesToHex(final DirectBuffer buffer, final int offset, final int length)
     {

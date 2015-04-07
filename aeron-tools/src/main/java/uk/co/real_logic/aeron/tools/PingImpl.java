0b17e83ae25f962752d6fe981e38e4b36b1cd2cd
philip <philip.johnson@kaazing.com>
2015-04-07 12:12:04 -0500

    Removed old ping/pong tools and updated build.gradle

--- aeron-tools/src/main/java/uk/co/real_logic/aeron/tools/PingImpl.java
+++
@@ -1,9 +0,0 @@
-package uk.co.real_logic.aeron.tools;
-
-public interface PingImpl
-{
-  void prepare();
-  void connect();
-  long sendPingAndReceivePong(int msgLen);
-  void shutdown();
-}
--- aeron-tools/src/main/java/uk/co/real_logic/aeron/tools/PongImpl.java
+++
@@ -1,8 +0,0 @@
-package uk.co.real_logic.aeron.tools;
-
-public interface PongImpl
-{
-  void prepare();
-  void run();
-  void shutdown();
-}

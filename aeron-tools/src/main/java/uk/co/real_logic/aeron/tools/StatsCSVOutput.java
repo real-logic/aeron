3a856fc22dff96fc1dd791589395747f4de140fe
philip <philip.johnson@kaazing.com>
2015-05-01 10:39:22 -0500

    Fixed formatting issues

--- aeron-tools/src/main/java/uk/co/real_logic/aeron/tools/StatsCSVOutput.java
+++ aeron-tools/src/main/java/uk/co/real_logic/aeron/tools/StatsCSVOutput.java
@@ -20,60 +20,58 @@ import java.io.FileWriter;
 
 public class StatsCSVOutput implements StatsOutput
 {
-  public static final String DEFAULT_FILE = "stats.csv";
+    public static final String DEFAULT_FILE = "stats.csv";
 
-  private String file;
-  private FileWriter out;
-  private boolean firstTime = true;
+    private String file;
+    private FileWriter out;
+    private boolean firstTime = true;
 
-  public StatsCSVOutput(final String file)
-  {
-    if (file != null)
+    public StatsCSVOutput(final String file)
     {
-      this.file = file;
-    }
-    else
-    {
-      this.file = DEFAULT_FILE;
-    }
+        if (file != null)
+        {
+            this.file = file;
+        }
+        else
+        {
+            this.file = DEFAULT_FILE;
+        }
 
-    try
-    {
-      System.out.println("Output file: " + this.file);
-      final File outFile = new File(this.file);
-      outFile.createNewFile();
-      out = new FileWriter(outFile);
-    }
-    catch (final Exception e)
-    {
-      e.printStackTrace();
-    }
-  }
-
-  @Override
-public void format(final String[] keys, final long[] vals) throws Exception
-  {
-    if (firstTime)
-    {
-      for (int i = 0; i < keys.length - 1; i++)
-      {
-        out.write(keys[i] + ",");
-      }
-      out.write(keys[keys.length - 1] + "\n");
-      firstTime = false;
+        try
+        {
+            System.out.println("Output file: " + this.file);
+            final File outFile = new File(this.file);
+            outFile.createNewFile();
+            out = new FileWriter(outFile);
+        }
+        catch (final Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
-    for (int i = 0; i < vals.length - 1; i++)
+    public void format(final String[] keys, final long[] vals) throws Exception
     {
-      out.write(vals[i] + ",");
-    }
-    out.write(vals[vals.length - 1] + "\n");
-    out.flush();
-  }
+        if (firstTime)
+        {
+            for (int i = 0; i < keys.length - 1; i++)
+            {
+                out.write(keys[i] + ",");
+            }
+            out.write(keys[keys.length - 1] + "\n");
+            firstTime = false;
+        }
 
-  @Override
-public void close() throws Exception
-  {
-    out.close();
-  }
+        for (int i = 0; i < vals.length - 1; i++)
+        {
+            out.write(vals[i] + ",");
+        }
+        out.write(vals[vals.length - 1] + "\n");
+        out.flush();
+    }
+
+    public void close() throws Exception
+    {
+        out.close();
+    }
 }

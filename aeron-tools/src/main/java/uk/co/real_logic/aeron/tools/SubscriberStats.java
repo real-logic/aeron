3b789eacef094df77b5e1f1a54213dc0165d0288
philip <philip.johnson@kaazing.com>
2015-05-01 10:44:19 -0500

    Fixed formatting issues

--- aeron-tools/src/main/java/uk/co/real_logic/aeron/tools/SubscriberStats.java
+++ aeron-tools/src/main/java/uk/co/real_logic/aeron/tools/SubscriberStats.java
@@ -17,30 +17,29 @@ package uk.co.real_logic.aeron.tools;
 
 public class SubscriberStats extends TransportStats
 {
-  private long hwm;
+    private long hwm;
 
-  public SubscriberStats(final String channel)
-  {
-    parseChannel(channel);
-    active = true;
-  }
-
-  public void setHWM(final long hwm)
-  {
-    if (hwm != this.hwm)
+    public SubscriberStats(final String channel)
     {
-      this.hwm = hwm;
-      active = true;
+        parseChannel(channel);
+        active = true;
     }
-  }
 
-  @Override
-public String toString()
-  {
-    final String s = String.format("%1$5s %2$8d %3$8d %4$10s:%5$5d %6$s%7$s %8$8s\n",
-        proto, pos, hwm, host, port, "0x", sessionId, active ? "ACTIVE" : "INACTIVE");
+    public void setHWM(final long hwm)
+    {
+        if (hwm != this.hwm)
+        {
+            this.hwm = hwm;
+            active = true;
+        }
+    }
 
-    active = false;
-    return s;
-  }
+    public String toString()
+    {
+        final String s = String.format("%1$5s %2$8d %3$8d %4$10s:%5$5d %6$s%7$s %8$8s\n",
+                proto, pos, hwm, host, port, "0x", sessionId, active ? "ACTIVE" : "INACTIVE");
+
+        active = false;
+        return s;
+    }
 }

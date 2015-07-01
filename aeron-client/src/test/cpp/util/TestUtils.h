071cb7acd2fb9612cee437be84648c2f8932ff3f
Todd L. Montgomery <tmont@nard.net>
2015-06-30 18:46:02 -0700

    [C++]: clean up warnings about use of tempnam

---
+++ aeron-client/src/test/cpp/util/TestUtils.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2015 Real Logic Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef AERON_TESTUTILS_H
+#define AERON_TESTUTILS_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <util/Exceptions.h>
+
+namespace aeron { namespace test {
+
+std::string makeTempFileName()
+{
+#if !defined(_MSC_VER)
+    char rawname[] = "/tmp/aeron-c.XXXXXXX";
+    int fd = ::mkstemp(rawname);
+    ::close(fd);
+    ::unlink(rawname);
+
+    return std::string(rawname);
+
+#else
+    char tmpdir[MAX_PATH+1];
+    char tmpfile[MAX_PATH];
+
+    if (::GetTempPath(MAX_PATH, &tmpdir[0]) > 0)
+    {
+        if (::GetTempFileName(buff, TEXT("aeron-c"), 0, &tmpfile[0]) != 0)
+        {
+            return std::string(tmpfile);
+        }
+    }
+
+    throw util::IllegalStateException("could not make unique temp filename", SOURCEINFO);
+#endif
+}
+
+}}
+
+#endif //AERON_TESTUTILS_H

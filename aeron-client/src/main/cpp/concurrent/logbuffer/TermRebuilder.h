1029d612a6c1e3e54f5fa0b10d93fc92bce89f13
Michael Barker <mikeb01@gmail.com>
2015-09-23 22:39:43 +1200

    [C++]: Copyright headers and remove comments.

--- aeron-client/src/main/cpp/concurrent/logbuffer/TermRebuilder.h
+++ aeron-client/src/main/cpp/concurrent/logbuffer/TermRebuilder.h
@@ -1,6 +1,18 @@
-//
-// Created by Michael Barker on 21/09/15.
-//
+/*
+ * Copyright 2014 - 2015 Real Logic Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef INCLUDED_AERON_CONCURRENT_LOGBUFFER_TERMREBUILDER__
 #define INCLUDED_AERON_CONCURRENT_LOGBUFFER_TERMREBUILDER__
@@ -14,20 +26,11 @@ namespace TermRebuilder {
 
 inline void insert(AtomicBuffer& termBuffer, std::int32_t termOffset, AtomicBuffer& packet, std::int32_t length)
 {
-/*
-        final int firstFrameLength = packet.getInt(0, LITTLE_ENDIAN);
-        packet.putIntOrdered(0, 0);
-
-        termBuffer.putBytes(termOffset, packet, 0, length);
-        frameLengthOrdered(termBuffer, termOffset, firstFrameLength);
- */
     const std::int32_t firstFrameLength = packet.getInt32(0);
     packet.putInt32Ordered(0, 0);
 
     termBuffer.putBytes(termOffset, packet, 0, length);
     FrameDescriptor::frameLengthOrdered(termBuffer, termOffset, firstFrameLength);
-
-    return;
 }
 
 };

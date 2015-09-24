de3c1591d6096789efb8ea959ee2180b0e1a611d
Michael Barker <mikeb01@gmail.com>
2015-09-24 22:42:39 +1200

    [C++]: Implement TermGapScanner.

---
+++ aeron-client/src/main/cpp/concurrent/logbuffer/TermGapScanner.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2014 - 2015 Real Logic Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef INCLUDED_AERON_CONCURRENT_LOGBUFFER_TERMGAPSCANNER__
+#define INCLUDED_AERON_CONCURRENT_LOGBUFFER_TERMGAPSCANNER__
+
+#include <util/BitUtil.h>
+#include "FrameDescriptor.h"
+
+namespace aeron { namespace concurrent { namespace logbuffer {
+
+namespace TermGapScanner {
+
+inline std::int32_t scanForGap(
+    AtomicBuffer& termBuffer, std::int32_t termId, util::index_t rebuildOffset, std::int32_t hwmOffset,
+    std::function<void(std::int32_t, AtomicBuffer&, std::int32_t, std::int32_t)> handler)
+{
+    do
+    {
+        const std::int32_t frameLength = FrameDescriptor::frameLengthVolatile(termBuffer, rebuildOffset);
+        if (frameLength <= 0)
+        {
+            break;
+        }
+
+        rebuildOffset += util::BitUtil::align(frameLength, FrameDescriptor::FRAME_ALIGNMENT);
+    }
+    while (rebuildOffset < hwmOffset);
+
+    const std::int32_t gapBeginOffset = rebuildOffset;
+    if (rebuildOffset < hwmOffset)
+    {
+        const std::int32_t limit = hwmOffset - FrameDescriptor::HEADER_LENGTH;
+
+        while (rebuildOffset < limit)
+        {
+            rebuildOffset += FrameDescriptor::FRAME_ALIGNMENT;
+
+            if (0 != termBuffer.getInt32Volatile(rebuildOffset))
+            {
+                rebuildOffset -= FrameDescriptor::HEADER_LENGTH;
+                break;
+            }
+        }
+
+        const std::int32_t gapLength = (rebuildOffset - gapBeginOffset) + FrameDescriptor::HEADER_LENGTH;
+        handler(termId, termBuffer, gapBeginOffset, gapLength);
+    }
+
+    return gapBeginOffset;
+}
+
+};
+
+}}};
+
+#endif //INCLUDED_AERON_CONCURRENT_LOGBUFFER_TERMGAPSCANNER__
---
+++ aeron-client/src/test/cpp/concurrent/TermGapScannerTest.cpp
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2014 - 2015 Real Logic Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include <gtest/gtest.h>
+
+#include <thread>
+#include "MockAtomicBuffer.h"
+#include <concurrent/logbuffer/TermGapScanner.h>
+#include <concurrent/logbuffer/LogBufferDescriptor.h>
+
+using namespace aeron::concurrent::logbuffer;
+using namespace aeron::concurrent::mock;
+using namespace aeron::concurrent;
+using namespace aeron;
+
+#define LOG_BUFFER_CAPACITY (LogBufferDescriptor::TERM_MIN_LENGTH)
+#define TERM_ID 1
+
+typedef std::array<std::uint8_t, LOG_BUFFER_CAPACITY> log_buffer_t;
+
+class TermGapScannerTest : public testing::Test
+{
+public:
+    TermGapScannerTest() :
+        m_termBuffer(&m_logBuffer[0], m_logBuffer.size())
+    {
+        m_logBuffer.fill(0);
+    }
+
+    virtual void SetUp()
+    {
+        m_logBuffer.fill(0);
+    }
+
+protected:
+    AERON_DECL_ALIGNED(log_buffer_t m_logBuffer, 16);
+    MockAtomicBuffer m_termBuffer;
+};
+
+TEST_F(TermGapScannerTest, shouldReportGapAtBeginningOfBuffer)
+{
+    bool called = false;
+    const util::index_t frameOffset = FrameDescriptor::HEADER_LENGTH * 3;
+    const util::index_t highWaterMark = frameOffset + FrameDescriptor::HEADER_LENGTH;
+
+    EXPECT_CALL(m_termBuffer, getInt32Volatile(testing::_))
+        .WillRepeatedly(testing::Return(0));
+    EXPECT_CALL(m_termBuffer, getInt32Volatile(frameOffset))
+        .WillOnce(testing::Return(FrameDescriptor::HEADER_LENGTH));
+
+    auto f = [&] (std::int32_t termId, AtomicBuffer& buffer, std::int32_t offset, std::int32_t length)
+    {
+        EXPECT_EQ(TERM_ID, termId);
+        EXPECT_EQ(0, offset);
+        EXPECT_EQ(frameOffset, length);
+        called = true;
+    };
+
+    EXPECT_EQ(0, TermGapScanner::scanForGap(m_termBuffer, TERM_ID, 0, highWaterMark, f));
+
+    EXPECT_TRUE(called);
+}
+
+TEST_F(TermGapScannerTest, shouldReportSingleGapWhenBufferNotFull)
+{
+    bool called = false;
+    const std::int32_t tail = FrameDescriptor::HEADER_LENGTH;
+    const std::int32_t highWaterMark = FrameDescriptor::HEADER_LENGTH * 3;
+
+    EXPECT_CALL(m_termBuffer, getInt32Volatile(testing::_)).WillRepeatedly(testing::Return(0));
+    EXPECT_CALL(m_termBuffer, getInt32Volatile(tail - FrameDescriptor::HEADER_LENGTH))
+        .WillRepeatedly(testing::Return(FrameDescriptor::HEADER_LENGTH));
+    EXPECT_CALL(m_termBuffer, getInt32Volatile(tail))
+        .WillRepeatedly(testing::Return(0));
+    EXPECT_CALL(m_termBuffer, getInt32Volatile(highWaterMark - FrameDescriptor::HEADER_LENGTH))
+        .WillRepeatedly(testing::Return(FrameDescriptor::HEADER_LENGTH));
+
+    auto f = [&] (std::int32_t termId, AtomicBuffer& buffer, std::int32_t offset, std::int32_t length)
+    {
+        EXPECT_EQ(TERM_ID, termId);
+        EXPECT_EQ(tail, offset);
+        EXPECT_EQ(FrameDescriptor::HEADER_LENGTH, length);
+        called = true;
+    };
+
+    EXPECT_EQ(tail, TermGapScanner::scanForGap(m_termBuffer, TERM_ID, 0, highWaterMark, f));
+
+    EXPECT_TRUE(called);
+}
+
+TEST_F(TermGapScannerTest, shouldReportSingleGapWhenBufferIsFull)
+{
+    bool called = false;
+    const std::int32_t tail = LOG_BUFFER_CAPACITY - (FrameDescriptor::HEADER_LENGTH * 2);
+    const std::int32_t highWaterMark = LOG_BUFFER_CAPACITY;
+
+    EXPECT_CALL(m_termBuffer, getInt32Volatile(testing::_)).WillRepeatedly(testing::Return(0));
+
+    EXPECT_CALL(m_termBuffer, getInt32Volatile(tail - FrameDescriptor::HEADER_LENGTH))
+        .WillRepeatedly(testing::Return(FrameDescriptor::HEADER_LENGTH));
+    EXPECT_CALL(m_termBuffer, getInt32Volatile(tail))
+        .WillRepeatedly(testing::Return(0));
+    EXPECT_CALL(m_termBuffer, getInt32Volatile(highWaterMark - FrameDescriptor::HEADER_LENGTH))
+        .WillRepeatedly(testing::Return(FrameDescriptor::HEADER_LENGTH));
+
+    auto f = [&] (std::int32_t termId, AtomicBuffer& buffer, std::int32_t offset, std::int32_t length)
+    {
+        EXPECT_EQ(TERM_ID, termId);
+        EXPECT_EQ(tail, offset);
+        EXPECT_EQ(FrameDescriptor::HEADER_LENGTH, length);
+        called = true;
+    };
+
+    EXPECT_EQ(tail, TermGapScanner::scanForGap(m_termBuffer, TERM_ID, tail, highWaterMark, f));
+
+    EXPECT_TRUE(called);
+}
+
+TEST_F(TermGapScannerTest, shouldReportNoGapWhenHwmIsInPadding)
+{
+    bool called = false;
+    const std::int32_t paddingLength = FrameDescriptor::HEADER_LENGTH * 2;
+    const std::int32_t tail = LOG_BUFFER_CAPACITY - paddingLength;
+    const std::int32_t highWaterMark = LOG_BUFFER_CAPACITY - paddingLength + FrameDescriptor::HEADER_LENGTH;
+
+    EXPECT_CALL(m_termBuffer, getInt32Volatile(tail))
+        .WillRepeatedly(testing::Return(paddingLength));
+    EXPECT_CALL(m_termBuffer, getInt32Volatile(tail + FrameDescriptor::HEADER_LENGTH))
+        .WillRepeatedly(testing::Return(0));
+
+    auto f = [&] (std::int32_t termId, AtomicBuffer& buffer, std::int32_t offset, std::int32_t length)
+    {
+        called = true;
+    };
+
+    EXPECT_EQ(LOG_BUFFER_CAPACITY, TermGapScanner::scanForGap(m_termBuffer, TERM_ID, tail, highWaterMark, f));
+
+    EXPECT_FALSE(called);
+}

e99565acecf996358a996f77bce7d474ddb63066
mjpt777 <mjpt777@gmail.com>
2016-01-01 16:53:18 +0000

    [Java] Fix bug introduced when idle strategies where updated. Issue #189

--- aeron-samples/src/main/java/uk/co/real_logic/aeron/samples/Pong.java
+++ aeron-samples/src/main/java/uk/co/real_logic/aeron/samples/Pong.java
@@ -84,7 +84,7 @@ public class Pong
     public static void pingHandler(
         final Publication pongPublication, final DirectBuffer buffer, final int offset, final int length)
     {
-        if (pongPublication.offer(buffer, offset, length) < 0L)
+        if (pongPublication.offer(buffer, offset, length) > 0L)
         {
             return;
         }

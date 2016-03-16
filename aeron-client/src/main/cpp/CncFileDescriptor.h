2733c34f28161ff3518cd02994bc79713422337d
Todd L. Montgomery <tmont@nard.net>
2016-03-16 16:54:29 -0700

    [C++]: use new cnc filename.

--- aeron-client/src/main/cpp/CncFileDescriptor.h
+++ aeron-client/src/main/cpp/CncFileDescriptor.h
@@ -68,7 +68,7 @@ using namespace aeron::concurrent;
 */
 namespace CncFileDescriptor {
 
-static const std::string CNC_FILE = "cnc";
+static const std::string CNC_FILE = "cnc.dat";
 
 static const std::int32_t CNC_VERSION = 4;
 

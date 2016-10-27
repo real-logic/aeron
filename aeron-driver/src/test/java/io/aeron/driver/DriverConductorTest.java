5cfc9c7c759e379926aa7cd7d553eb8211e1a3f4
mjpt777 <mjpt777@gmail.com>
2016-10-27 13:49:34 +0100

    [Java] Use correct types when setting up mocks in DriverConductorTest.

--- aeron-driver/src/test/java/io/aeron/driver/DriverConductorTest.java
+++ aeron-driver/src/test/java/io/aeron/driver/DriverConductorTest.java
@@ -110,9 +110,9 @@ public class DriverConductorTest
         // System GC required in order to ensure that the direct byte buffers get cleaned and avoid OOM.
         System.gc();
 
-        when(mockRawLogFactory.newNetworkPublication(anyString(), anyInt(), anyInt(), anyInt(), anyInt()))
+        when(mockRawLogFactory.newNetworkPublication(anyString(), anyInt(), anyInt(), anyLong(), anyInt()))
             .thenReturn(LogBufferHelper.newTestLogBuffers(TERM_BUFFER_LENGTH));
-        when(mockRawLogFactory.newNetworkedImage(anyString(), anyInt(), anyInt(), anyInt(), eq(TERM_BUFFER_LENGTH)))
+        when(mockRawLogFactory.newNetworkedImage(anyString(), anyInt(), anyInt(), anyLong(), eq(TERM_BUFFER_LENGTH)))
             .thenReturn(LogBufferHelper.newTestLogBuffers(TERM_BUFFER_LENGTH));
         when(mockRawLogFactory.newDirectPublication(anyInt(), anyInt(), anyLong(), anyInt()))
             .thenReturn(LogBufferHelper.newTestLogBuffers(TERM_BUFFER_LENGTH));

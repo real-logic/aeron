f7f4f578eb25285b40ea8e33daca14b57a5a3cf6
mjpt777 <mjpt777@gmail.com>
2016-12-04 01:26:28 +0000

    [Java] Minor code simplification.

--- aeron-driver/src/main/java/io/aeron/driver/uri/UriUtil.java
+++ aeron-driver/src/main/java/io/aeron/driver/uri/UriUtil.java
@@ -38,25 +38,23 @@ public class UriUtil
     {
         final String query = uri.getQuery();
 
-        if (null == query)
+        if (null != query)
         {
-            return queryParams;
-        }
-
-        for (final String pair : query.split("&"))
-        {
-            final String[] componentParts = pair.split("=");
-            if (componentParts.length == 2)
+            for (final String pair : query.split("&"))
             {
-                queryParams.put(componentParts[0], componentParts[1]);
-            }
-            else if (componentParts.length == 1)
-            {
-                queryParams.put(componentParts[0], "");
-            }
-            else
-            {
-                throw new URISyntaxException(pair, "Did not contain 1 or 2 parts");
+                final String[] componentParts = pair.split("=");
+                if (componentParts.length == 2)
+                {
+                    queryParams.put(componentParts[0], componentParts[1]);
+                }
+                else if (componentParts.length == 1)
+                {
+                    queryParams.put(componentParts[0], "");
+                }
+                else
+                {
+                    throw new URISyntaxException(pair, "Did not contain 1 or 2 parts");
+                }
             }
         }
 

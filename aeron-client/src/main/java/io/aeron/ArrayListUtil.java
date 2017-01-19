42368238557d1552b3ba10c119c9e236572a291a
nitsanw <nitsanw@yahoo.com>
2017-01-19 22:06:01 +0200

    [Java] Javadoc.

--- aeron-client/src/main/java/io/aeron/ArrayListUtil.java
+++ aeron-client/src/main/java/io/aeron/ArrayListUtil.java
@@ -12,10 +12,10 @@ public abstract class ArrayListUtil
      * Removes element at i, but instead of copying all elements to the left, moves into the same slot the last
      * element. If i is the last element it is just removed. This avoids the copy costs, but spoils the list order.
      *
-     * @param list
-     * @param i
-     * @param lastIndex
-     * @param <T>
+     * @param list to be modified
+     * @param i removal index
+     * @param lastIndex last element index
+     * @param <T> element type
      */
     public static <T> void fastUnorderedRemove(ArrayList<T> list, int i, int lastIndex)
     {

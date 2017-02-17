f40c8eac4e362835d47ae6e8db58d34515588c5d
Martin Thompson <mjpt777@gmail.com>
2017-02-17 14:50:04 +0000

    [Java] Ignore archiver SystemTest because it is unreliable on Windows.

--- aeron-archiver/src/test/java/io/aeron/archiver/SystemTest.java
+++ aeron-archiver/src/test/java/io/aeron/archiver/SystemTest.java
@@ -31,6 +31,7 @@ import java.nio.channels.FileChannel;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.locks.LockSupport;
 
+@Ignore
 public class SystemTest
 {
     private static final boolean DEBUG = false;

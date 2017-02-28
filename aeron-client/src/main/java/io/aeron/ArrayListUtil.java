11ffd22d0a98556575a7919e1b826dd78ceee06d
Martin Thompson <mjpt777@gmail.com>
2017-02-28 18:45:51 +0000

    [Java] Add copyright and fix Javadoc.

--- aeron-client/src/main/java/io/aeron/ArrayListUtil.java
+++ aeron-client/src/main/java/io/aeron/ArrayListUtil.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 - 2017 Real Logic Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.aeron;
 
 import java.util.ArrayList;
@@ -9,13 +24,14 @@ public abstract class ArrayListUtil
     }
 
     /**
-     * Removes element at i, but instead of copying all elements to the left, moves into the same slot the last
-     * element. If i is the last element it is just removed. This avoids the copy costs, but spoils the list order.
+     * Removes element at index i, but instead of copying all elements to the left, moves into the same slot the last
+     * element. This avoids the copy costs, but spoils the list order. If i is the last element it is just removed.
      *
-     * @param list      to be modified
-     * @param i         removal index
-     * @param lastIndex last element index
-     * @param <T>       element type
+     * @param list      to be modified.
+     * @param i         removal index.
+     * @param lastIndex last element index.
+     * @param <T>       element type.
+     * @throws IndexOutOfBoundsException if i or lastIndex are out of bounds.
      */
     public static <T> void fastUnorderedRemove(final ArrayList<T> list, final int i, final int lastIndex)
     {
@@ -27,13 +43,13 @@ public abstract class ArrayListUtil
     }
 
     /**
-     * Removes element at i, but instead of copying all elements to the left, moves into the same slot the last
-     * element. If i is the last element it is just removed. This avoids the copy costs, but spoils the list order.
+     * Removes element but instead of copying all elements to the left, moves into the same slot the last element.
+     * This avoids the copy costs, but spoils the list order. If e is the last element it is just removed.
      *
-     * @param list      to be modified
-     * @param e         to be removed
-     * @param <T>       element type
-     * @return true if found and removed, false otherwise
+     * @param list      to be modified.
+     * @param e         to be removed.
+     * @param <T>       element type.
+     * @return true if found and removed, false otherwise.
      */
     public static <T> boolean fastUnorderedRemove(final ArrayList<T> list, final T e)
     {
@@ -45,6 +61,7 @@ public abstract class ArrayListUtil
                 return true;
             }
         }
+
         return false;
     }
 }

58e677e425fd596253bdfc7eb9fb58e7628a1a93
Martin Thompson <mjpt777@gmail.com>
2017-04-25 11:24:39 +0100

    [Java] Should wait on both subscriptions to be ready.

--- aeron-system-tests/src/test/java/io/aeron/MultiDestinationCastTest.java
+++ aeron-system-tests/src/test/java/io/aeron/MultiDestinationCastTest.java
@@ -124,7 +124,7 @@ public class MultiDestinationCastTest
         subscriptionA = clientA.addSubscription(SUB1_MDC_DYNAMIC_URI, STREAM_ID);
         subscriptionB = clientB.addSubscription(SUB2_MDC_DYNAMIC_URI, STREAM_ID);
 
-        while (subscriptionA.hasNoImages() && subscriptionB.hasNoImages())
+        while (subscriptionA.hasNoImages() || subscriptionB.hasNoImages())
         {
             Thread.sleep(1);
         }
@@ -142,7 +142,7 @@ public class MultiDestinationCastTest
         publication.addDestination(SUB1_MDC_MANUAL_URI);
         publication.addDestination(SUB2_MDC_MANUAL_URI);
 
-        while (subscriptionA.hasNoImages() && subscriptionB.hasNoImages())
+        while (subscriptionA.hasNoImages() || subscriptionB.hasNoImages())
         {
             Thread.sleep(1);
         }
@@ -159,7 +159,7 @@ public class MultiDestinationCastTest
         subscriptionA = clientA.addSubscription(SUB1_MDC_DYNAMIC_URI, STREAM_ID);
         subscriptionB = clientB.addSubscription(SUB2_MDC_DYNAMIC_URI, STREAM_ID);
 
-        while (subscriptionA.hasNoImages() && subscriptionB.hasNoImages())
+        while (subscriptionA.hasNoImages() || subscriptionB.hasNoImages())
         {
             Thread.sleep(1);
         }
@@ -218,7 +218,7 @@ public class MultiDestinationCastTest
         subscriptionA = clientA.addSubscription(SUB1_MDC_DYNAMIC_URI, STREAM_ID);
         subscriptionB = clientA.addSubscription(SUB2_MDC_DYNAMIC_URI, STREAM_ID);
 
-        while (subscriptionA.hasNoImages() && subscriptionB.hasNoImages())
+        while (subscriptionA.hasNoImages() || subscriptionB.hasNoImages())
         {
             Thread.sleep(1);
         }
@@ -280,7 +280,7 @@ public class MultiDestinationCastTest
         publication.addDestination(SUB1_MDC_MANUAL_URI);
         publication.addDestination(SUB2_MDC_MANUAL_URI);
 
-        while (subscriptionA.hasNoImages() && subscriptionB.hasNoImages())
+        while (subscriptionA.hasNoImages() || subscriptionB.hasNoImages())
         {
             Thread.sleep(1);
         }
@@ -347,7 +347,7 @@ public class MultiDestinationCastTest
         publication.addDestination(SUB1_MDC_MANUAL_URI);
         publication.addDestination(SUB2_MDC_MANUAL_URI);
 
-        while (subscriptionA.hasNoImages() && subscriptionB.hasNoImages())
+        while (subscriptionA.hasNoImages() || subscriptionB.hasNoImages())
         {
             Thread.sleep(1);
         }

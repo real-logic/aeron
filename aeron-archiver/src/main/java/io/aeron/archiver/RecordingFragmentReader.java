18795459e43c4448093c8671848788af8276beee
mjpt777 <mjpt777@gmail.com>
2017-05-16 17:10:37 +0100

    [Java] Added TODO.

--- aeron-archiver/src/main/java/io/aeron/archiver/RecordingFragmentReader.java
+++ aeron-archiver/src/main/java/io/aeron/archiver/RecordingFragmentReader.java
@@ -168,6 +168,7 @@ class RecordingFragmentReader implements AutoCloseable
             {
                 IoUtil.unmap(termMappedUnsafeBuffer.byteBuffer());
             }
+            // TODO: Is a mapping created per term? Would it not be more effecient to do it per segment?
             // rotate term
             final MappedByteBuffer mappedByteBuffer = currentDataChannel.map(
                 READ_ONLY, recordingTermStartOffset, termBufferLength);

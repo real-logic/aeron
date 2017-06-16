1d8455f06b4e771926a5762e4b281bf8871ad6cf
mjpt777 <mjpt777@gmail.com>
2017-06-16 20:58:31 +0100

    [Java] Update Aeron client about what the default error handler will do.

--- aeron-client/src/main/java/io/aeron/Aeron.java
+++ aeron-client/src/main/java/io/aeron/Aeron.java
@@ -41,6 +41,9 @@ import static org.agrona.IoUtil.mapExistingFile;
  * Use an {@link Aeron.Context} to configure the Aeron object.
  * <p>
  * A client application requires only one Aeron object per Media Driver.
+ * <p>
+ * <b>Note:</b> If {@link Aeron.Context#errorHandler(ErrorHandler)} is not set and a {@link DriverTimeoutException}
+ * occurs then the process will face the wraith of {@link System#exit(int)}. See {@link #DEFAULT_ERROR_HANDLER}.
  */
 public final class Aeron implements AutoCloseable
 {

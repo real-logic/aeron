0b6966b7460d53d6f1cadf2e5721fa0569ea7421
Todd L. Montgomery <tmont@nard.net>
2017-07-26 21:35:46 -0700

    [C]: add build script for VS 2017 for driver. Does not build yet.

---
+++ cppbuild/cppbuild-driver-vs.cmd
@@ -0,0 +1,14 @@
+@if "%DEBUG%" == "" @echo off
+setlocal
+
+set SOURCE_DIR=%CD%
+set BUILD_DIR=%CD%\cppbuild\Release
+
+if EXIST %BUILD_DIR% rd /S /Q %BUILD_DIR%
+
+md %BUILD_DIR%
+pushd %BUILD_DIR%
+
+cmake -G "Visual Studio 15 Win64" -DBUILD_AERON_DRIVER=ON %SOURCE_DIR%
+cmake --build . --clean-first --config Release
+ctest -C Release

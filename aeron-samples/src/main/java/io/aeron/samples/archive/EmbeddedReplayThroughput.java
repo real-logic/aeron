9b82ad6ce7b7f9bfad528d5ac0738b1885213751
mjpt777 <mjpt777@gmail.com>
2017-08-12 21:21:37 +0100

    [Java] Add EmbeddedReplayThroughput sample.

---
+++ aeron-samples/src/main/java/io/aeron/samples/archive/EmbeddedReplayThroughput.java
@@ -0,0 +1,267 @@
+/*
+ *  Copyright 2017 Real Logic Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.aeron.samples.archive;
+
+import io.aeron.*;
+import io.aeron.archive.Archive;
+import io.aeron.archive.ArchiveThreadingMode;
+import io.aeron.archive.client.AeronArchive;
+import io.aeron.archive.codecs.SourceLocation;
+import io.aeron.driver.MediaDriver;
+import io.aeron.driver.ThreadingMode;
+import io.aeron.logbuffer.FragmentHandler;
+import io.aeron.logbuffer.Header;
+import io.aeron.samples.SampleConfiguration;
+import org.agrona.CloseHelper;
+import org.agrona.DirectBuffer;
+import org.agrona.collections.MutableLong;
+import org.agrona.concurrent.UnsafeBuffer;
+import org.agrona.console.ContinueBarrier;
+
+import java.io.File;
+import java.io.IOException;
+
+import static io.aeron.logbuffer.FrameDescriptor.FRAME_ALIGNMENT;
+import static org.agrona.BufferUtil.allocateDirectAligned;
+
+public class EmbeddedReplayThroughput implements AutoCloseable
+{
+    private static final double MEGABYTE = 1024.0d * 1024.0d;
+    private static final int REPLAY_STREAM_ID = 101;
+    private static final String REPLAY_URI = "aeron:udp?endpoint=127.0.0.1:54326";
+
+    private static final long NUMBER_OF_MESSAGES = SampleConfiguration.NUMBER_OF_MESSAGES;
+    private static final int MESSAGE_LENGTH = SampleConfiguration.MESSAGE_LENGTH;
+    private static final int FRAGMENT_COUNT_LIMIT = SampleConfiguration.FRAGMENT_COUNT_LIMIT;
+    private static final int STREAM_ID = SampleConfiguration.STREAM_ID;
+    private static final String CHANNEL = SampleConfiguration.CHANNEL;
+    private static final FragmentHandler NOOP_FRAGMENT_HANDLER = (buffer, offset, length, header) -> {};
+
+
+    private MediaDriver driver;
+    private Archive archive;
+    private Aeron aeron;
+    private AeronArchive aeronArchive;
+    private final UnsafeBuffer buffer = new UnsafeBuffer(allocateDirectAligned(MESSAGE_LENGTH, FRAME_ALIGNMENT));
+    private FragmentHandler fragmentHandler = new FragmentAssembler(this::onMessage);
+    private int messageCount;
+
+    public static void main(final String[] args) throws Exception
+    {
+        MediaDriver.loadPropertiesFiles(args);
+
+        try (EmbeddedReplayThroughput test = new EmbeddedReplayThroughput())
+        {
+            System.out.println("Making a recording for playback...");
+            final long recordingLength = test.makeRecording();
+            Thread.sleep(10);
+
+            System.out.println("Finding the recording...");
+            final long recordingId = test.findRecordingId(CHANNEL, STREAM_ID);
+            final ContinueBarrier barrier = new ContinueBarrier("Execute again?");
+
+            do
+            {
+                System.out.println("Replaying " + NUMBER_OF_MESSAGES + " messages");
+                final long start = System.currentTimeMillis();
+
+                test.replayRecording(recordingLength, recordingId);
+
+                final long durationMs = System.currentTimeMillis() - start;
+                final double dataRate = (recordingLength * 1000.0d / durationMs) / MEGABYTE;
+                final double recordingMb = recordingLength / MEGABYTE;
+                final long msgRate = (NUMBER_OF_MESSAGES / durationMs) * 1000L;
+
+                System.out.println("Performance inclusive of replay request and connection setup:");
+                System.out.printf("Replayed %.02f MB @ %.02f MB/s - %,d msg/sec%n", recordingMb, dataRate, msgRate);
+            }
+            while (barrier.await());
+        }
+    }
+
+    public EmbeddedReplayThroughput()
+    {
+        driver = MediaDriver.launch(
+            new MediaDriver.Context()
+                .threadingMode(ThreadingMode.DEDICATED)
+                .useConcurrentCounterManager(true)
+                .dirsDeleteOnStart(true));
+
+        archive = Archive.launch(
+            new Archive.Context()
+                .archiveDir(createTempDir())
+                .fileSyncLevel(0)
+                .threadingMode(ArchiveThreadingMode.DEDICATED)
+                .countersManager(driver.context().countersManager())
+                .errorHandler(driver.context().errorHandler()));
+
+        aeron = Aeron.connect();
+
+        aeronArchive = AeronArchive.connect(
+            new AeronArchive.Context()
+                .aeron(aeron));
+    }
+
+    public void close()
+    {
+        CloseHelper.close(aeronArchive);
+        CloseHelper.close(archive);
+        CloseHelper.close(driver);
+
+        archive.context().deleteArchiveDirectory();
+        driver.context().deleteAeronDirectory();
+    }
+
+    private long makeRecording()
+    {
+        aeronArchive.startRecording(CHANNEL, STREAM_ID, SourceLocation.LOCAL);
+
+        try (ExclusivePublication publication = aeron.addExclusivePublication(CHANNEL, STREAM_ID);
+             Subscription subscription = aeron.addSubscription(CHANNEL, STREAM_ID))
+        {
+            while (subscription.hasNoImages())
+            {
+                Thread.yield();
+            }
+
+            final Image image = subscription.getImage(0);
+
+            for (int i = 0; i < NUMBER_OF_MESSAGES; i++)
+            {
+                buffer.putInt(0, i);
+
+                while (publication.offer(buffer, 0, MESSAGE_LENGTH) < 0)
+                {
+                    Thread.yield();
+                    image.poll(NOOP_FRAGMENT_HANDLER, 10);
+                }
+
+                image.poll(NOOP_FRAGMENT_HANDLER, 10);
+            }
+
+            final long position = publication.position();
+
+            while (image.position() < position)
+            {
+                image.poll(NOOP_FRAGMENT_HANDLER, 10);
+            }
+
+            return position;
+        }
+    }
+
+    private void replayRecording(final long recordingLength, final long recordingId)
+    {
+        try (Subscription replaySubscription = aeronArchive.replay(
+            recordingId, 0L, recordingLength, REPLAY_URI, REPLAY_STREAM_ID))
+        {
+            while (replaySubscription.hasNoImages())
+            {
+                Thread.yield();
+            }
+
+            messageCount = 0;
+
+            while (messageCount < NUMBER_OF_MESSAGES)
+            {
+                final int fragments = replaySubscription.poll(fragmentHandler, FRAGMENT_COUNT_LIMIT);
+                if (0 == fragments)
+                {
+                    if (replaySubscription.hasNoImages())
+                    {
+                        System.out.println("Unexpected end of stream at message count: " + messageCount);
+                        break;
+                    }
+
+                    Thread.yield();
+                }
+            }
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public void onMessage(final DirectBuffer buffer, final int offset, final int length, final Header header)
+    {
+        final int count = buffer.getInt(offset);
+        if (count != messageCount)
+        {
+            throw new IllegalStateException("Invalid message count=" + count + " @ " + messageCount);
+        }
+
+        messageCount++;
+    }
+
+    private long findRecordingId(final String expectedChannel, final int expectedStreamId)
+    {
+        final MutableLong foundRecordingId = new MutableLong();
+
+        final int recordingsFound = aeronArchive.listRecordingsForUri(
+            0L,
+            10,
+            expectedChannel,
+            expectedStreamId,
+            (
+                controlSessionId,
+                correlationId,
+                recordingId,
+                startTimestamp,
+                stopTimestamp,
+                startPosition,
+                stopPosition,
+                initialTermId,
+                segmentFileLength,
+                termBufferLength,
+                mtuLength,
+                sessionId,
+                streamId,
+                strippedChannel,
+                originalChannel,
+                sourceIdentity
+            ) -> foundRecordingId.set(recordingId));
+
+        if (1 != recordingsFound)
+        {
+            throw new IllegalStateException("Should have been one recording");
+        }
+
+        return foundRecordingId.get();
+    }
+
+    private File createTempDir()
+    {
+        final File tempDirForTest;
+        try
+        {
+            tempDirForTest = File.createTempFile("archive", "tmp");
+        }
+        catch (final IOException ex)
+        {
+            throw new RuntimeException(ex);
+        }
+
+        if (!tempDirForTest.delete())
+        {
+            throw new IllegalStateException("Failed to delete: " + tempDirForTest);
+        }
+
+        if (!tempDirForTest.mkdir())
+        {
+            throw new IllegalStateException("Failed to create: " + tempDirForTest);
+        }
+
+        return tempDirForTest;
+    }
+}

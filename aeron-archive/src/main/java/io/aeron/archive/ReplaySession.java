6f1ac3ba2a865873c89f71d456ab3697ebd1e5de
Martin Thompson <mjpt777@gmail.com>
2017-08-16 20:05:29 +0100

    [Java] Give better error message when requesting a replay for data that does not exist.

--- aeron-archive/src/main/java/io/aeron/archive/ReplaySession.java
+++ aeron-archive/src/main/java/io/aeron/archive/ReplaySession.java
@@ -116,12 +116,11 @@ class ReplaySession implements Session, SimplifiedControlledFragmentHandler
             return;
         }
 
-        // TODO: need to consider the start and stop states for a replay of a stream that is live recording.
         final long stopPosition = descriptorDecoder.stopPosition();
-        if (replayPosition - stopPosition > 0)
+        if (replayPosition - stopPosition >= 0)
         {
             final String errorMessage = "requested replay start position(=" + replayPosition +
-                ") is after recording stop position(=" + stopPosition + ")";
+                ") must be before current highest recorded position(=" + stopPosition + ")";
             closeOnError(new IllegalArgumentException(errorMessage), errorMessage);
             cursor = null;
             replayPublication = null;

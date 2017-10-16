7f9f87708ede4d7bf930d79be022f6a7c52fb8b9
Martin Thompson <mjpt777@gmail.com>
2017-10-16 13:43:22 +0100

    [Java] Fix segment filename generation in Archive. Issue #414.

---
+++ aeron-archive/src/test/java/io/aeron/archive/ArchiveTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2017 Real Logic Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.aeron.archive;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ArchiveTest
+{
+    @Test
+    public void shouldGenerateRecordingName()
+    {
+        final long recordingId = 1L;
+        final int segmentIndex = 2;
+        final String expected = "1-2.rec";
+
+        final String actual = Archive.segmentFileName(recordingId, segmentIndex);
+
+        assertThat(actual, is(expected));
+    }
+}

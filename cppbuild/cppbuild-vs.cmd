f1f6a1b16b481f07e3fff2b673940304155343bb
Cesare Ferrari <cesare@loftsoft.co.uk>
2017-11-28 22:09:12 +0000

    [C++] Windows build command now mirrors the support for warnings as errors and building the driver

--- cppbuild/cppbuild-vs.cmd
+++ cppbuild/cppbuild-vs.cmd
@@ -1,14 +1,41 @@
 @if "%DEBUG%" == "" @echo off
 setlocal
+Setlocal EnableDelayedExpansion
 
 set SOURCE_DIR=%CD%
 set BUILD_DIR=%CD%\cppbuild\Release
+
+for %%o in (%*) do (
+
+    set PROCESSED=0
+
+    if "%%o"=="--help" (
+	    echo cppbuild.cmd [--c-warnings-as-errors] [--cxx-warnings-as-errors] [--build-aeron-driver]
+		exit /b
+	)    
+
+	if "%%o"=="--c-warnings-as-errors" (
+	    set EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DC_WARNINGS_AS_ERRORS=ON
+		set PROCESSED=1
+	)
+
+    if "%%o"=="--cxx-warnings-as-errors" (
+	    set EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DCXX_WARNINGS_AS_ERRORS=ON
+		set PROCESSED=1
+    )
+
+    if "%%o"=="--build-aeron-driver" (
+        set EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DBUILD_AERON_DRIVER=ON
+	    set PROCESSED=1
+	)
+)
+
 
 if EXIST %BUILD_DIR% rd /S /Q %BUILD_DIR%
 
 md %BUILD_DIR%
 pushd %BUILD_DIR%
 
-cmake -G "Visual Studio 15 Win64" %SOURCE_DIR%
+cmake -G "Visual Studio 15 Win64" %EXTRA_CMAKE_ARGS% %SOURCE_DIR%
 cmake --build . --clean-first --config Release
 ctest -C Release

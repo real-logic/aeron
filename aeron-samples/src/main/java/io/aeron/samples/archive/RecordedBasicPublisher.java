bae436d853b1b90406edced378b5a132d0f97010
mjpt777 <mjpt777@gmail.com>
2017-12-27 08:19:28 +0000

    [Java] Clean up by stopping recording in RecordedBasicPublisher and remove the unneeded linger code.

--- aeron-samples/src/main/java/io/aeron/samples/archive/RecordedBasicPublisher.java
+++ aeron-samples/src/main/java/io/aeron/samples/archive/RecordedBasicPublisher.java
@@ -25,9 +25,7 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * Basic Aeron publisher application which is recorded in an archive.
- * This publisher sends a fixed number of messages on a channel and stream ID,
- * then lingers to allow any consumers that may have experienced loss a chance to NAK for
- * and recover any missing data.
+ * This publisher sends a fixed number of messages on a channel and stream ID.
  * <p>
  * The default values for number of messages, channel, and stream ID are
  * defined in {@link SampleConfiguration} and can be overridden by
@@ -39,7 +37,6 @@ public class RecordedBasicPublisher
     private static final int STREAM_ID = SampleConfiguration.STREAM_ID;
     private static final String CHANNEL = SampleConfiguration.CHANNEL;
     private static final long NUMBER_OF_MESSAGES = SampleConfiguration.NUMBER_OF_MESSAGES;
-    private static final long LINGER_TIMEOUT_MS = SampleConfiguration.LINGER_TIMEOUT_MS;
 
     private static final UnsafeBuffer BUFFER = new UnsafeBuffer(BufferUtil.allocateDirectAligned(256, 64));
 
@@ -47,67 +44,67 @@ public class RecordedBasicPublisher
     {
         System.out.println("Publishing to " + CHANNEL + " on stream Id " + STREAM_ID);
 
-        try (AeronArchive archive = AeronArchive.connect();
-             Publication publication = archive.addRecordedPublication(CHANNEL, STREAM_ID))
+        try (AeronArchive archive = AeronArchive.connect())
         {
-            for (int i = 0; i < NUMBER_OF_MESSAGES; i++)
+            try (Publication publication = archive.addRecordedPublication(CHANNEL, STREAM_ID))
             {
-                final String message = "Hello World! " + i;
-                final byte[] messageBytes = message.getBytes();
-                BUFFER.putBytes(0, messageBytes);
-
-                System.out.print("Offering " + i + "/" + NUMBER_OF_MESSAGES + " - ");
-
-                final long result = publication.offer(BUFFER, 0, messageBytes.length);
-
-                if (result < 0L)
+                for (int i = 0; i < NUMBER_OF_MESSAGES; i++)
                 {
-                    if (result == Publication.BACK_PRESSURED)
+                    final String message = "Hello World! " + i;
+                    final byte[] messageBytes = message.getBytes();
+                    BUFFER.putBytes(0, messageBytes);
+
+                    System.out.print("Offering " + i + "/" + NUMBER_OF_MESSAGES + " - ");
+
+                    final long result = publication.offer(BUFFER, 0, messageBytes.length);
+
+                    if (result < 0L)
                     {
-                        System.out.println("Offer failed due to back pressure");
-                    }
-                    else if (result == Publication.NOT_CONNECTED)
-                    {
-                        System.out.println("Offer failed because publisher is not connected to subscriber");
-                    }
-                    else if (result == Publication.ADMIN_ACTION)
-                    {
-                        System.out.println("Offer failed because of an administration action in the system");
-                    }
-                    else if (result == Publication.CLOSED)
-                    {
-                        System.out.println("Offer failed publication is closed");
-                        break;
-                    }
-                    else if (result == Publication.MAX_POSITION_EXCEEDED)
-                    {
-                        System.out.println("Offer failed due to publication reaching max position");
-                        break;
+                        if (result == Publication.BACK_PRESSURED)
+                        {
+                            System.out.println("Offer failed due to back pressure");
+                        }
+                        else if (result == Publication.ADMIN_ACTION)
+                        {
+                            System.out.println("Offer failed because of an administration action in the system");
+                        }
+                        else if (result == Publication.NOT_CONNECTED)
+                        {
+                            System.out.println("Offer failed because publisher is not connected to subscriber");
+                        }
+                        else if (result == Publication.CLOSED)
+                        {
+                            System.out.println("Offer failed publication is closed");
+                            break;
+                        }
+                        else if (result == Publication.MAX_POSITION_EXCEEDED)
+                        {
+                            System.out.println("Offer failed due to publication reaching max position");
+                            break;
+                        }
+                        else
+                        {
+                            System.out.println("Offer failed due to unknown reason");
+                        }
                     }
                     else
                     {
-                        System.out.println("Offer failed due to unknown reason");
+                        System.out.println("yay!");
                     }
-                }
-                else
-                {
-                    System.out.println("yay!");
+
+                    if (!publication.isConnected())
+                    {
+                        System.out.println("No active subscribers detected");
+                    }
+
+                    Thread.sleep(TimeUnit.SECONDS.toMillis(1));
                 }
 
-                if (!publication.isConnected())
-                {
-                    System.out.println("No active subscribers detected");
-                }
-
-                Thread.sleep(TimeUnit.SECONDS.toMillis(1));
+                System.out.println("Done sending.");
             }
-
-            System.out.println("Done sending.");
-
-            if (LINGER_TIMEOUT_MS > 0)
+            finally
             {
-                System.out.println("Lingering for " + LINGER_TIMEOUT_MS + " milliseconds...");
-                Thread.sleep(LINGER_TIMEOUT_MS);
+                archive.stopRecording(CHANNEL, STREAM_ID);
             }
         }
     }

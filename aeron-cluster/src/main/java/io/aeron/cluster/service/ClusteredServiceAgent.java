8893db9b31f5a1f19fb37f3977e98988bf71ad66
James Watson <james@weareadaptive.com>
2018-03-10 01:27:18 +0000

    [Java] Adjust ClusteredServiceAgent Replay for partial Log

--- aeron-cluster/src/main/java/io/aeron/cluster/service/ClusteredServiceAgent.java
+++ aeron-cluster/src/main/java/io/aeron/cluster/service/ClusteredServiceAgent.java
@@ -354,12 +354,13 @@ final class ClusteredServiceAgent implements Agent, Cluster, ServiceControlListe
                 serviceControlPublisher.ackAction(baseLogPosition, leadershipTermId, serviceId, ClusterAction.READY);
 
                 final Image image = awaitImage(activeLog.sessionId, subscription);
+                final long initialPosition = image.position();
                 final ReadableCounter limit = new ReadableCounter(counters, counterId);
                 final BoundedLogAdapter adapter = new BoundedLogAdapter(image, limit, this);
 
                 consumeImage(image, adapter);
 
-                final long logPosition = baseLogPosition + image.position();
+                final long logPosition = baseLogPosition + image.position() - initialPosition;
                 serviceControlPublisher.ackAction(logPosition, leadershipTermId, serviceId, ClusterAction.REPLAY);
             }
         }

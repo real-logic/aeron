09d22aca82279b95cbaffa721fa89edcef4101b7
mjpt777 <mjpt777@gmail.com>
2018-03-20 00:17:59 +0000

    [Java] Minor simplification of SocketAddressUtil.

--- aeron-driver/src/main/java/io/aeron/driver/media/SocketAddressUtil.java
+++ aeron-driver/src/main/java/io/aeron/driver/media/SocketAddressUtil.java
@@ -50,9 +50,55 @@ class SocketAddressUtil
         return address;
     }
 
+    enum IpV4State
+    {
+        HOST, PORT
+    }
+
     enum IpV6State
     {
         START_ADDR, HOST, SCOPE, END_ADDR, PORT
+    }
+
+    private static InetSocketAddress tryParseIpV4(final CharSequence cs)
+    {
+        IpV4State state = IpV4State.HOST;
+        int separatorIndex = -1;
+        final int length = cs.length();
+
+        for (int i = 0; i < length; i++)
+        {
+            final char c = cs.charAt(i);
+            switch (state)
+            {
+                case HOST:
+                    if (':' == c)
+                    {
+                        separatorIndex = i;
+                        state = IpV4State.PORT;
+                    }
+                    break;
+
+                case PORT:
+                    if (':' == c)
+                    {
+                        return null;
+                    }
+                    else if (c < '0' || '9' < c)
+                    {
+                        return null;
+                    }
+            }
+        }
+
+        if (-1 != separatorIndex && 1 < length - separatorIndex)
+        {
+            final String hostname = cs.subSequence(0, separatorIndex).toString();
+            final int port = parseInt(cs.subSequence(separatorIndex + 1, length).toString());
+            return new InetSocketAddress(hostname, port);
+        }
+
+        throw new IllegalArgumentException("The 'port' portion of the address is required");
     }
 
     private static InetSocketAddress tryParseIpV6(final CharSequence cs)
@@ -133,67 +179,11 @@ class SocketAddressUtil
 
         if (-1 != portIndex && 1 < length - portIndex)
         {
-            return newSocketAddress(
-                cs.subSequence(1, scopeIndex != -1 ? scopeIndex : portIndex - 1).toString(),
-                cs.subSequence(portIndex + 1, length).toString());
+            final String hostname = cs.subSequence(1, scopeIndex != -1 ? scopeIndex : portIndex - 1).toString();
+            final int port = parseInt(cs.subSequence(portIndex + 1, length).toString());
+            return new InetSocketAddress(hostname, port);
         }
 
         throw new IllegalArgumentException("The 'port' portion of the address is required");
-    }
-
-    enum IpV4State
-    {
-        HOST, PORT
-    }
-
-    private static InetSocketAddress tryParseIpV4(final CharSequence cs)
-    {
-        IpV4State state = IpV4State.HOST;
-        int separatorIndex = -1;
-        final int length = cs.length();
-
-        for (int i = 0; i < length; i++)
-        {
-            final char c = cs.charAt(i);
-            switch (state)
-            {
-                case HOST:
-                    if (':' == c)
-                    {
-                        separatorIndex = i;
-                        state = IpV4State.PORT;
-                    }
-                    break;
-
-                case PORT:
-                    if (':' == c)
-                    {
-                        return null;
-                    }
-                    else if (c < '0' || '9' < c)
-                    {
-                        return null;
-                    }
-            }
-        }
-
-        if (-1 != separatorIndex && 1 < length - separatorIndex)
-        {
-            return newSocketAddress(
-                cs.subSequence(0, separatorIndex).toString(),
-                cs.subSequence(separatorIndex + 1, length).toString());
-        }
-
-        throw new IllegalArgumentException("The 'port' portion of the address is required");
-    }
-
-    private static InetSocketAddress newSocketAddress(final String host, final String port)
-    {
-        if (null == port)
-        {
-            throw new IllegalArgumentException("The 'port' portion of the address is required");
-        }
-
-        return new InetSocketAddress(host, parseInt(port));
     }
 }

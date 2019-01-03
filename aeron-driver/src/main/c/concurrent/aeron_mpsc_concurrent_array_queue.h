6083fe29e5ad6e131e2fe5cc5b1c812e1c2da72b
mjpt777 <mjpt777@gmail.com>
2019-01-03 10:17:22 +0000

    [C] Remove unneeded volatile loads from MPSC concurrent array queue.

--- aeron-driver/src/main/c/concurrent/aeron_mpsc_concurrent_array_queue.h
+++ aeron-driver/src/main/c/concurrent/aeron_mpsc_concurrent_array_queue.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2018 Real Logic Ltd.
+ * Copyright 2014-2019 Real Logic Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #ifndef AERON_MPSC_CONCURRENT_ARRAY_QUEUE_H
 #define AERON_MPSC_CONCURRENT_ARRAY_QUEUE_H
 
@@ -33,12 +32,14 @@ typedef struct aeron_mpsc_concurrent_array_queue_stct
         int8_t padding[(2 * AERON_CACHE_LINE_LENGTH) - (3 * sizeof(uint64_t))];
     }
     producer;
+
     struct
     {
         uint64_t head;
         int8_t padding[(2 * AERON_CACHE_LINE_LENGTH) - (1 * sizeof(uint64_t))];
     }
     consumer;
+
     uint64_t capacity;
     uint64_t mask;
     volatile void **buffer;
@@ -61,7 +62,6 @@ inline aeron_queue_offer_result_t aeron_mpsc_concurrent_array_queue_offer(
     uint64_t current_head;
     AERON_GET_VOLATILE(current_head, queue->producer.shared_head_cache);
     uint64_t buffer_limit = current_head + queue->capacity;
-
     uint64_t current_tail;
 
     do
@@ -83,8 +83,8 @@ inline aeron_queue_offer_result_t aeron_mpsc_concurrent_array_queue_offer(
     while(!aeron_cmpxchgu64(&queue->producer.tail, current_tail, current_tail + 1));
 
     const uint64_t index = current_tail & queue->mask;
-
     AERON_PUT_ORDERED(queue->buffer[index], element);
+
     return AERON_OFFER_SUCCESS;
 }
 
@@ -94,9 +94,7 @@ inline uint64_t aeron_mpsc_concurrent_array_queue_drain(
     void *clientd,
     uint64_t limit)
 {
-    uint64_t current_head;
-    AERON_GET_VOLATILE(current_head, queue->consumer.head);
-
+    uint64_t current_head = queue->consumer.head;
     uint64_t next_sequence = current_head;
     const uint64_t limit_sequence = next_sequence + limit;
 
@@ -125,8 +123,7 @@ inline uint64_t aeron_mpsc_concurrent_array_queue_drain_all(
     aeron_queue_drain_func_t func,
     void *clientd)
 {
-    uint64_t current_head;
-    AERON_GET_VOLATILE(current_head, queue->consumer.head);
+    uint64_t current_head = queue->consumer.head;
     uint64_t current_tail;
     AERON_GET_VOLATILE(current_tail, queue->producer.tail);
 

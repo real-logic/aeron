58598dd66d122128cbec49d9e9907a6d2ba5d7e5
mjpt777 <mjpt777@gmail.com>
2019-01-05 12:05:36 +0000

    [C] Formatting.

--- aeron-driver/src/main/c/aeron_driver_context.h
+++ aeron-driver/src/main/c/aeron_driver_context.h
@@ -60,8 +60,8 @@ typedef struct aeron_driver_sender_proxy_stct aeron_driver_sender_proxy_t;
 typedef struct aeron_driver_receiver_proxy_stct aeron_driver_receiver_proxy_t;
 
 typedef aeron_rb_handler_t aeron_driver_conductor_to_driver_interceptor_func_t;
-typedef void (*aeron_driver_conductor_to_client_interceptor_func_t)
-    (aeron_driver_conductor_t *conductor, int32_t msg_type_id, const void *message, size_t length);
+typedef void (*aeron_driver_conductor_to_client_interceptor_func_t)(
+    aeron_driver_conductor_t *conductor, int32_t msg_type_id, const void *message, size_t length);
 
 typedef enum aeron_threading_mode_enum
 {
@@ -94,8 +94,8 @@ typedef struct aeron_driver_context_stct
     size_t counters_values_buffer_length;       /* aeron.counters.buffer.length = 1MB */
     size_t counters_metadata_buffer_length;     /* counters value length times the ratio of metadata to values record */
     size_t error_buffer_length;                 /* aeron.error.buffer.length = 1MB */
-    size_t term_buffer_length;                  /* aeron.term.buffer.length = 16 * 1024 * 1024 */
-    size_t ipc_term_buffer_length;              /* aeron.ipc.term.buffer.length = 64 * 1024 * 1024 */
+    size_t term_buffer_length;                  /* aeron.term.buffer.length = 16MB */
+    size_t ipc_term_buffer_length;              /* aeron.ipc.term.buffer.length = 64MB */
     size_t mtu_length;                          /* aeron.mtu.length = 1408 */
     size_t ipc_mtu_length;                      /* aeron.ipc.mtu.length = 1408 */
     size_t ipc_publication_window_length;       /* aeron.ipc.publication.term.window.length = 0 */
@@ -231,9 +231,8 @@ inline size_t aeron_ipc_publication_term_window_length(aeron_driver_context_t *c
 
     if (0 != context->ipc_publication_window_length)
     {
-        publication_term_window_length = (publication_term_window_length < context->ipc_publication_window_length) ?
-            publication_term_window_length :
-            context->ipc_publication_window_length;
+        publication_term_window_length = publication_term_window_length < context->ipc_publication_window_length ?
+            publication_term_window_length : context->ipc_publication_window_length;
     }
 
     return publication_term_window_length;
@@ -245,9 +244,8 @@ inline size_t aeron_network_publication_term_window_length(aeron_driver_context_
 
     if (0 != context->publication_window_length)
     {
-        publication_term_window_length = (publication_term_window_length < context->publication_window_length) ?
-            publication_term_window_length :
-            context->publication_window_length;
+        publication_term_window_length = publication_term_window_length < context->publication_window_length ?
+            publication_term_window_length : context->publication_window_length;
     }
 
     return publication_term_window_length;

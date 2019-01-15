6eb2f9a2af9a2511d7965894f20bea7980617bf4
Todd L. Montgomery <tmont@nard.net>
2019-01-15 10:17:38 -0800

    [C]: add client timeout event to C media driver.

--- aeron-driver/src/main/c/aeron_driver_context.h
+++ aeron-driver/src/main/c/aeron_driver_context.h
@@ -30,7 +30,7 @@
 
 #define AERON_CNC_FILE "cnc.dat"
 #define AERON_LOSS_REPORT_FILE "loss-report.dat"
-#define AERON_CNC_VERSION (14)
+#define AERON_CNC_VERSION (15)
 
 #pragma pack(push)
 #pragma pack(4)

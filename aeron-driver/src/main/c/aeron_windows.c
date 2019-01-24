7ec1035223c0f4dcc38e98bf12e5ffcdd952576a
Todd L. Montgomery <tmont@nard.net>
2019-01-24 15:44:33 -0800

    [C]: tidy up.

--- aeron-driver/src/main/c/aeron_windows.c
+++ aeron-driver/src/main/c/aeron_windows.c
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014-2019 Real Logic Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include "util/aeron_platform.h"
 
 #if defined(AERON_COMPILER_MSVC)
@@ -166,7 +181,8 @@ aeron_uint128_t aeron_ipv6_netmask_from_prefixlen(size_t prefixlen)
     }
     else
     {
-        netmask = aeron_uint128_bitwise_negate(aeron_uint128_sub(aeron_uint128_bitwise_shift_left(make_aeron_uint128_t(1), (128 - prefixlen)), make_aeron_uint128_t(1)));
+        netmask = aeron_uint128_bitwise_negate(aeron_uint128_sub(aeron_uint128_bitwise_shift_left(
+            make_aeron_uint128_t(1), (128 - prefixlen)), make_aeron_uint128_t(1)));
     }
 
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
@@ -258,5 +274,20 @@ int aeron_clock_gettime_realtime(struct timespec *tv)
 
     return 0;
 }
+#else
+
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-function"
+#endif
+
+static int aeron_dummy_func()
+{
+    return 0;
+}
+
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#endif
 
 #endif

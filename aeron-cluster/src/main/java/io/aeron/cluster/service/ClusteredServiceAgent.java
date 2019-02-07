05cb0b20a3c2b4d0d2c70d7e3af0f1ec52d96a20
mjpt777 <mjpt777@gmail.com>
2019-02-07 19:13:50 +0000

    [Java] Use only service name in cluster service thread name.

--- aeron-cluster/src/main/java/io/aeron/cluster/service/ClusteredServiceAgent.java
+++ aeron-cluster/src/main/java/io/aeron/cluster/service/ClusteredServiceAgent.java
@@ -169,7 +169,7 @@ class ClusteredServiceAgent implements Agent, Cluster
 
     public String roleName()
     {
-        return ctx.serviceName() + "_" + memberId;
+        return ctx.serviceName();
     }
 
     public Cluster.Role role()

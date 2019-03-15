1f403abf22576649ec2c05f7fa3ad8702d2b30b5
Todd L. Montgomery <tmont@nard.net>
2019-03-15 13:01:10 -0700

    [C++]: add ControlResponseAdapter.

--- aeron-archive/src/main/cpp/client/ControlResponseAdapter.cpp
+++ aeron-archive/src/main/cpp/client/ControlResponseAdapter.cpp
@@ -14,17 +14,16 @@
  * limitations under the License.
  */
 
-#include "RecordingEventsAdapter.h"
+#include "ControlResponseAdapter.h"
 #include "ArchiveException.h"
 #include "aeron_archive_client/MessageHeader.h"
-#include "aeron_archive_client/RecordingStarted.h"
-#include "aeron_archive_client/RecordingProgress.h"
-#include "aeron_archive_client/RecordingStopped.h"
+#include "aeron_archive_client/ControlResponse.h"
+#include "aeron_archive_client/RecordingDescriptor.h"
 
 using namespace aeron;
 using namespace aeron::archive::client;
 
-static aeron::fragment_handler_t fragmentHandler(RecordingEventsAdapter& poller)
+static aeron::fragment_handler_t fragmentHandler(ControlResponseAdapter& poller)
 {
     return [&](AtomicBuffer& buffer, util::index_t offset, util::index_t length, Header& header)
     {
@@ -32,23 +31,21 @@ static aeron::fragment_handler_t fragmentHandler(RecordingEventsAdapter& poller)
     };
 }
 
-RecordingEventsAdapter::RecordingEventsAdapter(
-    const on_recording_start_t& onStart,
-    const on_recording_event_t& onProgress,
-    const on_recording_event_t& onStop,
+ControlResponseAdapter::ControlResponseAdapter(
+    const on_control_response_t& onResponse,
+    const recording_descriptor_consumer_t& onRecordingDescriptor,
     std::shared_ptr<aeron::Subscription> subscription,
     int fragmentLimit)
     :
     m_fragmentHandler(fragmentHandler(*this)),
     m_subscription(std::move(subscription)),
-    m_onStart(onStart),
-    m_onProgress(onProgress),
-    m_onStop(onStop),
+    m_onResponse(onResponse),
+    m_onRecordingDescriptor(onRecordingDescriptor),
     m_fragmentLimit(fragmentLimit)
 {
 }
 
-void RecordingEventsAdapter::onFragment(
+void ControlResponseAdapter::onFragment(
     AtomicBuffer& buffer, util::index_t offset, util::index_t length, Header& header)
 {
     MessageHeader msgHeader(
@@ -68,53 +65,50 @@ void RecordingEventsAdapter::onFragment(
     const std::uint16_t templateId = msgHeader.templateId();
     switch (templateId)
     {
-        case RecordingStarted::sbeTemplateId():
+        case ControlResponse::sbeTemplateId():
         {
-            RecordingStarted event(
+            ControlResponse response(
                 buffer.sbeData() + offset + MessageHeader::encodedLength(),
                 static_cast<std::uint64_t>(length) - MessageHeader::encodedLength(),
                 msgHeader.blockLength(),
                 msgHeader.version());
 
-            m_onStart(
-                event.recordingId(),
-                event.startPosition(),
-                event.sessionId(),
-                event.streamId(),
-                event.channel(),
-                event.sourceIdentity());
+            m_onResponse(
+                response.controlSessionId(),
+                response.correlationId(),
+                response.relevantId(),
+                response.code(),
+                response.errorMessage());
+            break;
         }
-        break;
 
-        case RecordingProgress::sbeTemplateId():
+        case RecordingDescriptor::sbeTemplateId():
         {
-            RecordingProgress event(
+            RecordingDescriptor descriptor(
                 buffer.sbeData() + offset + MessageHeader::encodedLength(),
                 static_cast<std::uint64_t>(length) - MessageHeader::encodedLength(),
                 msgHeader.blockLength(),
                 msgHeader.version());
 
-            m_onProgress(
-                event.recordingId(),
-                event.startPosition(),
-                event.position());
+            m_onRecordingDescriptor(
+                descriptor.controlSessionId(),
+                descriptor.correlationId(),
+                descriptor.recordingId(),
+                descriptor.startTimestamp(),
+                descriptor.stopTimestamp(),
+                descriptor.startPosition(),
+                descriptor.stopPosition(),
+                descriptor.initialTermId(),
+                descriptor.segmentFileLength(),
+                descriptor.termBufferLength(),
+                descriptor.mtuLength(),
+                descriptor.sessionId(),
+                descriptor.streamId(),
+                descriptor.strippedChannel(),
+                descriptor.originalChannel(),
+                descriptor.sourceIdentity());
+            break;
         }
-        break;
-
-        case RecordingStopped::sbeTemplateId():
-        {
-            RecordingStopped event(
-                buffer.sbeData() + offset + MessageHeader::encodedLength(),
-                static_cast<std::uint64_t>(length) - MessageHeader::encodedLength(),
-                msgHeader.blockLength(),
-                msgHeader.version());
-
-            m_onStop(
-                event.recordingId(),
-                event.startPosition(),
-                event.stopPosition());
-        }
-        break;
 
         default:
             break;
---
+++ aeron-archive/src/main/cpp/client/ControlResponseAdapter.h
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2014-2019 Real Logic Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef AERON_ARCHIVE_CONTROL_RESPONSE_ADAPTER_H
+#define AERON_ARCHIVE_CONTROL_RESPONSE_ADAPTER_H
+
+#include "Aeron.h"
+#include "RecordingDescriptorPoller.h"
+#include "RecordingEventsAdapter.h"
+
+namespace aeron {
+namespace archive {
+namespace client {
+
+/// Control Response Code of OK
+constexpr const std::int32_t CONTROL_RESPONSE_CODE_OK = 0;
+/// Control Response Code of ERROR
+constexpr const std::int32_t CONTROL_RESPONSE_CODE_ERROR = 1;
+/// Control Response Code of RECORDING_UNKNOWN
+constexpr const std::int32_t CONTROL_RESPONSE_CODE_RECORDING_UNKNOWN = 2;
+/// Control Response Code of SUBSCRIPTION_UNKNOWN
+constexpr const std::int32_t CONTROL_RESPONSE_CODE_SUBSCRIPTION_UNKNOWN = 3;
+
+/**
+ * An event has been received from the Archive in response to a request with a given correlation id.
+ *
+ * @param controlSessionId of the originating session.
+ * @param correlationId    of the associated request.
+ * @param relevantId       of the object to which the response applies.
+ * @param code             for the response status.
+ * @param errorMessage     when is set if the response code is not OK.
+ */
+typedef std::function<void(
+    std::int64_t controlSessionId,
+    std::int64_t correlationId,
+    std::int64_t relevantId,
+    std::int32_t code,
+    const std::string& errorMessage)> on_control_response_t;
+
+class ControlResponseAdapter
+{
+public:
+    /**
+     * Create an adapter for a given subscription to an archive for control response messages.
+     *
+     * @param onResponse    to which control responses are dispatched.
+     * @param onRecordingDescriptor to which recording descriptors are dispatched.
+     * @param subscription  to poll for new events.
+     * @param fragmentLimit to apply for each polling operation.
+     */
+    ControlResponseAdapter(
+        const on_control_response_t& onResponse,
+        const recording_descriptor_consumer_t& onRecordingDescriptor,
+        std::shared_ptr<Subscription> subscription,
+        int fragmentLimit = 10);
+
+    /**
+     * Get the Subscription used for polling messages.
+     *
+     * @return the Subscription used for polling messages.
+     */
+    inline std::shared_ptr<Subscription> subscription()
+    {
+        return m_subscription;
+    }
+
+    /**
+     * Poll for recording events and dispatch them to the callbacks for this instance.
+     *
+     * @return the number of fragments read during the operation. Zero if no events are available.
+     */
+    inline int poll()
+    {
+        return m_subscription->poll(m_fragmentHandler, m_fragmentLimit);
+    }
+
+    void onFragment(AtomicBuffer& buffer, util::index_t offset, util::index_t length, Header& header);
+
+private:
+    fragment_handler_t m_fragmentHandler;
+    std::shared_ptr<Subscription> m_subscription;
+    on_control_response_t m_onResponse;
+    recording_descriptor_consumer_t m_onRecordingDescriptor;
+    const int m_fragmentLimit;
+};
+
+}}}
+#endif //AERON_ARCHIVE_CONTROL_RESPONSE_ADAPTER_H

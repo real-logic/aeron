ceee0634b6dcc67d35b004f2a613eeb85195b28e
Marion Roche <marion@weareadaptive.com>
2019-08-20 17:00:51 -0400

    Add test for cluster not being able to recover when followers killed and client present

--- aeron-cluster/src/test/java/io/aeron/cluster/ResilienceTest.java
+++ aeron-cluster/src/test/java/io/aeron/cluster/ResilienceTest.java
@@ -15,6 +15,7 @@ import java.util.regex.Pattern;
 import org.agrona.concurrent.ShutdownSignalBarrier;
 import org.junit.Test;
 
+import io.aeron.cluster.service.Cluster;
 import io.aeron.driver.ext.LossGenerator;
 
 public class ResilienceTest
@@ -54,26 +55,23 @@ public class ResilienceTest
             cluster.connectClient();
             cluster.sendMessages(100);
 
-            Thread.sleep(5000);
+            Thread.sleep(10_00);
             assertNull("no election in progress", follower.electionState());
             assertNull("no election in progress", follower2.electionState());
 
             System.out.println("start loss");
             shouldDrop.set(true);
-            Thread.sleep(20000);
+            Thread.sleep(10_000);
 
             assertThat("loss between leader and follower - election state should be CANVASS",
                 follower.electionState(), is(Election.State.CANVASS));
             assertNull(follower2.electionState());
 
-            System.out.println("send more messages");
-            cluster.sendMessages(100);
-
-            Thread.sleep(20000);
+            Thread.sleep(10_000);
 
             System.out.println("end loss");
             shouldDrop.set(false);
-            Thread.sleep(20000);
+            Thread.sleep(15_000);
 
             assertNull("no election in progress", follower2.electionState());
             // this is the issue instead of -1 (null) election state is Election.State.FOLLOWER_CATCHUP_TRANSITION
@@ -83,6 +81,44 @@ public class ResilienceTest
 //            return;
             new ShutdownSignalBarrier().await();
 
+        }
+    }
+
+    @Test
+    public void shouldRecoverQuicklyAfterKillingFollowersThenRestarting() throws Exception
+    {
+        try (TestCluster cluster = TestCluster.startThreeNodeStaticCluster(-1))
+        {
+            cluster.awaitLeader();
+            final TestNode leader = cluster.findLeader();
+            final TestNode follower = cluster.followers().get(0);
+            final TestNode follower2 = cluster.followers().get(1);
+
+            // without client, it's ok, only 8 sec to get the leader...
+            cluster.connectClient();
+            cluster.sendMessages(10);
+
+            cluster.stopNode(follower);
+            cluster.stopNode(follower2);
+
+            while (leader.role() != Cluster.Role.FOLLOWER)
+            {
+                System.out.println("leader role " + leader.role());
+                Thread.sleep(5_000);
+            }
+
+            System.out.println("cluster is down");
+
+            System.out.println("restart 2nd follower");
+            cluster.startStaticNode(follower2.index(), true);
+
+            System.out.println("awaiting leader");
+            final long start = System.currentTimeMillis();
+            cluster.awaitLeader();
+            final long end = System.currentTimeMillis();
+            System.out.println("time to get leader again " + (end - start));
+
+            System.out.println("new leader " + cluster.findLeader().index());
         }
     }
 }

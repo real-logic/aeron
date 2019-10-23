c07ac5d89b48e0bdd50a511a875857e45c8fdb93
mjpt777 <mjpt777@gmail.com>
2019-10-23 17:55:42 +0100

    [Java] Network buffers only need alignment to cachelines.

--- aeron-driver/src/main/java/io/aeron/driver/media/ReceiveChannelEndpointThreadLocals.java
+++ aeron-driver/src/main/java/io/aeron/driver/media/ReceiveChannelEndpointThreadLocals.java
@@ -21,6 +21,7 @@ import io.aeron.protocol.NakFlyweight;
 import io.aeron.protocol.RttMeasurementFlyweight;
 import io.aeron.protocol.StatusMessageFlyweight;
 import org.agrona.BitUtil;
+import org.agrona.BufferUtil;
 
 import java.nio.ByteBuffer;
 import java.util.UUID;
@@ -54,7 +55,7 @@ public class ReceiveChannelEndpointThreadLocals
         final UUID uuid = UUID.randomUUID();
         receiverId = uuid.getMostSignificantBits() ^ uuid.getLeastSignificantBits();
 
-        final ByteBuffer byteBuffer = NetworkUtil.allocateDirectAlignedAndPadded(bufferLength, CACHE_LINE_LENGTH * 2);
+        final ByteBuffer byteBuffer = BufferUtil.allocateDirectAligned(bufferLength, CACHE_LINE_LENGTH);
 
         byteBuffer.limit(smLength);
         smBuffer = byteBuffer.slice();

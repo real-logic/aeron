5fd9276ecea1b49e959be8e1f33d3a69a930d6ea
Martin Thompson <mjpt777@gmail.com>
2019-11-27 10:07:08 +0000

    [Java] Fix Windows build by unmapping file used in test.

--- aeron-system-tests/src/test/java/io/aeron/test/LossReportTestUtil.java
+++ aeron-system-tests/src/test/java/io/aeron/test/LossReportTestUtil.java
@@ -1,7 +1,23 @@
+/*
+ * Copyright 2014-2019 Real Logic Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.aeron.test;
 
 import io.aeron.driver.reports.LossReportReader;
 import io.aeron.driver.reports.LossReportUtil;
+import org.agrona.IoUtil;
 import org.agrona.concurrent.AtomicBuffer;
 import org.agrona.concurrent.UnsafeBuffer;
 
@@ -19,25 +35,36 @@ import static org.mockito.Mockito.verify;
 
 public class LossReportTestUtil
 {
-    public static void verifyLossOccurredForStream(
-        final String aeronDirectoryName,
-        final int streamId) throws IOException
+    public static void verifyLossOccurredForStream(final String aeronDirectoryName, final int streamId)
+        throws IOException
     {
         final File lossReportFile = LossReportUtil.file(aeronDirectoryName);
         assertTrue(lossReportFile.exists());
 
-        try (
-            RandomAccessFile file = new RandomAccessFile(lossReportFile, "r");
+        MappedByteBuffer mappedByteBuffer = null;
+
+        try (RandomAccessFile file = new RandomAccessFile(lossReportFile, "r");
             FileChannel channel = file.getChannel())
         {
-            final MappedByteBuffer mappedByteBuffer = channel.map(READ_ONLY, 0, channel.size());
+            mappedByteBuffer = channel.map(READ_ONLY, 0, channel.size());
             final AtomicBuffer buffer = new UnsafeBuffer(mappedByteBuffer);
 
             final LossReportReader.EntryConsumer lossEntryConsumer = mock(LossReportReader.EntryConsumer.class);
             LossReportReader.read(buffer, lossEntryConsumer);
 
             verify(lossEntryConsumer).accept(
-                longThat(l -> l > 0), longThat(l -> l > 0), anyLong(), anyLong(), anyInt(), eq(streamId), any(), any());
+                longThat((l) -> l > 0),
+                longThat((l) -> l > 0),
+                anyLong(),
+                anyLong(),
+                anyInt(),
+                eq(streamId),
+                any(),
+                any());
+        }
+        finally
+        {
+            IoUtil.unmap(mappedByteBuffer);
         }
     }
 }

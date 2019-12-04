c84a5db2dc4eff972b5dd19d578f6c7c6613308b
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2019-12-04 12:31:11 +0100

    Fix `nakMulticastMaxBackoffNs()` to assign provided value

---
+++ aeron-driver/src/test/java/io/aeron/driver/MediaDriverContextTest.java
@@ -0,0 +1,41 @@
+package io.aeron.driver;
+
+import io.aeron.driver.MediaDriver.Context;
+import org.junit.Test;
+
+import static io.aeron.driver.Configuration.NAK_MAX_BACKOFF_DEFAULT_NS;
+import static io.aeron.driver.Configuration.NAK_MULTICAST_MAX_BACKOFF_PROP_NAME;
+import static org.junit.Assert.assertEquals;
+
+public class MediaDriverContextTest
+{
+    @Test
+    public void nakMulticastMaxBackoffNsDefaultValue()
+    {
+        final Context context = new Context();
+        assertEquals(NAK_MAX_BACKOFF_DEFAULT_NS, context.nakMulticastMaxBackoffNs());
+    }
+
+    @Test
+    public void nakMulticastMaxBackoffNsValueFromSystemProperty()
+    {
+        System.setProperty(NAK_MULTICAST_MAX_BACKOFF_PROP_NAME, "333");
+        try
+        {
+            final Context context = new Context();
+            assertEquals(333, context.nakMulticastMaxBackoffNs());
+        }
+        finally
+        {
+            System.clearProperty(NAK_MULTICAST_MAX_BACKOFF_PROP_NAME);
+        }
+    }
+
+    @Test
+    public void nakMulticastMaxBackoffNsExplicitValue()
+    {
+        final Context context = new Context();
+        context.nakMulticastMaxBackoffNs(Long.MIN_VALUE);
+        assertEquals(Long.MIN_VALUE, context.nakMulticastMaxBackoffNs());
+    }
+}

bea528cdc2f16b0e437c99f63c1e9a5c7855943b
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2019-12-05 23:16:17 +0100

    [Java] Show result code for unknown reason

--- aeron-samples/src/main/java/io/aeron/samples/BasicSubscriber.java
+++ aeron-samples/src/main/java/io/aeron/samples/BasicSubscriber.java
@@ -34,7 +34,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * This application only handles non-fragmented data. A DataHandler method is called
  * for every received message or message fragment.
  * For an example that implements reassembly of large, fragmented messages, see
- * {link@ MultipleSubscribersWithFragmentAssembly}.
+ * {@link MultipleSubscribersWithFragmentAssembly}.
  */
 public class BasicSubscriber
 {

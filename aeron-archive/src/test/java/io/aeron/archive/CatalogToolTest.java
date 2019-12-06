6bb545cb9a8496a95aea59763f2198bbbe0dd2a6
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2019-12-06 18:24:53 +0100

    [Java] Add JUnit 5 platform
    Configure Gradle build to support both JUnit 5 and JUnit 4 tests.

---
+++ aeron-archive/src/test/java/io/aeron/archive/CatalogToolTest.java
@@ -0,0 +1,14 @@
+package io.aeron.archive;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CatalogToolTest
+{
+    @Test
+    void foo()
+    {
+        assertTrue(true);
+    }
+}

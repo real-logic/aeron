d1a15ffba1ea29aacd4736bdae5884210eacf555
Todd L. Montgomery <tmont@nard.net>
2020-01-11 09:32:11 -0800

    [C]: adjust buffer length to accomodate max sizes. Remove info arg from gradle to lower noise level of build.

--- aeron-driver/src/main/c/agent/aeron_driver_agent.c
+++ aeron-driver/src/main/c/agent/aeron_driver_agent.c
@@ -437,7 +437,7 @@ static const char *dissect_msg_type_id(int32_t id)
 
 static const char *dissect_timestamp(int64_t time_ms)
 {
-    static char buffer[256];
+    static char buffer[80];
 
     snprintf(buffer, sizeof(buffer) - 1, "%" PRId64 ".%" PRId64, time_ms / 1000, time_ms % 1000);
     return buffer;

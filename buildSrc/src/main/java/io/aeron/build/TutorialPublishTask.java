24e808d974e1d53d2e07196e3fc37bfb90ded61a
Michael Barker <mikeb01@gmail.com>
2020-02-04 11:09:06 +1300

    [Java] Derive the wiki location from the remote origin uri.

--- buildSrc/src/main/java/io/aeron/build/TutorialPublishTask.java
+++ buildSrc/src/main/java/io/aeron/build/TutorialPublishTask.java
@@ -1,15 +1,23 @@
 package io.aeron.build;
 
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.transport.CredentialsProvider;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.TaskAction;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+
+import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_KEY_URL;
+import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_REMOTE_SECTION;
 
 public class TutorialPublishTask extends DefaultTask
 {
@@ -25,13 +33,13 @@ public class TutorialPublishTask extends DefaultTask
     @TaskAction
     public void publish() throws Exception
     {
+        final String wikiUri = getWikiUri();
         final File directory = new File(getProject().getBuildDir(), "tmp/tutorialPublish");
-
         // Use Personal Access Token or GITHUB_TOKEN for workflows
-        UsernamePasswordCredentialsProvider credentialsProvider = new UsernamePasswordCredentialsProvider(apiKey, "");
+        final CredentialsProvider credentialsProvider = new UsernamePasswordCredentialsProvider(apiKey, "");
 
         Git git = Git.cloneRepository()
-            .setURI("https://github.com/mikeb01/Aeron.wiki.git")
+            .setURI(wikiUri)
             .setCredentialsProvider(credentialsProvider)
             .setDirectory(directory)
             .call();
@@ -43,6 +51,31 @@ public class TutorialPublishTask extends DefaultTask
 
         git.add().addFilepattern(".").setUpdate(true).call();
         git.commit().setMessage("Update Docs").call();
+
+        System.out.println("Publishing to: " + wikiUri);
+
         git.push().setCredentialsProvider(credentialsProvider).call();
+    }
+
+    public String getWikiUri() throws IOException, URISyntaxException
+    {
+        final File baseGitDir = new File(getProject().getRootDir(), ".git");
+        if (!baseGitDir.exists() || !baseGitDir.isDirectory())
+        {
+            throw new IllegalStateException("Unable to find valid git repository at: " + baseGitDir);
+        }
+
+        final Repository baseGitRepo = new FileRepositoryBuilder()
+            .setGitDir(new File(getProject().getRootDir(), ".git"))
+            .build();
+
+        final String origin = baseGitRepo.getConfig().getString(CONFIG_REMOTE_SECTION, "origin", CONFIG_KEY_URL);
+
+        if (null == origin)
+        {
+            throw new IllegalStateException("Unable to find origin URI");
+        }
+
+        return GithubUtil.getWikiUriFromOriginUri(origin);
     }
 }

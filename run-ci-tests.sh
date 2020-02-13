cc994f0b8306c7ae710eacd3575b22725db1b7bd
Michael Barker <mikeb01@gmail.com>
2020-02-13 15:02:17 +1300

    [Java] Allow commit tests to be triggered by a repository dispatch event.  Script to trigger the repository dispatch events.

---
+++ run-ci-tests.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+if [ -z "$AERON_GITHUB_PAT" ]
+then
+  echo "Please set AERON_GITHUB_PAT environment variable to contain your token"
+  exit -1
+fi
+
+event_type=run-tests
+
+for option in "$@"
+do
+  case ${option} in
+    -s|--slow)
+      event_type=run-slow-tests
+      shift
+      ;;
+    -c|--commit)
+      shift
+      ;;
+    *)
+      echo "$0 [-s|--slow-tests] (run slow tests) [-c|--commit] (run commit tests) default: commit tests"
+      exit
+      ;;
+  esac
+done
+
+echo "Sending repository_dispatch, event_type: ${event_type}"
+
+curl -H "Accept: application/vnd.github.everest-preview+json" \
+    -H "Authorization: token ${AERON_GITHUB_PAT}" \
+    --request POST \
+    --data '{"event_type": "${event_type}"}' \
+    https://api.github.com/repos/real-logic/aeron/dispatches

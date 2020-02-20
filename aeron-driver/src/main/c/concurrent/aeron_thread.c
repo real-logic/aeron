58645ac34d449c96a32fdab2151ed9eceb5cee91
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2020-02-20 16:52:36 +0100

    [C] Fix thread handling on Windows (#864)
    - A function with the wrong signature was being passed to CreateThread
     - The thread handle was never closed
     - Failure to start a thread was not detected

--- aeron-driver/src/main/c/concurrent/aeron_thread.c
+++ aeron-driver/src/main/c/concurrent/aeron_thread.c
@@ -14,6 +14,11 @@
  * limitations under the License.
  */
 
+#if defined(__linux__)
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+#endif
+
 #include "concurrent/aeron_thread.h"
 
 void aeron_nano_sleep(uint64_t nanoseconds)
@@ -42,6 +47,16 @@ void aeron_nano_sleep(uint64_t nanoseconds)
 }
 
 #if defined(AERON_COMPILER_GCC)
+
+void aeron_thread_set_name(const char* role_name)
+{
+#if defined(Darwin)
+    pthread_setname_np(role_name);
+#else
+    pthread_setname_np(pthread_self(), role_name);
+#endif
+}
+
 #elif defined(AERON_COMPILER_MSVC) && defined(AERON_CPU_X64)
 
 static BOOL aeron_thread_once_callback(PINIT_ONCE init_once, void (*callback)(void), void** context)
@@ -75,38 +90,62 @@ int aeron_thread_attr_init(pthread_attr_t* attr)
     return 0;
 }
 
-int aeron_thread_create(aeron_thread_t* thread, void* attr, void*(*callback)(void*), void* arg0)
+static DWORD WINAPI aeron_thread_proc(LPVOID parameter)
 {
-    DWORD id;
-    *thread = CreateThread(
-        NULL,              // default security attributes
-        0,                 // use default stack size
-        callback,          // thread function name
-        arg0,              // argument to thread function
-        0,                 // use default creation flags
-        &id);              // returns the thread identifier
-
+    aeron_thread_t* thread = (aeron_thread_t*)parameter;
+    thread->result = thread->callback(thread->arg0);
     return 0;
 }
 
-void aeron_thread_set_name(aeron_thread_t self, const char* role_name)
+int aeron_thread_create(aeron_thread_t* thread, void* attr, void*(*callback)(void*), void* arg0)
+{
+    thread->callback = callback;
+    thread->arg0 = arg0;
+	
+    DWORD id;
+    thread->handle = CreateThread(
+        NULL,              // default security attributes
+        0,                 // use default stack size
+        aeron_thread_proc, // thread function name
+        thread,            // argument to thread function
+        0,                 // use default creation flags
+        &id);              // returns the thread identifier
+
+    return thread->handle ? 0 : -1;
+}
+
+void aeron_thread_set_name(const char* role_name)
 {
     size_t wn = mbstowcs(NULL, role_name, 0);
     wchar_t * buf = malloc(sizeof(wchar_t) * (wn + 1));  // value-initialize to 0 (see below)
+    if (!buf)
+    {
+        return;
+    }
+	
     mbstowcs(buf, role_name, wn + 1);
-    SetThreadDescription(self, buf);
+    SetThreadDescription(GetCurrentThread(), buf);
 
     free(buf);
 }
 
-aeron_thread_t aeron_thread_self()
+int aeron_thread_join(aeron_thread_t thread, void **value_ptr)
 {
-    return GetCurrentThread();
-}
+    if (thread.handle)
+    {
+        WaitForSingleObject(thread.handle, INFINITE);
+        CloseHandle(thread.handle);
+    }
+    else
+    {
+        return EINVAL;
+    }
 
-DWORD aeron_thread_join(aeron_thread_t thread, void **value_ptr)
-{
-    WaitForSingleObject(thread, INFINITE);
+    if (value_ptr)
+    {
+        *value_ptr = thread.result;
+    }
+
     return 0;
 }
 

543cea978de071ac19c6b98ff1fded58bfa0eefa
Todd L. Montgomery <tmont@nard.net>
2020-02-23 22:42:05 -0800

    [Java]: add check to avoid trying to re-resolve dynamic DMC.

--- aeron-driver/src/main/java/io/aeron/driver/media/SendChannelEndpoint.java
+++ aeron-driver/src/main/java/io/aeron/driver/media/SendChannelEndpoint.java
@@ -225,7 +225,9 @@ public class SendChannelEndpoint extends UdpChannelTransport
         {
             multiSndDestination.checkForReResolution(this, nowNs, conductorProxy);
         }
-        else if (!udpChannel.isMulticast() && nowNs > (timeOfLastSmNs + DESTINATION_TIMEOUT))
+        else if (!udpChannel.isMulticast() &&
+            !udpChannel.isDynamicControlMode() &&
+            nowNs > (timeOfLastSmNs + DESTINATION_TIMEOUT))
         {
             final String endpoint = udpChannel.channelUri().get(CommonContext.ENDPOINT_PARAM_NAME);
             final InetSocketAddress address = udpChannel.remoteData();

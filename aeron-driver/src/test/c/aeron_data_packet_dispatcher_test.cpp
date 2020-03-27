0026f98b420add252732ef1f7fdf940346dfeb24
Michael Barker <mikeb01@gmail.com>
2020-03-27 14:10:11 +1300

    [C] Add stub transport bindings.  Add test that setup messages will be elicited.

--- aeron-driver/src/test/c/aeron_data_packet_dispatcher_test.cpp
+++ aeron-driver/src/test/c/aeron_data_packet_dispatcher_test.cpp
@@ -26,6 +26,7 @@ extern "C"
 #include <aeron_data_packet_dispatcher.h>
 #include <aeron_driver_receiver.h>
 #include <aeron_position.h>
+#include "aeron_test_udp_bindings.h"
 
 int aeron_driver_ensure_dir_is_recreated(aeron_driver_context_t *context);
 }
@@ -50,10 +51,14 @@ public:
 protected:
     virtual void SetUp()
     {
+        aeron_test_udp_bindings_load(&m_transport_bindings);
+
         aeron_driver_context_init(&m_context);
         aeron_driver_context_set_dir_delete_on_start(m_context, true);
         aeron_driver_context_set_congestioncontrol_supplier(
             m_context, aeron_static_window_congestion_control_strategy_supplier);
+        aeron_driver_context_set_udp_channel_transport_bindings(m_context, &m_transport_bindings);
+
         aeron_driver_ensure_dir_is_recreated(m_context);
 
         aeron_mpsc_concurrent_array_queue_init(&m_conductor_command_queue, 1024);
@@ -61,6 +66,8 @@ protected:
         m_conductor_proxy.threading_mode = AERON_THREADING_MODE_INVOKER;
         m_conductor_proxy.fail_counter = &m_conductor_fail_counter;
         m_conductor_proxy.conductor = NULL;
+
+        m_context->conductor_proxy = &m_conductor_proxy;
 
         aeron_counters_manager_init(
             &m_counters_manager,
@@ -73,13 +80,31 @@ protected:
         aeron_distinct_error_log_init(
             &m_error_log, m_error_log_buffer.data(), m_error_log_buffer.size(), aeron_epoch_clock, stub_linger, NULL);
         aeron_driver_receiver_init(&m_receiver, m_context, &m_system_counters, &m_error_log);
-        aeron_data_packet_dispatcher_init(&m_dispatcher, &m_conductor_proxy, &m_receiver);
+
+        m_receiver_proxy.receiver = &m_receiver;
+        m_context->receiver_proxy = &m_receiver_proxy;
+
+        aeron_name_resolver_t resolver;
+        aeron_default_name_resolver_supplier(&resolver, NULL, NULL);
+
+        const char *uri = "aeron:udp?endpoint=localhost:9090";
+        aeron_udp_channel_parse(strlen(uri), uri, &resolver, &m_channel);
+
+        aeron_atomic_counter_t status_indicator;
+        status_indicator.counter_id = aeron_counter_receive_channel_status_allocate(
+            &m_counters_manager, m_channel->uri_length, m_channel->original_uri);
+        status_indicator.value_addr = aeron_counter_addr(&m_counters_manager, status_indicator.counter_id);
+
+        aeron_receive_channel_endpoint_create(
+            &m_receive_endpoint, m_channel, &status_indicator, &m_system_counters, m_context);
+
+        m_dispatcher = &m_receive_endpoint->dispatcher;
 
         // This is slight hack as we are using the static window and fixing the term buffer size
         // we can share the congestion control algorithm.
         m_context->congestion_control_supplier_func(
             &m_congestion_control_strategy, 0, 0, 0, 0, 0, TERM_BUFFER_SIZE, MTU,
-            &m_control_address, &m_source_address, m_context, &m_counters_manager);
+            &m_channel->remote_control, &m_channel->remote_data, m_context, &m_counters_manager);
     };
 
     virtual void TearDown()
@@ -88,7 +113,7 @@ protected:
         {
             aeron_publication_image_close(&m_counters_manager, image);
         }
-        aeron_data_packet_dispatcher_close(&m_dispatcher);
+        aeron_receive_channel_endpoint_delete(&m_counters_manager, m_receive_endpoint);
         aeron_driver_receiver_on_close(&m_receiver);
         aeron_distinct_error_log_close(&m_error_log);
     }
@@ -107,11 +132,10 @@ protected:
             &m_counters_manager, 0, session_id, stream_id, strlen("foo"), "foo");
         hwm_position.value_addr = aeron_counter_addr(&m_counters_manager, hwm_position.counter_id);
 
-
         if (aeron_publication_image_create(
             &image, m_receive_endpoint, m_context, 0, session_id, stream_id, 0, 0, 0,
             &hwm_position, &pos_position, m_congestion_control_strategy,
-            &m_control_address, &m_source_address,
+            &m_channel->remote_control, &m_channel->local_data,
             TERM_BUFFER_SIZE, MTU, NULL, true, true, false, &m_system_counters) < 0)
         {
             return NULL;
@@ -125,16 +149,17 @@ protected:
     aeron_driver_context_t *m_context;
     aeron_receive_channel_endpoint_t *m_receive_endpoint;
     aeron_congestion_control_strategy_t *m_congestion_control_strategy;
-    aeron_data_packet_dispatcher_t m_dispatcher;
+    aeron_data_packet_dispatcher_t *m_dispatcher;
     aeron_driver_conductor_proxy_t m_conductor_proxy;
+    aeron_driver_receiver_proxy_t m_receiver_proxy;
     aeron_mpsc_concurrent_array_queue_t m_conductor_command_queue;
     aeron_driver_receiver_t m_receiver;
     aeron_distinct_error_log_t m_error_log;
     aeron_counters_manager_t m_counters_manager;
     aeron_system_counters_t m_system_counters;
     int64_t m_conductor_fail_counter;
-    struct sockaddr_storage m_control_address;
-    struct sockaddr_storage m_source_address;
+    aeron_udp_channel_transport_bindings_t m_transport_bindings;
+    aeron_udp_channel_t *m_channel;
     AERON_DECL_ALIGNED(buffer_t m_error_log_buffer, 16);
     AERON_DECL_ALIGNED(buffer_t m_counter_value_buffer, 16);
     AERON_DECL_ALIGNED(buffer_2x_t m_counter_meta_buffer, 16);
@@ -158,14 +183,14 @@ TEST_F(DataPacketDispatcherTest, shouldInsertDataInputSubscribedPublicationImage
     data_header->term_offset = 0;
     size_t len = sizeof(aeron_data_header_t) + 8;
 
-    ASSERT_EQ(0, aeron_data_packet_dispatcher_add_subscription(&m_dispatcher, stream_id));
-    ASSERT_EQ(0, aeron_data_packet_dispatcher_add_publication_image(&m_dispatcher, image));
+    ASSERT_EQ(0, aeron_data_packet_dispatcher_add_subscription(m_dispatcher, stream_id));
+    ASSERT_EQ(0, aeron_data_packet_dispatcher_add_publication_image(m_dispatcher, image));
 
     int64_t position_before_data = *image->rcv_hwm_position.value_addr;
     int64_t expected_position_after_data = position_before_data + (int64_t)len;
 
     int bytes_written = aeron_data_packet_dispatcher_on_data(
-        &m_dispatcher, NULL, data_header, data_buffer.data(), len, &m_source_address);
+        m_dispatcher, m_receive_endpoint, data_header, data_buffer.data(), len, &m_channel->local_data);
 
     ASSERT_EQ((int)len, bytes_written);
     ASSERT_EQ(expected_position_after_data, *image->rcv_hwm_position.value_addr);
@@ -189,15 +214,47 @@ TEST_F(DataPacketDispatcherTest, shouldNotInsertDataInputWithNoSubscription)
     size_t len = sizeof(aeron_data_header_t) + 8;
 
     // No subscription made...
-    ASSERT_EQ(0, aeron_data_packet_dispatcher_add_publication_image(&m_dispatcher, image));
+    ASSERT_EQ(0, aeron_data_packet_dispatcher_add_publication_image(m_dispatcher, image));
 
     int64_t position_before_data = *image->rcv_hwm_position.value_addr;
     int64_t expected_position_after_data = position_before_data;
 
     int bytes_written = aeron_data_packet_dispatcher_on_data(
-        &m_dispatcher, NULL, data_header, data_buffer.data(), len, &m_source_address);
+        m_dispatcher, m_receive_endpoint, data_header, data_buffer.data(), len, &m_channel->local_data);
 
     ASSERT_EQ(0, bytes_written);
     ASSERT_EQ(expected_position_after_data, *image->rcv_hwm_position.value_addr);
 }
+
+TEST_F(DataPacketDispatcherTest, shouldElicitSetupMessageForSubscriptionWithoutImage)
+{
+    AERON_DECL_ALIGNED(buffer_t data_buffer, 16);
+
+    int32_t session_id = 123123;
+    int32_t stream_id = 434523;
+
+    aeron_data_header_t *data_header = (aeron_data_header_t *)data_buffer.data();
+    data_header->stream_id = stream_id;
+    data_header->session_id = session_id;
+    data_header->term_id = 0;
+    data_header->term_offset = 0;
+    size_t len = sizeof(aeron_data_header_t) + 8;
+
+    // No publicaction added...
+    ASSERT_EQ(0, aeron_data_packet_dispatcher_add_subscription(m_dispatcher, stream_id));
+
+    aeron_test_udp_bindings_state_t *state = static_cast<aeron_test_udp_bindings_state_t *>(
+        m_receive_endpoint->transport.bindings_clientd);
+    int64_t initial_count = state->sm_count;
+
+    ASSERT_EQ(0, aeron_data_packet_dispatcher_on_data(
+        m_dispatcher, m_receive_endpoint, data_header, data_buffer.data(), len, &m_channel->local_data));
+    ASSERT_EQ(0, aeron_data_packet_dispatcher_on_data(
+        m_dispatcher, m_receive_endpoint, data_header, data_buffer.data(), len, &m_channel->local_data));
+    ASSERT_EQ(0, aeron_data_packet_dispatcher_on_data(
+        m_dispatcher, m_receive_endpoint, data_header, data_buffer.data(), len, &m_channel->local_data));
+
+    ASSERT_EQ(initial_count + 1, state->sm_count);
+}
+
 

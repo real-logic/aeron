4f9254357f4e98fdce0a4d0ab4b6afdaf2b8dd5b
mjpt777 <mjpt777@gmail.com>
2020-04-20 14:00:59 +0100

    [C] Support subset of Java driver idle strategy alias names.

--- aeron-client/src/main/c/aeron_agent.c
+++ aeron-client/src/main/c/aeron_agent.c
@@ -305,15 +305,18 @@ aeron_idle_strategy_func_t aeron_idle_strategy_load(
 
     *idle_strategy_state = NULL;
 
-    if (strncmp(idle_strategy_name, "sleeping", sizeof("sleeping")) == 0)
+    if (strncmp(idle_strategy_name, "sleep-ns", sizeof("sleep-ns")) == 0 ||
+        strncmp(idle_strategy_name, "sleeping", sizeof("sleeping")) == 0)
     {
         return aeron_idle_strategy_load("aeron_idle_strategy_sleeping", idle_strategy_state, env_var, init_args);
     }
-    else if (strncmp(idle_strategy_name, "yielding", sizeof("yielding")) == 0)
+    else if (strncmp(idle_strategy_name, "yield", sizeof("yield")) == 0 ||
+        strncmp(idle_strategy_name, "yielding", sizeof("yielding")) == 0)
     {
         return aeron_idle_strategy_load("aeron_idle_strategy_yielding", idle_strategy_state, env_var, init_args);
     }
-    else if (strncmp(idle_strategy_name, "spinning", sizeof("spinning")) == 0)
+    else if (strncmp(idle_strategy_name, "spin", sizeof("spin")) == 0 ||
+        strncmp(idle_strategy_name, "spinning", sizeof("spinning")) == 0)
     {
         return aeron_idle_strategy_load("aeron_idle_strategy_busy_spinning", idle_strategy_state, env_var, init_args);
     }

ddcdb2e389e098bcd8f0ef006732a77a0ae0efa4
Yonggang Luo <luoyonggang@gmail.com>
2020-05-07 21:49:12 +0800

    Getting cppbuild-vs.cmd to be consistence with cppbuild-vs. Getting java_system_tests_c_media_driver and java_slow_system_tests_c_media_driver works under win32

--- cppbuild/cppbuild-vs.cmd
+++ cppbuild/cppbuild-vs.cmd
@@ -5,36 +5,59 @@ Setlocal EnableDelayedExpansion
 cd /d "%~dp0.."
 set SOURCE_DIR=%CD%
 set BUILD_DIR=%CD%\cppbuild\Release
-set EXTRA_CMAKE_ARGS=-DAERON_SYSTEM_TESTS=OFF
+set BUILD_CONFIG=Release
+set "EXTRA_CMAKE_ARGS="
 
 for %%o in (%*) do (
 
     set PROCESSED=0
 
     if "%%o"=="--help" (
-	    echo cppbuild-vs.cmd [--c-warnings-as-errors] [--cxx-warnings-as-errors] [--build-aeron-driver]
-		exit /b
-	)
+        echo cppbuild-vs.cmd [--c-warnings-as-errors] [--cxx-warnings-as-errors] [--build-aeron-driver]
+        exit /b
+    )
 
-	if "%%o"=="--c-warnings-as-errors" (
-	    set EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DC_WARNINGS_AS_ERRORS=ON
-		set PROCESSED=1
-	)
+    if "%%o"=="--c-warnings-as-errors" (
+        set EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DC_WARNINGS_AS_ERRORS=ON
+        set PROCESSED=1
+    )
 
     if "%%o"=="--cxx-warnings-as-errors" (
-	    set EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DCXX_WARNINGS_AS_ERRORS=ON
-		set PROCESSED=1
+        set EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DCXX_WARNINGS_AS_ERRORS=ON
+        set PROCESSED=1
+    )
+
+    if "%%o"=="--build-archive-api" (
+        set EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DBUILD_AERON_ARCHIVE_API=ON
+        set PROCESSED=1
+    )
+
+    if "%%o"=="--slow-system-tests" (
+        set EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DAERON_SLOW_SYSTEM_TESTS=ON
+        set PROCESSED=1
+    )
+
+    if "%%o"=="--debug-build" (
+        set EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DCMAKE_BUILD_TYPE=Debug
+        set BUILD_DIR=%CD%\cppbuild\Debug
+        set BUILD_CONFIG=Debug
+        set PROCESSED=1
     )
 
     if "%%o"=="--build-aeron-driver" (
         set EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DBUILD_AERON_DRIVER=ON
-	    set PROCESSED=1
-	)
+        set PROCESSED=1
+    )
 
-	if "%%o"=="--link-samples-client-shared" (
+    if "%%o"=="--link-samples-client-shared" (
         set EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DLINK_SAMPLES_CLIENT_SHARED=ON
-	    set PROCESSED=1
-	)
+        set PROCESSED=1
+    )
+
+    if "%%o"=="--no-system-tests" (
+        set EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DAERON_SYSTEM_TESTS=OFF
+        set PROCESSED=1
+    )
 )
 
 call cppbuild/vs-helper.cmd
@@ -47,8 +70,8 @@ pushd %BUILD_DIR%
 cmake %EXTRA_CMAKE_ARGS% %SOURCE_DIR%
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-cmake --build . --config Release
+cmake --build . --config %BUILD_CONFIG%
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-ctest -C Release --output-on-failure
+ctest -C %BUILD_CONFIG% --output-on-failure
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%

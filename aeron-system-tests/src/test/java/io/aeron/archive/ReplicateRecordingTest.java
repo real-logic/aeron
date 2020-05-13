f1965464a30c2f98572008aedb89752323cea1e4
mjpt777 <mjpt777@gmail.com>
2020-05-13 15:29:35 +0100

    [Java] Disable failing test on C driver again.

--- aeron-system-tests/src/test/java/io/aeron/archive/ReplicateRecordingTest.java
+++ aeron-system-tests/src/test/java/io/aeron/archive/ReplicateRecordingTest.java
@@ -255,6 +255,7 @@ public class ReplicateRecordingTest
     @Test
     public void shouldReplicateStoppedRecordingsConcurrently()
     {
+        TestMediaDriver.notSupportedOnCMediaDriver("Awaiting Bug Fix");
         final String messagePrefix = "Message-Prefix-";
         final int messageCount = 10;
         final long[] srcRecordingIds = new long[2];

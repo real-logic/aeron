2a71cc73b57779a28eec829eb3b103569b181e10
mjpt777 <mjpt777@gmail.com>
2020-05-20 14:49:00 +0100

    [Java] Reduce test waiting time.

--- aeron-cluster/src/test/java/io/aeron/cluster/TestCluster.java
+++ aeron-cluster/src/test/java/io/aeron/cluster/TestCluster.java
@@ -154,7 +154,7 @@ public class TestCluster implements AutoCloseable
             {
                 return clusterMembership;
             }
-            Tests.sleep(100);
+            Tests.sleep(10);
         }
     }
 

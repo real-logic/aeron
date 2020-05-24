09a7b9395e950999cb2779e40ba443c592f6dd7f
Todd L. Montgomery <tmont@nard.net>
2020-05-23 20:33:48 -0700

    [C]: add basic message offer/poll test

--- aeron-driver/src/test/c/aeron_c_system_test.cpp
+++ aeron-driver/src/test/c/aeron_c_system_test.cpp
@@ -30,6 +30,8 @@ using namespace aeron;
 class CSystemTest : public testing::Test
 {
 public:
+    using poll_handler_t = std::function<void(const uint8_t *, size_t, size_t, aeron_header_t *)>;
+
     CSystemTest()
     {
         m_driver.start();
@@ -70,7 +72,7 @@ public:
         return m_aeron;
     }
 
-    aeron_publication_t *awaitPublicationOrError(aeron_async_add_publication_t *async)
+    static aeron_publication_t *awaitPublicationOrError(aeron_async_add_publication_t *async)
     {
         aeron_publication_t *publication = nullptr;
 
@@ -87,7 +89,7 @@ public:
         return publication;
     }
 
-    aeron_exclusive_publication_t *awaitExclusivePublicationOrError(
+    static aeron_exclusive_publication_t *awaitExclusivePublicationOrError(
         aeron_async_add_exclusive_publication_t *async)
     {
         aeron_exclusive_publication_t *publication = nullptr;
@@ -105,7 +107,7 @@ public:
         return publication;
     }
 
-    aeron_subscription_t *awaitSubscriptionOrError(aeron_async_add_subscription_t *async)
+    static aeron_subscription_t *awaitSubscriptionOrError(aeron_async_add_subscription_t *async)
     {
         aeron_subscription_t *subscription = nullptr;
 
@@ -122,7 +124,7 @@ public:
         return subscription;
     }
 
-    aeron_counter_t *awaitCounterOrError(aeron_async_add_counter_t *async)
+    static aeron_counter_t *awaitCounterOrError(aeron_async_add_counter_t *async)
     {
         aeron_counter_t *counter = nullptr;
 
@@ -139,10 +141,33 @@ public:
         return counter;
     }
 
+    static void awaitConnected(aeron_subscription_t *subscription)
+    {
+        while (!aeron_subscription_is_connected(subscription))
+        {
+            std::this_thread::yield();
+        }
+    }
+
+    static void poll_handler(
+        void *clientd, const uint8_t *buffer, size_t offset, size_t length, aeron_header_t *header)
+    {
+        auto test = reinterpret_cast<CSystemTest *>(clientd);
+
+        test->m_poll_handler(buffer, offset, length, header);
+    }
+
+    int poll(aeron_subscription_t *subscription, poll_handler_t &handler, int fragment_limit)
+    {
+        m_poll_handler = handler;
+        return aeron_subscription_poll(subscription, poll_handler, this, fragment_limit);
+    }
+
 protected:
     EmbeddedMediaDriver m_driver;
     aeron_context_t *m_context = nullptr;
     aeron_t *m_aeron = nullptr;
+    poll_handler_t m_poll_handler = nullptr;
 };
 
 TEST_F(CSystemTest, shouldSpinUpDriverAndConnectSuccessfully)
@@ -235,10 +260,47 @@ TEST_F(CSystemTest, shouldAddPublicationAndSubscription)
 
     ASSERT_TRUE((subscription = awaitSubscriptionOrError(async_sub))) << aeron_errmsg();
 
-    while (!aeron_subscription_is_connected(subscription))
+    awaitConnected(subscription);
+
+    EXPECT_EQ(aeron_publication_close(publication), 0);
+    EXPECT_EQ(aeron_subscription_close(subscription), 0);
+}
+
+TEST_F(CSystemTest, shouldOfferAndPollOneMessage)
+{
+    aeron_async_add_publication_t *async_pub;
+    aeron_publication_t *publication;
+    aeron_async_add_subscription_t *async_sub;
+    aeron_subscription_t *subscription;
+    const char message[] = "message";
+
+    ASSERT_TRUE(connect());
+    ASSERT_EQ(aeron_async_add_publication(&async_pub, m_aeron, PUB_URI, STREAM_ID), 0);
+    ASSERT_TRUE((publication = awaitPublicationOrError(async_pub))) << aeron_errmsg();
+    ASSERT_EQ(aeron_async_add_subscription(
+        &async_sub, m_aeron, SUB_URI, STREAM_ID, nullptr, nullptr, nullptr, nullptr), 0);
+    ASSERT_TRUE((subscription = awaitSubscriptionOrError(async_sub))) << aeron_errmsg();
+    awaitConnected(subscription);
+
+    while (aeron_publication_offer(publication, (uint8_t *)message, strlen(message), nullptr, nullptr) < 0)
     {
         std::this_thread::yield();
     }
+
+    int poll_result;
+    bool called = false;
+    poll_handler_t handler = [&](const uint8_t *buffer, size_t offset, size_t length, aeron_header_t *header)
+    {
+        EXPECT_EQ(length, strlen(message));
+        called = true;
+    };
+
+    while ((poll_result = poll(subscription, handler, 1)) == 0)
+    {
+        std::this_thread::yield();
+    }
+    EXPECT_EQ(poll_result, 1) << aeron_errmsg();
+    EXPECT_TRUE(called);
 
     EXPECT_EQ(aeron_publication_close(publication), 0);
     EXPECT_EQ(aeron_subscription_close(subscription), 0);

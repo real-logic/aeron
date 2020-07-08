88954cf40c8c545451db7ef0cda0da0d4928d073
Todd L. Montgomery <tmont@nard.net>
2020-07-08 16:22:49 -0700

    [C]: fix guaranteed short counted sends on each SM sent.

--- aeron-driver/src/main/c/media/aeron_receive_channel_endpoint.c
+++ aeron-driver/src/main/c/media/aeron_receive_channel_endpoint.c
@@ -182,17 +182,18 @@ int aeron_receive_channel_endpoint_close(aeron_receive_channel_endpoint_t *endpo
 
 int aeron_receive_channel_endpoint_sendmsg(aeron_receive_channel_endpoint_t *endpoint, struct msghdr *msghdr)
 {
+    int min_bytes_sent = msghdr->msg_iov->iov_len;
+
     for (size_t i = 0, len = endpoint->destinations.length; i < len; i++)
     {
         aeron_receive_destination_t *destination = endpoint->destinations.array[i].destination;
+        const int sendmsg_result =
+            destination->data_paths->sendmsg_func(destination->data_paths, &destination->transport, msghdr);
 
-        if (destination->data_paths->sendmsg_func(destination->data_paths, &destination->transport, msghdr) < 0)
-        {
-            return -1;
-        }
+        min_bytes_sent = sendmsg_result < min_bytes_sent ? sendmsg_result : min_bytes_sent;
     }
 
-    return 0;
+    return min_bytes_sent;
 }
 
 int aeron_receive_channel_endpoint_send_sm(

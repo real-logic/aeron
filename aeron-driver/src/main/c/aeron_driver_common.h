a8a71c8b2b200109b8c220ae6aea44c35a2f7697
mjpt777 <mjpt777@gmail.com>
2020-07-08 15:00:52 +0100

    [C] Formatting.

--- aeron-driver/src/main/c/aeron_driver_common.h
+++ aeron-driver/src/main/c/aeron_driver_common.h
@@ -68,18 +68,18 @@ typedef struct aeron_tetherable_position_stct
 aeron_tetherable_position_t;
 
 typedef void (*aeron_untethered_subscription_state_change_func_t)(
-        aeron_tetherable_position_t *,
-        int64_t now_ns,
-        aeron_subscription_tether_state_t new_state,
-        int32_t stream_id,
-        int32_t session_id);
+    aeron_tetherable_position_t *tetherable_position,
+    int64_t now_ns,
+    aeron_subscription_tether_state_t new_state,
+    int32_t stream_id,
+    int32_t session_id);
 
-inline static void aeron_untethered_subscription_state_change(
-        aeron_tetherable_position_t *tetherable_position,
-        int64_t now_ns,
-        aeron_subscription_tether_state_t new_state,
-        int32_t stream_id,
-        int32_t session_id)
+inline void aeron_untethered_subscription_state_change(
+    aeron_tetherable_position_t *tetherable_position,
+    int64_t now_ns,
+    aeron_subscription_tether_state_t new_state,
+    int32_t stream_id,
+    int32_t session_id)
 {
     tetherable_position->state = new_state;
     tetherable_position->time_of_last_update_ns = now_ns;

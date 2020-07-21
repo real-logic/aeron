62d235d6b63da740a0b161e46070598e4da303da
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2020-07-21 23:58:38 +0300

    [C] Enable system tests on Windows and add missing build options.

--- cppbuild/cppbuild-vs.cmd
+++ cppbuild/cppbuild-vs.cmd
@@ -1,44 +1,54 @@
 @if "%DEBUG%" == "" @echo off
 setlocal EnableDelayedExpansion
 
-set SOURCE_DIR=%CD%
-set ZLIB_ZIP=%CD%\cppbuild\zlib1211.zip
-set BUILD_DIR=%CD%\cppbuild\Release
-set ZLIB_BUILD_DIR=%BUILD_DIR%\zlib-build
-set ZLIB_INSTALL_DIR=%BUILD_DIR%\zlib64
-set EXTRA_CMAKE_ARGS=-DAERON_SYSTEM_TESTS=OFF
+set "DIR=%~dp0"
+set "SOURCE_DIR=%DIR%\.."
+set "BUILD_DIR=%DIR%\Release"
+set "BUILD_CONFIG=Release"
+set "ZLIB_ZIP=%DIR%\zlib1211.zip"
+set "ZLIB_BUILD_DIR=%BUILD_DIR%\zlib-build"
+set "ZLIB_INSTALL_DIR=%BUILD_DIR%\zlib64"
+set "EXTRA_CMAKE_ARGS="
 
 for %%o in (%*) do (
 
-    set PROCESSED=0
-
     if "%%o"=="--help" (
-        echo cppbuild-vs.cmd [--c-warnings-as-errors] [--cxx-warnings-as-errors] [--build-aeron-driver]
+        echo %0 [--c-warnings-as-errors] [--cxx-warnings-as-errors] [--build-aeron-driver] [--link-samples-client-shared] [--build-archive-api] [--no-system-tests] [--debug-build] [--help]
         exit /b
     )
 
     if "%%o"=="--c-warnings-as-errors" (
-        set EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DC_WARNINGS_AS_ERRORS=ON
-        set PROCESSED=1
+        set "EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DC_WARNINGS_AS_ERRORS=ON"
     )
 
     if "%%o"=="--cxx-warnings-as-errors" (
-        set EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DCXX_WARNINGS_AS_ERRORS=ON
-        set PROCESSED=1
+        set "EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DCXX_WARNINGS_AS_ERRORS=ON"
     )
 
     if "%%o"=="--build-aeron-driver" (
         echo "Enabling building of aeron driver is now the default"
-        set PROCESSED=1
     )
 
     if "%%o"=="--link-samples-client-shared" (
-        set EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DLINK_SAMPLES_CLIENT_SHARED=ON
-        set PROCESSED=1
+        set "EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DLINK_SAMPLES_CLIENT_SHARED=ON"
+    )
+
+    if "%%o"=="--build-archive-api" (
+        set "EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DBUILD_AERON_ARCHIVE_API=ON"
+    )
+
+    if "%%o"=="--no-system-tests" (
+        set "EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DAERON_SYSTEM_TESTS=OFF"
+    )
+
+    if "%%o"=="--debug-build" (
+        set "EXTRA_CMAKE_ARGS=!EXTRA_CMAKE_ARGS! -DCMAKE_BUILD_TYPE=Debug"
+        set "BUILD_DIR=%DIR%\Debug"
+        set "BUILD_CONFIG=Debug"
     )
 )
 
-call cppbuild\vs-helper.cmd
+call %DIR%\vs-helper.cmd
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 if EXIST %BUILD_DIR% rd /S /Q %BUILD_DIR%
@@ -62,8 +72,8 @@ pushd %BUILD_DIR%
 cmake %EXTRA_CMAKE_ARGS% %SOURCE_DIR%
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-cmake --build . --config Release
+cmake --build . --config %BUILD_CONFIG%
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-ctest -C Release --output-on-failure
+ctest -C %BUILD_CONFIG% --output-on-failure
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%

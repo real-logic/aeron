ff9a89ef1e508791bc9a35dc1ccd81c79075d368
mjpt777 <mjpt777@gmail.com>
2020-08-05 15:59:58 +0100

    [Java] Naming.

--- aeron-system-tests/src/test/java/io/aeron/ResolvedEndpointSystemTest.java
+++ aeron-system-tests/src/test/java/io/aeron/ResolvedEndpointSystemTest.java
@@ -33,7 +33,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assumptions.assumeFalse;
 import static org.mockito.Mockito.mock;
 
-public class WildcardPortsSystemTest
+public class ResolvedEndpointSystemTest
 {
     private static final int STREAM_ID = 2002;
 
@@ -67,14 +67,14 @@ public class WildcardPortsSystemTest
 
     @Test
     @Timeout(5)
-    void shouldSubscribeToWildcardPorts()
+    void shouldSubscribeToSystemAssignedPorts()
     {
-        final String wildCardUri1 = "aeron:udp?endpoint=127.0.0.1:0|tags=1002";
-        final String wildCardUri2 = "aeron:udp?endpoint=127.0.0.1:0|tags=1003";
+        final String systemAssignedPortUri1 = "aeron:udp?endpoint=127.0.0.1:0|tags=1002";
+        final String systemAssignedPortUri2 = "aeron:udp?endpoint=127.0.0.1:0|tags=1003";
         final String tagged1 = "aeron:udp?tags=1002";
 
-        try (Subscription sub1 = client.addSubscription(wildCardUri1, STREAM_ID);
-            Subscription sub2 = client.addSubscription(wildCardUri2, STREAM_ID);
+        try (Subscription sub1 = client.addSubscription(systemAssignedPortUri1, STREAM_ID);
+            Subscription sub2 = client.addSubscription(systemAssignedPortUri2, STREAM_ID);
             Subscription sub3 = client.addSubscription(tagged1, STREAM_ID + 1))
         {
             List<String> bindAddressAndPort1;
@@ -100,7 +100,8 @@ public class WildcardPortsSystemTest
             assertEquals(bindAddressAndPort3, bindAddressAndPort1);
 
             final String pub2Uri = new ChannelUriStringBuilder()
-                .media("udp").endpoint(bindAddressAndPort1.get(0))
+                .media("udp")
+                .endpoint(bindAddressAndPort1.get(0))
                 .build();
 
             try (Publication publication = client.addPublication(pub2Uri, STREAM_ID))
@@ -120,14 +121,14 @@ public class WildcardPortsSystemTest
 
     @Test
     @Timeout(5)
-    void shouldSubscribeToWildcardPortsUsingIPv6()
+    void shouldSubscribeToSystemAssignedPortsUsingIPv6()
     {
         assumeFalse(Boolean.getBoolean("java.net.preferIPv4Stack"));
 
-        final String wildCardUri1 = "aeron:udp?endpoint=[::1]:0|tags=1001";
+        final String systemAssignedPortUri = "aeron:udp?endpoint=[::1]:0|tags=1001";
         final String tagged2 = "aeron:udp?tags=1001";
 
-        try (Subscription sub1 = client.addSubscription(wildCardUri1, STREAM_ID);
+        try (Subscription sub1 = client.addSubscription(systemAssignedPortUri, STREAM_ID);
             Subscription sub4 = client.addSubscription(tagged2, STREAM_ID + 1))
         {
             List<String> bindAddressAndPort1;
@@ -165,15 +166,15 @@ public class WildcardPortsSystemTest
 
     @Test
     @Timeout(5)
-    void shouldBindMultipleWildcardsToMultiDestinationSubscription()
+    void shouldBindMultipleSystemAssignedEndpointsForMultiDestinationSubscription()
     {
-        final String wildCardUri2 = "aeron:udp?endpoint=127.0.0.1:0";
-        final String wildCardUri3 = "aeron:udp?endpoint=127.0.0.1:0";
+        final String systemAssignedPortUri1 = "aeron:udp?endpoint=127.0.0.1:0";
+        final String systemAssignedPortUri2 = "aeron:udp?endpoint=127.0.0.1:0";
 
         try (Subscription mdsSub = client.addSubscription("aeron:udp?control-mode=manual", STREAM_ID))
         {
-            mdsSub.addDestination(wildCardUri2);
-            mdsSub.addDestination(wildCardUri3);
+            mdsSub.addDestination(systemAssignedPortUri1);
+            mdsSub.addDestination(systemAssignedPortUri2);
 
             List<String> bindAddressAndPorts;
             while (2 > (bindAddressAndPorts = mdsSub.localSocketAddresses()).size())
@@ -182,11 +183,13 @@ public class WildcardPortsSystemTest
             }
 
             final String pub1Uri = new ChannelUriStringBuilder()
-                .media("udp").endpoint(bindAddressAndPorts.get(0))
+                .media("udp")
+                .endpoint(bindAddressAndPorts.get(0))
                 .build();
 
             final String pub2Uri = new ChannelUriStringBuilder()
-                .media("udp").endpoint(bindAddressAndPorts.get(1))
+                .media("udp")
+                .endpoint(bindAddressAndPorts.get(1))
                 .build();
 
             try (Publication pub1 = client.addPublication(pub1Uri, STREAM_ID);
@@ -226,7 +229,9 @@ public class WildcardPortsSystemTest
             }
 
             final String mdcSubUri1 = new ChannelUriStringBuilder()
-                .media("udp").controlEndpoint(bindAddressAndPort1.get(0)).group(true)
+                .media("udp")
+                .controlEndpoint(bindAddressAndPort1.get(0))
+                .group(true)
                 .build();
 
             try (Subscription sub = client.addSubscription(mdcSubUri1, STREAM_ID))

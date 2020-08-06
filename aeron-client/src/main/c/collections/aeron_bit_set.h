b8b439b85527ab0d79dd5b1edaa7f0ddc73646a4
mjpt777 <mjpt777@gmail.com>
2020-08-06 18:46:08 +0100

    [C] Use aeron_alloc and aeron_free in aeron_bit_set.

--- aeron-client/src/main/c/collections/aeron_bit_set.h
+++ aeron-client/src/main/c/collections/aeron_bit_set.h
@@ -19,9 +19,11 @@
 
 #include <errno.h>
 #include <stdbool.h>
-#include <stdlib.h>
 #include <string.h>
+#include "util/aeron_platform.h"
 #include "util/aeron_bitutil.h"
+#include "aeron_alloc.h"
+#include "util/aeron_error.h"
 
 struct aeron_bit_set_stct
 {
@@ -44,17 +46,21 @@ inline int aeron_bit_set_init(aeron_bit_set_t *bit_set, bool initial_value)
 inline int aeron_bit_set_stack_alloc(
     size_t bit_set_length, uint64_t *static_array, size_t static_array_len, aeron_bit_set_t *bit_set)
 {
-    (bit_set)->bit_set_length = bit_set_length;
-    (bit_set)->static_array = static_array;
+    bit_set->bit_set_length = bit_set_length;
+    bit_set->static_array = static_array;
 
     const size_t u64_len = ((bit_set_length + 63) / 64);
     if (NULL != static_array && u64_len <= static_array_len)
     {
-        (bit_set)->bits = static_array;
+        bit_set->bits = static_array;
     }
     else
     {
-        (bit_set)->bits = (uint64_t *)malloc(sizeof(uint64_t) * u64_len);
+        if (aeron_alloc((void **)&bit_set->bits, sizeof(uint64_t) * u64_len) < 0)
+        {
+            aeron_set_err_from_last_err_code("%s:%d", __FILE__, __LINE__);
+            return -1;
+        }
     }
 
     return 0;
@@ -67,11 +73,10 @@ inline int aeron_bit_set_heap_alloc(size_t bit_set_length, aeron_bit_set_t **bit
         return -EINVAL;
     }
 
-    *bit_set = (aeron_bit_set_t *)malloc(sizeof(aeron_bit_set_t));
-
-    if (NULL == *bit_set)
+    if (aeron_alloc((void **)bit_set, sizeof(aeron_bit_set_t)) < 0)
     {
-        return -ENOMEM;
+        aeron_set_err_from_last_err_code("%s:%d", __FILE__, __LINE__);
+        return -1;
     }
 
     return aeron_bit_set_stack_alloc(bit_set_length, NULL, 0, *bit_set);
@@ -93,10 +98,7 @@ inline int aeron_bit_set_stack_init(
     return aeron_bit_set_init(bit_set, initial_value);
 }
 
-inline int aeron_bit_set_heap_init(
-    size_t bit_set_length,
-    bool initial_value,
-    aeron_bit_set_t **bit_set)
+inline int aeron_bit_set_heap_init(size_t bit_set_length, bool initial_value, aeron_bit_set_t **bit_set)
 {
     int result;
     if (0 != (result = aeron_bit_set_heap_alloc(bit_set_length, bit_set)))
@@ -111,7 +113,7 @@ inline void aeron_bit_set_stack_free(aeron_bit_set_t *bit_set)
 {
     if (bit_set->static_array != bit_set->bits)
     {
-        free(bit_set->bits);
+        aeron_free(bit_set->bits);
     }
 
     bit_set->bits = NULL;
@@ -121,7 +123,7 @@ inline void aeron_bit_set_stack_free(aeron_bit_set_t *bit_set)
 inline void aeron_bit_set_heap_free(aeron_bit_set_t *bit_set)
 {
     aeron_bit_set_stack_free(bit_set);
-    free(bit_set);
+    aeron_free(bit_set);
 }
 
 inline int aeron_bit_set_get(aeron_bit_set_t *bit_set, size_t bit_index, bool *value)
@@ -166,7 +168,7 @@ inline int aeron_bit_set_set(aeron_bit_set_t *bit_set, size_t bit_index, bool va
 inline int aeron_bit_set_find_first(aeron_bit_set_t *bit_set, bool value, size_t *bit_index)
 {
     const uint64_t entry_empty_value = value ? 0 : UINT64_C(0xFFFFFFFFFFFFFFFF);
-    size_t num_entries = ((bit_set->bit_set_length + 63) / 64);
+    size_t num_entries = (bit_set->bit_set_length + 63) / 64;
 
     for (size_t i = 0; i < num_entries; i++)
     {
@@ -175,7 +177,7 @@ inline int aeron_bit_set_find_first(aeron_bit_set_t *bit_set, bool value, size_t
             uint64_t bits_to_search = value ? bit_set->bits[i] : ~bit_set->bits[i];
             *bit_index = (i * 64 + (size_t)aeron_number_of_trailing_zeroes_u64(bits_to_search));
 
-            return (*bit_index < bit_set->bit_set_length) ? 0 : -1;
+            return *bit_index < bit_set->bit_set_length ? 0 : -1;
         }
     }
 

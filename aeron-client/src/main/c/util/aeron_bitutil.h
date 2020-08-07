3fe255889fb80eef07f66532002cf0c6cd92798e
mjpt777 <mjpt777@gmail.com>
2020-08-07 11:42:09 +0100

    [C] Formatting.

--- aeron-client/src/main/c/util/aeron_bitutil.h
+++ aeron-client/src/main/c/util/aeron_bitutil.h
@@ -50,7 +50,9 @@ inline int aeron_number_of_trailing_zeroes(int32_t value)
     unsigned long r;
 
     if (_BitScanForward(&r, (unsigned long)value))
+    {
         return (int)r;
+    }
 
     return 32;
 #else
@@ -86,7 +88,9 @@ inline int aeron_number_of_trailing_zeroes_u64(uint64_t value)
     unsigned long r;
 
     if (_BitScanForward64(&r, (__int64)value))
+    {
         return (int)r;
+    }
 
     return 64;
 #else

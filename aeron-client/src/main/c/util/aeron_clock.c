d29711c7804228f122a85e8f684b051ff4d3edb0
Yonggang Luo <luoyonggang@gmail.com>
2020-08-16 19:31:27 +0800

    [C] include intrin.h in bitutil instead of windows.h  but still preserve standalone property. (#1042)

--- aeron-client/src/main/c/util/aeron_clock.c
+++ aeron-client/src/main/c/util/aeron_clock.c
@@ -27,6 +27,8 @@
 
 #if defined(AERON_COMPILER_MSVC)
 
+#include <windows.h>
+
 #define MS_PER_SEC  1000ULL     // MS = milliseconds
 #define US_PER_MS   1000ULL     // US = microseconds
 #define HNS_PER_US  10ULL       // HNS = hundred-nanoseconds (e.g., 1 hns = 100 ns)

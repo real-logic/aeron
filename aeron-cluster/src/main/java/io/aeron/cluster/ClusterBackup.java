122e82f0b504ab4cd0d6ed41d6cbdd373b32bef1
mjpt777 <mjpt777@gmail.com>
2020-11-10 13:37:21 +0000

    [Java] Javadoc.

--- aeron-cluster/src/main/java/io/aeron/cluster/ClusterBackup.java
+++ aeron-cluster/src/main/java/io/aeron/cluster/ClusterBackup.java
@@ -134,17 +134,16 @@ public final class ClusterBackup implements AutoCloseable
             ctx.conclude();
             this.ctx = ctx;
 
-            final ClusterBackupAgent clusterBackupAgent = new ClusterBackupAgent(ctx);
+            final ClusterBackupAgent agent = new ClusterBackupAgent(ctx);
 
             if (ctx.useAgentInvoker())
             {
                 agentRunner = null;
-                agentInvoker = new AgentInvoker(ctx.errorHandler(), ctx.errorCounter(), clusterBackupAgent);
+                agentInvoker = new AgentInvoker(ctx.errorHandler(), ctx.errorCounter(), agent);
             }
             else
             {
-                agentRunner = new AgentRunner(
-                    ctx.idleStrategy(), ctx.errorHandler(), ctx.errorCounter(), clusterBackupAgent);
+                agentRunner = new AgentRunner(ctx.idleStrategy(), ctx.errorHandler(), ctx.errorCounter(), agent);
                 agentInvoker = null;
             }
         }
@@ -225,7 +224,15 @@ public final class ClusterBackup implements AutoCloseable
      */
     public static class Configuration
     {
+        /**
+         * Default which is derived from {@link ConsensusModule.Context#consensusChannel()} with the member endpoint
+         * added for the consensus channel.
+         */
         public static final String CONSENSUS_CHANNEL_DEFAULT;
+
+        /**
+         * Member endpoint used for the the catchup channel.
+         */
         public static final String CATCHUP_ENDPOINT_DEFAULT;
 
         /**

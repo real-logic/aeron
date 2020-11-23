8468e3df030a75971f3cdb6bf135bb907d977366
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2020-11-23 12:04:51 +0100

    [Java] Support Java versions with minor components and `-ea` specifier.

--- aeron-client/src/test/java/io/aeron/VerifyBuildTimePropertiesTest.java
+++ aeron-client/src/test/java/io/aeron/VerifyBuildTimePropertiesTest.java
@@ -11,10 +11,18 @@ public class VerifyBuildTimePropertiesTest
     private static final String BUILD_JAVA_VERSION_ENV_VAR = "BUILD_JAVA_VERSION";
 
     @Test
-    @EnabledIfEnvironmentVariable(named = BUILD_JAVA_VERSION_ENV_VAR, matches = "\\d+")
+    @EnabledIfEnvironmentVariable(named = BUILD_JAVA_VERSION_ENV_VAR, matches = ".+")
     void checkVersion()
     {
-        final String version = System.getenv(BUILD_JAVA_VERSION_ENV_VAR);
+        String version = System.getenv(BUILD_JAVA_VERSION_ENV_VAR);
+        if (version.indexOf('.') > 0)
+        {
+            version = version.substring(0, version.indexOf('.'));
+        }
+        if (version.indexOf('-') > 0)
+        {
+            version = version.substring(0, version.indexOf('-'));
+        }
         final String currentVersion = JRE.currentVersion().name();
         assertEquals(version, currentVersion.substring(currentVersion.indexOf('_') + 1));
     }

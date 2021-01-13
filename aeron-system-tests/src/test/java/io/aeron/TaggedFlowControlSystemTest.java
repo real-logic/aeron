1b19671916197445359aa819c3b7aabb53ea2181
mjpt777 <mjpt777@gmail.com>
2021-01-13 14:09:29 +0000

    [Java] Formatting.

--- aeron-system-tests/src/test/java/io/aeron/TaggedFlowControlSystemTest.java
+++ aeron-system-tests/src/test/java/io/aeron/TaggedFlowControlSystemTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014-2021 Real Logic Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.aeron;
 
 import io.aeron.driver.*;
@@ -214,10 +229,7 @@ public class TaggedFlowControlSystemTest
         }
 
         verify(fragmentHandlerB, times(state.numMessagesToSend)).onFragment(
-            any(DirectBuffer.class),
-            anyInt(),
-            eq(MESSAGE_LENGTH),
-            any(Header.class));
+            any(DirectBuffer.class), anyInt(), eq(MESSAGE_LENGTH), any(Header.class));
     }
 
     @Test
@@ -275,10 +287,7 @@ public class TaggedFlowControlSystemTest
         }
 
         verify(fragmentHandlerA, times(state.numMessagesToSend)).onFragment(
-            any(DirectBuffer.class),
-            anyInt(),
-            eq(MESSAGE_LENGTH),
-            any(Header.class));
+            any(DirectBuffer.class), anyInt(), eq(MESSAGE_LENGTH), any(Header.class));
     }
 
     private int pollWithTimeout(
@@ -487,7 +496,6 @@ public class TaggedFlowControlSystemTest
         subscriptionA = clientA.addSubscription(subscriberUri, STREAM_ID);
 
         awaitConnectionAndStatusMessages(countersReader, subscriptionA);
-
         assertEquals(currentSenderLimit, countersReader.getCounterValue(senderLimitCounterId));
 
         subscriptionB = clientB.addSubscription(groupSubscriberUri, STREAM_ID);

1e5717b70a0de1c09ca6f62eaa7e1e191a818e88
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2021-01-25 13:57:02 +0100

    [Java] Print Java-related ENV and system properties.

---
+++ aeron-system-tests/src/test/java/io/aeron/PrintEnvInfo.java
@@ -0,0 +1,25 @@
+package io.aeron;
+
+import org.junit.jupiter.api.Test;
+
+class PrintEnvInfo
+{
+    @Test
+    void test()
+    {
+        System.out.println("=========================");
+        System.out.println("[PrintEnvInfo] System properties:");
+        System.out.println("=========================");
+        System.getProperties().entrySet().stream()
+            .filter(e -> ((String)e.getKey()).startsWith("java."))
+            .forEach(e -> System.out.println("- " + e.getKey() + ": " + e.getValue()));
+
+        System.out.println("\n=========================");
+        System.out.println("[PrintEnvInfo] ENV variables:");
+        System.out.println("=========================");
+        for (final String env : new String[]{ "JAVA_HOME", "BUILD_JAVA_HOME", "BUILD_JAVA_VERSION", "PATH" })
+        {
+            System.out.println("- " + env + ": " + System.getenv(env));
+        }
+    }
+}

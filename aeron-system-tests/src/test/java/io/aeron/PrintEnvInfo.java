80da1759c8d133aabc3bb9f73f4a6c54451e7aad
mjpt777 <mjpt777@gmail.com>
2021-01-26 12:52:54 +0000

    [Java] Disable the printing of env information.

--- aeron-system-tests/src/test/java/io/aeron/PrintEnvInfo.java
+++ aeron-system-tests/src/test/java/io/aeron/PrintEnvInfo.java
@@ -1,9 +1,26 @@
+/*
+ * Copyright 2014-2021 Real Logic Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.aeron;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class PrintEnvInfo
 {
+    @Disabled
     @Test
     void test()
     {
@@ -11,8 +28,8 @@ class PrintEnvInfo
         System.out.println("[PrintEnvInfo] System properties:");
         System.out.println("=========================");
         System.getProperties().entrySet().stream()
-            .filter(e -> ((String)e.getKey()).contains("java."))
-            .forEach(e -> System.out.println("- " + e.getKey() + ": " + e.getValue()));
+            .filter((e) -> ((String)e.getKey()).contains("java."))
+            .forEach((e) -> System.out.println("- " + e.getKey() + ": " + e.getValue()));
 
         System.out.println("\n=========================");
         System.out.println("[PrintEnvInfo] ENV variables:");

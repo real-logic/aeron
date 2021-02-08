d574a5ea0955ddde3e836b9e9a0a268e02245fb5
mjpt777 <mjpt777@gmail.com>
2021-02-08 16:58:08 +0000

    [C] Return early from aeron_raw_log_map_existing method on error.

--- aeron-client/src/main/c/util/aeron_fileutil.c
+++ aeron-client/src/main/c/util/aeron_fileutil.c
@@ -477,11 +477,13 @@ int aeron_raw_log_map_existing(aeron_mapped_raw_log_t *mapped_raw_log, const cha
             if (aeron_mmap(&mapped_raw_log->mapped_file, fd, 0) < 0)
             {
                 AERON_SET_ERR(errno, "Failed to mmap existing raw log, filename: %s", path);
+                return -1;
             }
         }
         else
         {
             AERON_SET_ERR(errno, "Failed to stat existing raw log, filename: %s", path);
+            return -1;
         }
 
         mapped_raw_log->log_meta_data.addr =

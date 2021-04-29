670fcdca44a7bf17051fff99dcd58db336077cf6
Michael Barker <mikeb01@gmail.com>
2021-04-30 08:41:29 +1200

    [C] Remove minor warning.

--- aeron-driver/src/main/c/aeron_driver_sender.c
+++ aeron-driver/src/main/c/aeron_driver_sender.c
@@ -263,7 +263,7 @@ void aeron_driver_sender_on_add_publication(void *clientd, void *command)
 
     int ensure_capacity_result = 0;
     AERON_ARRAY_ENSURE_CAPACITY(
-        ensure_capacity_result, sender->network_publications, aeron_driver_sender_network_publication_entry_t);
+        ensure_capacity_result, sender->network_publications, aeron_driver_sender_network_publication_entry_t)
 
     if (ensure_capacity_result < 0)
     {

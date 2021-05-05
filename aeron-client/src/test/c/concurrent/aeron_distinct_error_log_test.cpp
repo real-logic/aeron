a050cbfa073163231a816a28b977d1e1d471819f
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2021-05-05 14:13:38 +0200

    [C++] Add concurrent tests for the distinct_error_log.

--- aeron-client/src/test/c/concurrent/aeron_distinct_error_log_test.cpp
+++ aeron-client/src/test/c/concurrent/aeron_distinct_error_log_test.cpp
@@ -15,6 +15,8 @@
  */
 
 #include <array>
+#include <thread>
+#include <atomic>
 #include <gtest/gtest.h>
 
 extern "C"
@@ -23,7 +25,7 @@ extern "C"
 #include "concurrent/aeron_distinct_error_log.h"
 }
 
-#define CAPACITY (1024)
+#define CAPACITY (256 * 1024)
 
 typedef std::array<std::uint8_t, CAPACITY> buffer_t;
 typedef std::array<std::uint8_t, 32> insufficient_buffer_t;
@@ -39,7 +41,10 @@ public:
 
     ~DistinctErrorLogTest() override
     {
-        aeron_distinct_error_log_close(&m_log);
+        if (m_close_log)
+        {
+            aeron_distinct_error_log_close(&m_log);
+        }
     }
 
     void SetUp() override
@@ -60,6 +65,7 @@ public:
 protected:
     AERON_DECL_ALIGNED(buffer_t m_buffer, 16){};
     aeron_distinct_error_log_t m_log{};
+    bool m_close_log = true;
 
     static int64_t clock_value;
 };
@@ -278,4 +284,216 @@ TEST_F(DistinctErrorLogTest, shouldReadOneObservationSinceTimestamp)
 
     EXPECT_EQ(aeron_error_log_read(
         m_buffer.data(), m_buffer.size(), error_log_reader_since_observation, nullptr, 8), (size_t)1);
+}
+
+#define APPENDS_PER_THREAD (1000)
+#define NUM_THREADS (2)
+#define ITERATIONS (500)
+
+static void distinct_message_log_reader(
+    int32_t observation_count,
+    int64_t first_observation_timestamp,
+    int64_t last_observation_timestamp,
+    const char *error,
+    size_t error_length,
+    void *clientd)
+{
+    ASSERT_GE(observation_count, 0);
+    ASSERT_LE(observation_count, APPENDS_PER_THREAD);
+    auto counts = (std::vector<int> *)clientd;
+    char *end;
+    const auto index = strtol(error, &end, 10);
+    counts->at(index) = observation_count;
+}
+
+static void test_append_distinct_message(aeron_distinct_error_log_t *error_log, buffer_t *log_buffer)
+{
+    std::atomic<int> countDown(NUM_THREADS);
+    std::vector<std::thread> threads;
+
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        threads.push_back(std::thread(
+            [&]()
+            {
+                const int err_code = countDown.fetch_sub(1) - 1;
+                while (countDown > 0)
+                {
+                    std::this_thread::yield();
+                }
+
+                const std::string err_msg = std::to_string(err_code);
+                for (int m = 0; m < APPENDS_PER_THREAD; m++)
+                {
+                    ASSERT_EQ(0, aeron_distinct_error_log_record(error_log, err_code, err_msg.c_str()));
+                }
+            }));
+    }
+
+    for (std::thread &thr: threads)
+    {
+        thr.join();
+    }
+
+    std::vector<int> counts;
+    counts.resize(NUM_THREADS, 0);
+
+    size_t entries =
+        aeron_error_log_read(log_buffer->data(), log_buffer->size(), distinct_message_log_reader, &counts, 0);
+    ASSERT_EQ(entries, NUM_THREADS) << "invalid number of messages";
+
+    for (int count : counts)
+    {
+        EXPECT_EQ(count, APPENDS_PER_THREAD) << "invalid number of obesrvation";
+    }
+}
+
+TEST_F(DistinctErrorLogTest, concurrentAppendDistinctMessages)
+{
+    m_close_log = false;
+    for (int i = 0; i < ITERATIONS; i++)
+    {
+        m_buffer.fill(0);
+        ASSERT_EQ(aeron_distinct_error_log_init(&m_log, m_buffer.data(), m_buffer.size(), clock, linger_resource, nullptr), 0);
+        test_append_distinct_message(&m_log, &m_buffer);
+
+        aeron_distinct_error_log_close(&m_log);
+    }
+}
+
+static void same_message_log_reader(
+    int32_t observation_count,
+    int64_t first_observation_timestamp,
+    int64_t last_observation_timestamp,
+    const char *error,
+    size_t error_length,
+    void *clientd)
+{
+    ASSERT_GE(observation_count, 0);
+    auto count = (size_t *)clientd;
+    (*count) = observation_count;
+}
+
+static void test_update_same_message(aeron_distinct_error_log_t *error_log, buffer_t *log_buffer)
+{
+    std::atomic<int> countDown(NUM_THREADS);
+    std::vector<std::thread> threads;
+    const int err_code = 111;
+    const std::string err_msg = std::string("common message");
+
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        threads.push_back(std::thread(
+            [&]()
+            {
+                countDown.fetch_sub(1);
+                while (countDown > 0)
+                {
+                    std::this_thread::yield();
+                }
+
+                for (int m = 0; m < APPENDS_PER_THREAD; m++)
+                {
+                    ASSERT_EQ(0, aeron_distinct_error_log_record(error_log, err_code, err_msg.c_str()));
+                }
+            }));
+    }
+
+    for (std::thread &thr: threads)
+    {
+        thr.join();
+    }
+
+    size_t count = 0;
+    size_t entries =
+        aeron_error_log_read(log_buffer->data(), log_buffer->size(), same_message_log_reader, &count, 0);
+    ASSERT_EQ(entries, 1) << "message appended multiple times";
+    ASSERT_EQ(count,  APPENDS_PER_THREAD * NUM_THREADS) << "missing observations";
+}
+
+TEST_F(DistinctErrorLogTest, concurrentAppendSameMessage)
+{
+    m_close_log = false;
+    for (int i = 0; i < ITERATIONS; i++)
+    {
+        m_buffer.fill(0);
+        ASSERT_EQ(aeron_distinct_error_log_init(&m_log, m_buffer.data(), m_buffer.size(), clock, linger_resource, nullptr), 0);
+        test_update_same_message(&m_log, &m_buffer);
+
+        aeron_distinct_error_log_close(&m_log);
+    }
+}
+
+static void unique_message_log_reader(
+    int32_t observation_count,
+    int64_t first_observation_timestamp,
+    int64_t last_observation_timestamp,
+    const char *error,
+    size_t error_length,
+    void *clientd)
+{
+    ASSERT_GE(observation_count, 0);
+    ASSERT_LE(observation_count, 1);
+    auto counts = (std::vector<int> *)clientd;
+    char *end;
+    const auto index = strtol(error, &end, 10);
+    counts->at(index) = 1;
+}
+
+static void test_append_unique_messages(aeron_distinct_error_log_t *error_log, buffer_t *log_buffer)
+{
+    std::atomic<int> countDown(NUM_THREADS);
+    std::vector<std::thread> threads;
+
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        threads.push_back(std::thread(
+            [&]()
+            {
+                const int err_code_offset = (countDown.fetch_sub(1) - 1) * APPENDS_PER_THREAD;
+                while (countDown > 0)
+                {
+                    std::this_thread::yield();
+                }
+
+                for (int m = 0; m < APPENDS_PER_THREAD; m++)
+                {
+                    const int err_code = err_code_offset + m;
+                    const std::string err_msg = std::to_string(err_code);
+                    ASSERT_EQ(0, aeron_distinct_error_log_record(error_log, err_code, err_msg.c_str()));
+                }
+            }));
+    }
+
+    for (std::thread &thr: threads)
+    {
+        thr.join();
+    }
+
+    const size_t total_number_of_entries = APPENDS_PER_THREAD * NUM_THREADS;
+    std::vector<int> counts;
+    counts.resize(total_number_of_entries, 0);
+
+    size_t entries =
+        aeron_error_log_read(log_buffer->data(), log_buffer->size(), unique_message_log_reader, &counts, 0);
+    ASSERT_EQ(entries, total_number_of_entries);
+
+    for (int count : counts)
+    {
+        EXPECT_EQ(count, 1);
+    }
+}
+
+TEST_F(DistinctErrorLogTest, concurrentAppendUniqueMessages)
+{
+    m_close_log = false;
+    for (int i = 0; i < ITERATIONS; i++)
+    {
+        m_buffer.fill(0);
+        ASSERT_EQ(aeron_distinct_error_log_init(&m_log, m_buffer.data(), m_buffer.size(), clock, linger_resource, nullptr), 0);
+
+        test_append_unique_messages(&m_log, &m_buffer);
+
+        aeron_distinct_error_log_close(&m_log);
+    }
 }

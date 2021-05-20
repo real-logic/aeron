5645925ede04796a37c51ab515c6930168f645c7
Michael Barker <mikeb01@gmail.com>
2021-05-21 11:35:41 +1200

    [Java] Checkstyle.

--- aeron-test-support/src/main/java/io/aeron/test/NetworkTestingUtil.java
+++ aeron-test-support/src/main/java/io/aeron/test/NetworkTestingUtil.java
@@ -1,6 +1,20 @@
+/*
+ * Copyright 2014-2021 Real Logic Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.aeron.test;
 
-import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 
@@ -11,15 +25,15 @@ public class NetworkTestingUtil
      * @param address The address to attempt to bind to.
      * @return null if successful, error message otherwise.
      */
-    public static String isBindAddressAvailable(String address)
+    public static String isBindAddressAvailable(final String address)
     {
         try (ServerSocket ignored = new ServerSocket(0, 1024, InetAddress.getByName(address)))
         {
             return null;
         }
-        catch (Exception e)
+        catch (final Exception ex)
         {
-            return "Binding to " + address + " failed, error: " + e.getMessage();
+            return "Binding to " + address + " failed, error: " + ex.getMessage();
         }
     }
 }

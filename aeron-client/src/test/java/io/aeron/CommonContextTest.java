51a66edfb6e6d1a4d8ed645a625a4942d2d19163
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2021-09-16 11:11:44 +0200

    [Java] Restore delete test that verifies invocation order of the user error handler and the internal error handling.

--- aeron-client/src/test/java/io/aeron/CommonContextTest.java
+++ aeron-client/src/test/java/io/aeron/CommonContextTest.java
@@ -23,6 +23,7 @@ import org.agrona.concurrent.errors.DistinctErrorLog;
 import org.agrona.concurrent.errors.LoggingErrorHandler;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+import org.mockito.InOrder;
 
 import java.io.File;
 import java.io.PrintStream;
@@ -56,6 +57,30 @@ class CommonContextTest
         assertNotNull(errorHandler);
         final LoggingErrorHandler loggingErrorHandler = assertInstanceOf(LoggingErrorHandler.class, errorHandler);
         assertSame(distinctErrorLog, loggingErrorHandler.distinctErrorLog());
+    }
+
+    @Test
+    void setupErrorHandlerReturnsAnErrorHandlerThatFirstInvokesUserSuppliedErrorHandlerBeforeTheLoggingErrorHandler()
+    {
+        final Throwable throwable = new Throwable("Hello, world!");
+        final ErrorHandler userErrorHandler = mock(ErrorHandler.class);
+        final Exception userHandlerError = new IndexOutOfBoundsException("user handler error");
+        doThrow(userHandlerError).when(userErrorHandler).onError(throwable);
+        final DistinctErrorLog distinctErrorLog = mock(DistinctErrorLog.class);
+        doReturn(true).when(distinctErrorLog).record(any(Throwable.class));
+        final InOrder inOrder = inOrder(userErrorHandler, distinctErrorLog);
+
+        final ErrorHandler errorHandler = CommonContext.setupErrorHandler(userErrorHandler, distinctErrorLog);
+
+        assertNotNull(errorHandler);
+        assertNotSame(userErrorHandler, errorHandler);
+
+        errorHandler.onError(throwable);
+
+        inOrder.verify(userErrorHandler).onError(throwable);
+        inOrder.verify(distinctErrorLog).record(userHandlerError);
+        inOrder.verify(distinctErrorLog).record(throwable);
+        inOrder.verifyNoMoreInteractions();
     }
 
     @Test

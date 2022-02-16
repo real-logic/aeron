9c3729704ee7c56bbee6af74d2c2c8220e98daec
mjpt777 <mjpt777@gmail.com>
2022-02-16 12:02:33 +0000

    [Java] Assert the removed publication is not managed by the conductor.

--- aeron-system-tests/src/test/java/io/aeron/AsyncResourceTest.java
+++ aeron-system-tests/src/test/java/io/aeron/AsyncResourceTest.java
@@ -27,10 +27,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @SuppressWarnings("try")
@@ -113,6 +110,7 @@ class AsyncResourceTest
 
             aeron.asyncRemovePublication(registrationId);
             assertTrue(publication.isClosed());
+            assertNull(aeron.getPublication(registrationId));
         }
         finally
         {

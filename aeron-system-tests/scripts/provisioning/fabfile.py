64395ae51f62cabc4b489f42d66b634a020222d5
Michael Barker <mikeb01@gmail.com>
2022-03-31 16:29:04 +1300

    [Java] Remove shell script and handle all launching logic in fabfile.

--- aeron-system-tests/scripts/provisioning/fabfile.py
+++ aeron-system-tests/scripts/provisioning/fabfile.py
@@ -1,25 +1,68 @@
 from invoke import task
 from invoke import run as local
 from fabric import Connection
+import re
 
 @task()
 def prepare_deploy(c):
     local("./gradlew --console=verbose clean", env = {"JAVA_HOME": "/home/mike/opt/jdk/jdk8"})
     local("./gradlew --console=verbose :aeron-all:jar", env = {"JAVA_HOME": "/home/mike/opt/jdk/jdk8"})
 
+def parse_version(version_string):
+    g = re.search("version \"(.*)\\.(.*)\\..*\"", version_string)
+
+    if (g is None):
+        raise RuntimeError("Unable to get version information: {}".format(r.stdout))
+
+    if (g.group(1) == "1"):
+        version = int(g.group(2))
+    else:
+        version = int(g.group(1))
+
+    return version
+
+@task()
+def version(c, java_home=None):
+    if (java_home is None):
+        raise RuntimeError("--java-home must be specified")
+
+    r = c.run("{}/bin/java -version 2>&1".format(java_home), hide=True)
+    print(parse_version(r.stdout.splitlines()[0]))
+
 @task()
 def deploy(c, java_home=None, provisioning_host=None):
+    if (java_home is None):
+        raise RuntimeError("--java-home must be specified")
+
+    r = c.run("{}/bin/java -version 2>&1".format(java_home), hide=True)
+    java_version = parse_version(r.stdout.splitlines()[0])
+
     if (provisioning_host is None):
         provisioning_host = c.host
+
+    command = [
+      "{}/bin/java".format(java_home),
+      "-Dcom.sun.management.jmxremote",
+      "-Dcom.sun.management.jmxremote.authenticate=false",
+      "-Dcom.sun.management.jmxremote.ssl=false",
+      "-Dcom.sun.management.jmxremote.port=10000",
+      "-Djava.rmi.server.hostname={}".format(provisioning_host),
+      "-cp ./provisioning/aeron-all-1.38.0-SNAPSHOT.jar",
+      "io.aeron.samples.echo.ProvisioningServerMain",
+      "< /dev/null > ./provisioning/log 2>&1",
+      "&"
+    ]
+
+    if (8 < java_version):
+        command.insert(1, "--add-opens java.base/sun.nio.ch=ALL-UNNAMED")
 
     with open('version.txt') as f:
         lines = f.readlines()
         c.run("rm -rf provisioning")
         c.run("mkdir -p provisioning")
-        c.put("aeron-system-tests/scripts/provisioning/provisioning_server.sh", "provisioning/.")
         c.put("aeron-all/build/libs/aeron-all-{}.jar".format(lines[0]), "provisioning/.")
-        c.run("AERON_HOME=./provisioning JAVA_HOME={} PROVISIONING_HOST={} ./provisioning/provisioning_server.sh start < /dev/null > ./provisioning/log 2>&1".format(java_home, provisioning_host), pty=False)
-        c.run("sleep 2 ; AERON_HOME=./provisioning ./provisioning/provisioning_server.sh status")
+        c.run(" ".join(command))
+        c.run("sleep 2 ; pgrep -f io.aeron.samples.echo.ProvisioningServerMain")
 
 @task()
 def test(c, provisioning_host=None, test_host=None, aeron_dir=None):
@@ -45,4 +88,4 @@ def test(c, provisioning_host=None, test_host=None, aeron_dir=None):
 
 @task()
 def stop(c):
-    c.run("AERON_HOME=./provisioning ./provisioning/provisioning_server.sh stop")
+    c.run("pkill -f io.aeron.samples.echo.ProvisioningServerMain", warn=True)

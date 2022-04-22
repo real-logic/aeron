e7d1706b1f7e028fafd563d75131c914f5909d63
Michael Barker <mikeb01@gmail.com>
2022-04-22 13:19:41 +1200

    [C] Fix setting CPU affinity to CPU 0.

--- aeron-driver/src/main/c/aeron_driver_context.c
+++ aeron-driver/src/main/c/aeron_driver_context.c
@@ -478,6 +478,9 @@ int aeron_driver_context_init(aeron_driver_context_t **context)
     _context->sender_io_vector_capacity = AERON_SENDER_IO_VECTOR_CAPACITY_DEFAULT;
     _context->network_publication_max_messages_per_send = AERON_SENDER_MAX_MESSAGES_PER_SEND_DEFAULT;
     _context->connect_enabled = AERON_DRIVER_CONNECT_DEFAULT;
+    _context->conductor_cpu_affinity_no = AERON_CPU_AFFINITY_DEFAULT;
+    _context->sender_cpu_affinity_no = AERON_CPU_AFFINITY_DEFAULT;
+    _context->receiver_cpu_affinity_no = AERON_CPU_AFFINITY_DEFAULT;
 
     char *value = NULL;
 
@@ -2720,15 +2723,15 @@ void aeron_set_thread_affinity_on_start(void *state, const char *role_name)
 {
     aeron_driver_context_t *context = (aeron_driver_context_t *)state;
     int result = 0;
-    if (0 == strcmp("conductor", role_name) && 0 < context->conductor_cpu_affinity_no)
+    if (0 == strcmp("conductor", role_name) && 0 <= context->conductor_cpu_affinity_no)
     {
         result = aeron_thread_set_affinity(role_name, (uint8_t)context->conductor_cpu_affinity_no);
     }
-    else if (0 == strcmp("sender", role_name) && 0 < context->sender_cpu_affinity_no)
+    else if (0 == strcmp("sender", role_name) && 0 <= context->sender_cpu_affinity_no)
     {
         result = aeron_thread_set_affinity(role_name, (uint8_t)context->sender_cpu_affinity_no);
     }
-    else if (0 == strcmp("receiver", role_name) && 0 < context->receiver_cpu_affinity_no)
+    else if (0 == strcmp("receiver", role_name) && 0 <= context->receiver_cpu_affinity_no)
     {
         result = aeron_thread_set_affinity(role_name, (uint8_t)context->receiver_cpu_affinity_no);
     }

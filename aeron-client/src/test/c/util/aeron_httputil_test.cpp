b56d8ba40e6507fd4cda8d2053cea0d7356b288b
Michael Barker <mikeb01@gmail.com>
2022-05-11 21:51:34 +1200

    [C] Fix bug in HTTP client when the server closes the connection before the client has finished parsing the response. (#1320)

--- aeron-client/src/test/c/util/aeron_httputil_test.cpp
+++ aeron-client/src/test/c/util/aeron_httputil_test.cpp
@@ -19,8 +19,9 @@
 
 extern "C"
 {
+#include "util/aeron_error.h"
 #include "util/aeron_http_util.h"
-bool aeron_http_response_is_complete(aeron_http_response_t *response);
+int aeron_http_parse_response(aeron_http_response_t *response);
 }
 
 class HttpUtilTest : public testing::Test
@@ -59,36 +60,19 @@ TEST_F(HttpUtilTest, shouldParseHttpContent)
         response.status_code = 0;
         response.content_length = 0;
         response.parse_err = false;
+        response.is_complete = false;
 
         memcpy(buffer, content, offset);
         response.length = offset;
 
-        bool is_complete = false;
-        for (int i = 0; i < 10; i++)
-        {
-            is_complete = aeron_http_response_is_complete(&response);
+        ASSERT_EQ(0, aeron_http_parse_response(&response));
+        ASSERT_FALSE(response.is_complete);
 
-            if (is_complete)
-            {
-                break;
-            }
-        }
+        memcpy(buffer + offset, content + offset, total_length - offset);
+        response.length = total_length;
 
-        if (!is_complete)
-        {
-            memcpy(buffer + offset, content + offset, total_length - offset);
-            response.length = total_length;
-
-            for (int i = 0; i < 10; i++)
-            {
-                is_complete = aeron_http_response_is_complete(&response);
-
-                if (is_complete)
-                {
-                    break;
-                }
-            }
-        }
+        ASSERT_EQ(0, aeron_http_parse_response(&response));
+        ASSERT_TRUE(response.is_complete);
 
         ASSERT_EQ(200, response.status_code);
         ASSERT_EQ(203, response.body_offset);
@@ -96,4 +80,101 @@ TEST_F(HttpUtilTest, shouldParseHttpContent)
         ASSERT_EQ(false, response.parse_err);
         ASSERT_STREQ("10.2.75.14", response.buffer + response.body_offset) << offset;
     }
+}
+
+TEST_F(HttpUtilTest, shouldParseHttpContentWithoutContentLength)
+{
+    const char *content =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/plain\r\n"
+        "Accept-Ranges: none\r\n"
+        "Last-Modified: Thu, 05 May 2022 18:43:25 GMT\r\n"
+        "Date: Thu, 05 May 2022 18:54:35 GMT\r\n"
+        "Server: EC2ws\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        "10.2.75.14";
+
+    size_t total_length = strlen(content);
+
+    for (size_t offset = 1; offset < total_length; offset++)
+    {
+        char buffer[1024] = {};
+
+        aeron_http_response_t response;
+        response.buffer = buffer;
+        response.headers_offset = 0;
+        response.cursor = 0;
+        response.body_offset = 0;
+        response.length = 0;
+        response.capacity = 0;
+        response.status_code = 0;
+        response.content_length = 0;
+        response.parse_err = false;
+        response.is_complete = false;
+
+        memcpy(buffer, content, offset);
+        response.length = offset;
+
+        ASSERT_EQ(0, aeron_http_parse_response(&response));
+        ASSERT_FALSE(response.is_complete);
+
+        memcpy(buffer + offset, content + offset, total_length - offset);
+        response.length = total_length;
+
+        ASSERT_EQ(0, aeron_http_parse_response(&response));
+        ASSERT_FALSE(response.is_complete);
+
+        ASSERT_EQ(200, response.status_code);
+        ASSERT_EQ(183, response.body_offset);
+        ASSERT_EQ(0, response.content_length);
+        ASSERT_EQ(false, response.parse_err);
+        ASSERT_STREQ("10.2.75.14", response.buffer + response.body_offset) << offset;
+    }
+}
+
+TEST_F(HttpUtilTest, shouldErrorWithInvalidStatusCode)
+{
+    const char *content =
+        "HTTP/1.1 AAA OK\r\n"
+        "Content-Type: text/plain\r\n"
+        "Accept-Ranges: none\r\n"
+        "Last-Modified: Thu, 05 May 2022 18:43:25 GMT\r\n"
+        "Content-Length: 10\r\n"
+        "Date: Thu, 05 May 2022 18:54:35 GMT\r\n"
+        "Server: EC2ws\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        "10.2.75.14";
+
+    aeron_http_response_t response = {};
+    response.buffer = const_cast<char *>(content);
+    response.length = strlen(content);
+
+    ASSERT_EQ(-1, aeron_http_parse_response(&response)) << aeron_errmsg();
+    ASSERT_FALSE(response.is_complete);
+    ASSERT_TRUE(response.parse_err);
+}
+
+TEST_F(HttpUtilTest, shouldErrorWithZeroStatusCode)
+{
+    const char *content =
+        "HTTP/1.1 000 OK\r\n"
+        "Content-Type: text/plain\r\n"
+        "Accept-Ranges: none\r\n"
+        "Last-Modified: Thu, 05 May 2022 18:43:25 GMT\r\n"
+        "Content-Length: 10\r\n"
+        "Date: Thu, 05 May 2022 18:54:35 GMT\r\n"
+        "Server: EC2ws\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        "10.2.75.14";
+
+    aeron_http_response_t response = {};
+    response.buffer = const_cast<char *>(content);
+    response.length = strlen(content);
+
+    ASSERT_EQ(-1, aeron_http_parse_response(&response)) << aeron_errmsg();
+    ASSERT_FALSE(response.is_complete);
+    ASSERT_TRUE(response.parse_err);
 }

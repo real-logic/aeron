ba588951e61b922a60aa3a4dd7549e3300fec9bd
mjpt777 <mjpt777@gmail.com>
2022-05-17 20:59:35 +0100

    [Java] Update recordingId on any signal with a valid recording id when handling signals for snapshot replication.

--- aeron-cluster/src/main/java/io/aeron/cluster/SnapshotReplication.java
+++ aeron-cluster/src/main/java/io/aeron/cluster/SnapshotReplication.java
@@ -67,10 +67,13 @@ final class SnapshotReplication
     {
         if (correlationId == replicationId)
         {
-            if (RecordingSignal.EXTEND == signal)
+            if (RecordingPos.NULL_RECORDING_ID != recordingId)
             {
                 this.recordingId = recordingId;
+            }
 
+            if (RecordingSignal.EXTEND == signal)
+            {
                 if (isNew && 0 != position)
                 {
                     throw new ClusterException("expected new extend signal at 0: position=" + position);

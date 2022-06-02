2c75e5d627203d4dc12c8a79a22228a08b55f06e
Michael Barker <mikeb01@gmail.com>
2022-06-02 14:31:22 +1200

    [Java] Fix lambda formatting. Ignore errors due to stray messages that can occur.

--- aeron-system-tests/src/test/java/io/aeron/cluster/StartFromTruncatedRecordingLogTest.java
+++ aeron-system-tests/src/test/java/io/aeron/cluster/StartFromTruncatedRecordingLogTest.java
@@ -27,6 +27,7 @@ import io.aeron.test.cluster.TestNode;
 import org.agrona.IoUtil;
 import org.agrona.collections.LongHashSet;
 import org.agrona.collections.MutableInteger;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.RegisterExtension;
@@ -54,6 +55,13 @@ public class StartFromTruncatedRecordingLogTest
 
     private final MutableInteger responseCount = new MutableInteger();
     private TestCluster cluster;
+
+    @BeforeEach
+    void setUp()
+    {
+        systemTestWatcher.ignoreErrorsMatching(
+            (s) -> s.contains("ats_gcm_decrypt final_ex: error:00000000:lib(0):func(0):reason(0)"));
+    }
 
     @Test
     @InterruptAfter(30)

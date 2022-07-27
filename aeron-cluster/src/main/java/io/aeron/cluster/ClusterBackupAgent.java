ef3df006479e49715ef0d59c03e1e521e1ce43df
mjpt777 <mjpt777@gmail.com>
2022-07-27 13:39:00 +0100

    [Java] Leader will not be null when fetched from members for cluster backup query.

--- aeron-cluster/src/main/java/io/aeron/cluster/ClusterBackupAgent.java
+++ aeron-cluster/src/main/java/io/aeron/cluster/ClusterBackupAgent.java
@@ -459,11 +459,7 @@ public final class ClusterBackupAgent implements Agent
 
             clusterMembers = ClusterMember.parse(backupResponseDecoder.clusterMembers());
             leaderMember = ClusterMember.findMember(clusterMembers, leaderMemberId);
-
-            if (null != leaderMember)
-            {
-                leaderMember.leadershipTermId(logLeadershipTermId);
-            }
+            leaderMember.leadershipTermId(logLeadershipTermId);
 
             if (null != eventsListener)
             {

3b25005d88db807d6a78b8fe68f167bcaaa733f1
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2022-09-14 12:16:57 +0200

    [Java] Extend the test with min/max term buffer length

--- aeron-system-tests/src/test/java/io/aeron/TermBufferLengthTest.java
+++ aeron-system-tests/src/test/java/io/aeron/TermBufferLengthTest.java
@@ -22,37 +22,40 @@ import io.aeron.test.Tests;
 import io.aeron.test.driver.TestMediaDriver;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TermBufferLengthTest
 {
-    private static final int TEST_TERM_LENGTH = LogBufferDescriptor.TERM_MIN_LENGTH * 2;
     private static final int STREAM_ID = 1001;
 
     @RegisterExtension
     final SystemTestWatcher testWatcher = new SystemTestWatcher();
 
     @ParameterizedTest
-    @ValueSource(strings = {
-        "aeron:udp?endpoint=localhost:24325|" + CommonContext.TERM_LENGTH_PARAM_NAME + "=" + TEST_TERM_LENGTH,
-        "aeron:ipc?" + CommonContext.TERM_LENGTH_PARAM_NAME + "=" + TEST_TERM_LENGTH
+    @CsvSource({
+        "aeron:udp?endpoint=localhost:24325|term-length=64K, 65536",
+        "aeron:ipc?term-length=64K, 65536",
+        "aeron:udp?endpoint=localhost:24325|term-length=1G, 1073741824",
+        "aeron:ipc?term-length=1G, 1073741824",
+        "aeron:udp?endpoint=localhost:24325|term-length=4M, 4194304",
+        "aeron:ipc?term-length=512K, 524288",
     })
-    void shouldHaveCorrectTermBufferLength(final String channel)
+    void shouldHaveCorrectTermBufferLength(final String channel, final int expectedTermBufferLength)
     {
         final MediaDriver.Context ctx = new MediaDriver.Context()
             .errorHandler(Tests::onError)
             .dirDeleteOnStart(true)
-            .publicationTermBufferLength(TEST_TERM_LENGTH * 2)
-            .ipcTermBufferLength(TEST_TERM_LENGTH * 2);
+            .publicationTermBufferLength(LogBufferDescriptor.TERM_MIN_LENGTH * 2)
+            .ipcTermBufferLength(LogBufferDescriptor.TERM_MIN_LENGTH * 2);
 
         try (
             TestMediaDriver mediaDriver = TestMediaDriver.launch(ctx, testWatcher);
             Aeron aeron = Aeron.connect(new Aeron.Context().aeronDirectoryName(mediaDriver.aeronDirectoryName()));
             Publication publication = aeron.addPublication(channel, STREAM_ID))
         {
-            assertEquals(TEST_TERM_LENGTH, publication.termBufferLength());
+            assertEquals(expectedTermBufferLength, publication.termBufferLength());
         }
         finally
         {

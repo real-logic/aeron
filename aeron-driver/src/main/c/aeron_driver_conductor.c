4d1fccb649b6773e34ca1ee49b6c32a2000e9c62
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2022-11-09 17:31:26 +0100

    Revert "[C] Remove `AERON_FILEUTIL_ERROR_ENOSPC`."
    This reverts commit 3ec4c8dab80425568288dc9ed242b5f4ef665a95.

--- aeron-driver/src/main/c/aeron_driver_conductor.c
+++ aeron-driver/src/main/c/aeron_driver_conductor.c
@@ -2744,7 +2744,18 @@ aeron_rb_read_action_t aeron_driver_conductor_on_command(
     {
         int os_errno = aeron_errcode();
         int code;
-        code = os_errno < 0 ? -os_errno : AERON_ERROR_CODE_GENERIC_ERROR;
+        if (os_errno < 0)
+        {
+            code = -os_errno;
+        }
+        else if (AERON_FILEUTIL_ERROR_ENOSPC == os_errno)
+        {
+            code = AERON_ERROR_CODE_STORAGE_SPACE;
+        }
+        else
+        {
+            code = AERON_ERROR_CODE_GENERIC_ERROR;
+        }
         aeron_driver_conductor_on_error(conductor, code, aeron_errmsg(), strlen(aeron_errmsg()), correlation_id);
         aeron_driver_conductor_log_error(conductor);
     }

fb3a6156d3754710079e22a33e4561e14ba75abf
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2022-11-17 19:25:26 +0100

    [Java] Add tests for `ClusterMember#is*Candidate` and `ClusterMember#is*Vote` methods.

--- aeron-cluster/src/test/java/io/aeron/cluster/ClusterMemberTest.java
+++ aeron-cluster/src/test/java/io/aeron/cluster/ClusterMemberTest.java
@@ -16,11 +16,14 @@
 package io.aeron.cluster;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static io.aeron.cluster.ClusterMember.quorumPosition;
-import static io.aeron.cluster.ClusterMember.quorumThreshold;
+import static io.aeron.archive.client.AeronArchive.NULL_POSITION;
+import static io.aeron.cluster.ClusterMember.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ClusterMemberTest
 {
@@ -77,5 +80,213 @@ public class ClusterMemberTest
             final long quorumPosition = quorumPosition(members, rankedPositions);
             assertThat("Test: " + i, quorumPosition, is(quorumPositions[i]));
         }
+    }
+
+    @Test
+    void isUnanimousCandidateReturnFalseIfThereIsAMemberWithoutLogPosition()
+    {
+        final ClusterMember candidate = newMember(4, 100, 1000);
+        final ClusterMember[] members = new ClusterMember[]
+        {
+            newMember(1, 2, 100),
+            newMember(2, 8, NULL_POSITION),
+            newMember(3, 1, 1)
+        };
+
+        assertFalse(isUnanimousCandidate(members, candidate));
+    }
+
+    @Test
+    void isUnanimousCandidateReturnFalseIfThereIsAMemberWithMoreUpToDateLog()
+    {
+        final ClusterMember candidate = newMember(4, 10, 800);
+        final ClusterMember[] members = new ClusterMember[]
+        {
+            newMember(1, 2, 100),
+            newMember(2, 8, 6),
+            newMember(3, 11, 1000)
+        };
+
+        assertFalse(isUnanimousCandidate(members, candidate));
+    }
+
+    @Test
+    void isUnanimousCandidateReturnTrueIfTheCandidateHasTheMostUpToDateLog()
+    {
+        final ClusterMember candidate = newMember(2, 10, 800);
+        final ClusterMember[] members = new ClusterMember[]
+        {
+            newMember(10, 2, 100),
+            newMember(20, 8, 6),
+            newMember(30, 10, 800)
+        };
+
+        assertTrue(isUnanimousCandidate(members, candidate));
+    }
+
+    @Test
+    void isQuorumCandidateReturnFalseWhenQuorumIsNotReached()
+    {
+        final ClusterMember candidate = newMember(2, 10, 800);
+        final ClusterMember[] members = new ClusterMember[]
+        {
+            newMember(10, 2, 100),
+            newMember(20, 18, 6),
+            newMember(30, 10, 800),
+            newMember(40, 19, 800),
+            newMember(50, 10, 1000),
+        };
+
+        assertFalse(isQuorumCandidate(members, candidate));
+    }
+
+    @Test
+    void isQuorumCandidateReturnTrueWhenQuorumIsReached()
+    {
+        final ClusterMember candidate = newMember(2, 10, 800);
+        final ClusterMember[] members = new ClusterMember[]
+        {
+            newMember(10, 2, 100),
+            newMember(20, 18, 6),
+            newMember(30, 10, 800),
+            newMember(40, 9, 800),
+            newMember(50, 10, 700)
+        };
+
+        assertTrue(isQuorumCandidate(members, candidate));
+    }
+
+    @Test
+    void isQuorumVoteReturnsTrueWhenQuorumIsReached()
+    {
+        final int candidateTermId = -5;
+        final ClusterMember[] members = new ClusterMember[]
+        {
+            newMember(1).candidateTermId(candidateTermId).vote(Boolean.TRUE),
+            newMember(2).candidateTermId(candidateTermId * 2).vote(Boolean.FALSE),
+            newMember(3).candidateTermId(candidateTermId).vote(null),
+            newMember(4).candidateTermId(candidateTermId).vote(Boolean.TRUE),
+            newMember(5).candidateTermId(candidateTermId).vote(Boolean.TRUE)
+        };
+
+        assertTrue(isQuorumVote(members, candidateTermId));
+    }
+
+    @Test
+    void isQuorumVoteReturnsFalseIfAtLeastOneNegativeVoteIsDetected()
+    {
+        final int candidateTermId = 8;
+        final ClusterMember[] members = new ClusterMember[]
+        {
+            newMember(1).candidateTermId(candidateTermId).vote(Boolean.TRUE),
+            newMember(2).candidateTermId(candidateTermId).vote(Boolean.FALSE),
+            newMember(3).candidateTermId(candidateTermId).vote(Boolean.TRUE)
+        };
+
+        assertFalse(isQuorumVote(members, candidateTermId));
+    }
+
+    @Test
+    void isQuorumVoteReturnsFalseWhenQuorumIsNotReached()
+    {
+        final int candidateTermId = 2;
+        final ClusterMember[] members = new ClusterMember[]
+        {
+            newMember(1).candidateTermId(candidateTermId).vote(Boolean.TRUE),
+            newMember(2).candidateTermId(candidateTermId).vote(null),
+            newMember(3).candidateTermId(candidateTermId + 5).vote(Boolean.TRUE)
+        };
+
+        assertFalse(isQuorumVote(members, candidateTermId));
+    }
+
+    @Test
+    void isUnanimousVoteReturnsFalseIfThereIsAtLeastOneNegativeVoteForAGivenCandidateTerm()
+    {
+        final int candidateTermId = 42;
+        final ClusterMember[] members = new ClusterMember[]
+        {
+            newMember(1).candidateTermId(candidateTermId).vote(Boolean.TRUE),
+            newMember(2).candidateTermId(candidateTermId).vote(Boolean.TRUE),
+            newMember(3).candidateTermId(candidateTermId).vote(Boolean.FALSE)
+        };
+
+        assertFalse(isUnanimousVote(members, candidateTermId));
+    }
+
+    @Test
+    void isUnanimousVoteReturnsFalseIfNotAllNodesVotedPositively()
+    {
+        final int candidateTermId = 2;
+        final ClusterMember[] members = new ClusterMember[]
+        {
+            newMember(1).candidateTermId(candidateTermId).vote(Boolean.TRUE),
+            newMember(2).candidateTermId(candidateTermId).vote(null),
+            newMember(3).candidateTermId(candidateTermId).vote(Boolean.TRUE)
+        };
+
+        assertFalse(isUnanimousVote(members, candidateTermId));
+    }
+
+    @Test
+    void isUnanimousVoteReturnsFalseIfNotAllNodesHadTheExpectedCandidateTermId()
+    {
+        final int candidateTermId = 2;
+        final ClusterMember[] members = new ClusterMember[]
+        {
+            newMember(1).candidateTermId(candidateTermId).vote(Boolean.TRUE),
+            newMember(2).candidateTermId(candidateTermId + 1).vote(Boolean.TRUE),
+            newMember(3).candidateTermId(candidateTermId).vote(Boolean.TRUE)
+        };
+
+        assertFalse(isUnanimousVote(members, candidateTermId));
+    }
+
+    @Test
+    void isUnanimousVoteReturnsTrueIfAllNodesVotedWithTrue()
+    {
+        final int candidateTermId = 42;
+        final ClusterMember[] members = new ClusterMember[]
+        {
+            newMember(1).candidateTermId(candidateTermId).vote(Boolean.TRUE),
+            newMember(2).candidateTermId(candidateTermId).vote(Boolean.TRUE),
+            newMember(3).candidateTermId(candidateTermId).vote(Boolean.TRUE)
+        };
+
+        assertTrue(isUnanimousVote(members, candidateTermId));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "5,1000,3,999999,1",
+        "-100,99999,4,0,-1",
+        "42,371239192371239,42,1001,1",
+        "3,-777,3,273291846723894,-1",
+        "1,1024,1,1024,0",
+    })
+    void compareLogReturnsResult(
+        final long lhsLogLeadershipTermId,
+        final long lhsLogPosition,
+        final long rhsLogLeadershipTermId,
+        final long rhsLogPosition,
+        final int expectedResult)
+    {
+        assertEquals(
+            expectedResult,
+            compareLog(lhsLogLeadershipTermId, lhsLogPosition, rhsLogLeadershipTermId, rhsLogPosition));
+        assertEquals(expectedResult, compareLog(
+            newMember(5, lhsLogLeadershipTermId, lhsLogPosition),
+            newMember(100, rhsLogLeadershipTermId, rhsLogPosition)));
+    }
+
+    private static ClusterMember newMember(final int id, final long leadershipTermId, final long logPosition)
+    {
+        final ClusterMember clusterMember = newMember(id);
+        return clusterMember.leadershipTermId(leadershipTermId).logPosition(logPosition);
+    }
+
+    private static ClusterMember newMember(final int id)
+    {
+        return new ClusterMember(id, null, null, null, null, null, null);
     }
 }

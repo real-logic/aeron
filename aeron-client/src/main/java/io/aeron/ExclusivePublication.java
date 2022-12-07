1707a621f0d241df7ec5a3b2edf799b84db755dc
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2022-12-07 10:36:05 +0100

    [Java] Fix hot fields padding.

--- aeron-client/src/main/java/io/aeron/ExclusivePublication.java
+++ aeron-client/src/main/java/io/aeron/ExclusivePublication.java
@@ -23,8 +23,6 @@ import org.agrona.concurrent.UnsafeBuffer;
 import org.agrona.concurrent.status.ReadablePosition;
 
 import static io.aeron.logbuffer.FrameDescriptor.*;
-import static io.aeron.logbuffer.FrameDescriptor.END_FRAG_FLAG;
-import static io.aeron.logbuffer.FrameDescriptor.frameFlags;
 import static io.aeron.logbuffer.LogBufferDescriptor.TERM_TAIL_COUNTERS_OFFSET;
 import static io.aeron.logbuffer.LogBufferDescriptor.packTail;
 import static io.aeron.protocol.DataHeaderFlyweight.*;
@@ -35,6 +33,11 @@ import static org.agrona.BitUtil.align;
 @SuppressWarnings("unused")
 abstract class ExclusivePublicationLhsPadding extends Publication
 {
+    byte p000, p001, p002, p003, p004, p005, p006, p007, p008, p009, p010, p011, p012, p013, p014, p015;
+    byte p016, p017, p018, p019, p020, p021, p022, p023, p024, p025, p026, p027, p028, p029, p030, p031;
+    byte p032, p033, p034, p035, p036, p037, p038, p039, p040, p041, p042, p043, p044, p045, p046, p047;
+    byte p048, p049, p050, p051, p052, p053, p054, p055, p056, p057, p058, p059, p060, p061, p062, p063;
+
     ExclusivePublicationLhsPadding(
         final ClientConductor clientConductor,
         final String channel,
@@ -57,15 +60,15 @@ abstract class ExclusivePublicationLhsPadding extends Publication
             originalRegistrationId,
             registrationId);
     }
-
-    byte p000, p001, p002, p003, p004, p005, p006, p007, p008, p009, p010, p011, p012, p013, p014, p015;
-    byte p016, p017, p018, p019, p020, p021, p022, p023, p024, p025, p026, p027, p028, p029, p030, p031;
-    byte p032, p033, p034, p035, p036, p037, p038, p039, p040, p041, p042, p043, p044, p045, p046, p047;
-    byte p048, p049, p050, p051, p052, p053, p054, p055, p056, p057, p058, p059, p060, p061, p062, p063;
 }
 
 abstract class ExclusivePublicationValues extends ExclusivePublicationLhsPadding
 {
+    int termOffset;
+    int termId;
+    int activePartitionIndex;
+    long termBeginPosition;
+
     ExclusivePublicationValues(
         final ClientConductor clientConductor,
         final String channel,
@@ -88,11 +91,6 @@ abstract class ExclusivePublicationValues extends ExclusivePublicationLhsPadding
             originalRegistrationId,
             registrationId);
     }
-
-    int termOffset;
-    int termId;
-    int activePartitionIndex;
-    long termBeginPosition;
 }
 
 /**
--- aeron-samples/src/main/java/io/aeron/samples/archive/EmbeddedReplayThroughput.java
+++ aeron-samples/src/main/java/io/aeron/samples/archive/EmbeddedReplayThroughput.java
@@ -55,10 +55,18 @@ abstract class EmbeddedReplayThroughputValue extends EmbeddedReplayThroughputLhs
     long messageCount;
 }
 
+abstract class EmbeddedReplayThroughputRhsPadding extends EmbeddedReplayThroughputValue
+{
+    byte p064, p065, p066, p067, p068, p069, p070, p071, p072, p073, p074, p075, p076, p077, p078, p079;
+    byte p080, p081, p082, p083, p084, p085, p086, p087, p088, p089, p090, p091, p092, p093, p094, p095;
+    byte p096, p097, p098, p099, p100, p101, p102, p103, p104, p105, p106, p107, p108, p109, p110, p111;
+    byte p112, p113, p114, p115, p116, p117, p118, p119, p120, p121, p122, p123, p124, p125, p126, p127;
+}
+
 /**
  * Tests the throughput when replaying a recorded stream of messages.
  */
-public class EmbeddedReplayThroughput extends EmbeddedReplayThroughputValue implements AutoCloseable
+public class EmbeddedReplayThroughput extends EmbeddedReplayThroughputRhsPadding implements AutoCloseable
 {
     private static final int REPLAY_STREAM_ID = 101;
     private static final String REPLAY_URI = "aeron:ipc";
@@ -67,11 +75,6 @@ public class EmbeddedReplayThroughput extends EmbeddedReplayThroughputValue impl
     private static final int FRAGMENT_COUNT_LIMIT = SampleConfiguration.FRAGMENT_COUNT_LIMIT;
     private static final int STREAM_ID = SampleConfiguration.STREAM_ID;
     private static final String CHANNEL = SampleConfiguration.CHANNEL;
-
-    byte p064, p065, p066, p067, p068, p069, p070, p071, p072, p073, p074, p075, p076, p077, p078, p079;
-    byte p080, p081, p082, p083, p084, p085, p086, p087, p088, p089, p090, p091, p092, p093, p094, p095;
-    byte p096, p097, p098, p099, p100, p101, p102, p103, p104, p105, p106, p107, p108, p109, p110, p111;
-    byte p112, p113, p114, p115, p116, p117, p118, p119, p120, p121, p122, p123, p124, p125, p126, p127;
 
     private final ArchivingMediaDriver archivingMediaDriver;
     private final Aeron aeron;

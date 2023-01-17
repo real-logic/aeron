77fc169689545d4bcc2e6cf5299f7767d8cef605
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2023-01-17 20:15:31 +0100

    [C++] Removed unused code from the C++ wrapper project.

--- aeron-client/src/test/cpp_wrapper/CountersTest.cpp
+++ aeron-client/src/test/cpp_wrapper/CountersTest.cpp
@@ -22,7 +22,6 @@
 #include "EmbeddedMediaDriver.h"
 #include "Aeron.h"
 #include "TestUtil.h"
-#include "HeartbeatTimestamp.h"
 
 #define COUNTER_LABEL "counter label"
 #define COUNTER_TYPE_ID (102)

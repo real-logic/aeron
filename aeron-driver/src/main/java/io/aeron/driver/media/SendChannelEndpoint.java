4a37432b4db523f0cba7a88ebc9aaef87a3a3345
mjpt777 <mjpt777@gmail.com>
2023-01-31 10:17:19 +0000

    [Java] Address false sharing on MDC destination structure.

--- aeron-driver/src/main/java/io/aeron/driver/media/SendChannelEndpoint.java
+++ aeron-driver/src/main/java/io/aeron/driver/media/SendChannelEndpoint.java
@@ -785,7 +785,7 @@ class DynamicSndMultiDestination extends MultiSndDestination
         final long nowNs = nanoClock.nanoTime();
         final int position = buffer.position();
         final int length = destinations.length;
-        int toBeRemoved = 0;
+        int inactiveDestinationCount = 0;
 
         int startingIndex = roundRobinIndex++;
         if (startingIndex >= length)
@@ -809,7 +809,7 @@ class DynamicSndMultiDestination extends MultiSndDestination
             }
             else
             {
-                toBeRemoved++;
+                inactiveDestinationCount++;
             }
         }
 
@@ -829,11 +829,11 @@ class DynamicSndMultiDestination extends MultiSndDestination
             }
             else
             {
-                toBeRemoved++;
+                inactiveDestinationCount++;
             }
         }
 
-        if (toBeRemoved > 0)
+        if (inactiveDestinationCount > 0)
         {
             removeInactiveDestinations(nowNs);
         }
@@ -847,10 +847,10 @@ class DynamicSndMultiDestination extends MultiSndDestination
         destinationsCounter.setOrdered(destinations.length);
     }
 
-    private void truncateDestinations(final int removed)
+    private void truncateDestinations(final int removedCount)
     {
         final int length = destinations.length;
-        final int newLength = length - removed;
+        final int newLength = length - removedCount;
 
         if (0 == newLength)
         {
@@ -866,7 +866,7 @@ class DynamicSndMultiDestination extends MultiSndDestination
 
     private void removeInactiveDestinations(final long nowNs)
     {
-        int removed = 0;
+        int removedCount = 0;
 
         for (int lastIndex = destinations.length - 1, i = lastIndex; i >= 0; i--)
         {
@@ -877,21 +877,38 @@ class DynamicSndMultiDestination extends MultiSndDestination
                 {
                     destinations[i] = destinations[lastIndex--];
                 }
-                removed++;
+                removedCount++;
             }
         }
 
-        if (removed > 0)
+        if (removedCount > 0)
         {
-            truncateDestinations(removed);
+            truncateDestinations(removedCount);
         }
     }
 }
 
-final class Destination
+abstract class DestinationLhsPadding
 {
-    long receiverId;
+    byte p000, p001, p002, p003, p004, p005, p006, p007, p008, p009, p010, p011, p012, p013, p014, p015;
+    byte p016, p017, p018, p019, p020, p021, p022, p023, p024, p025, p026, p027, p028, p029, p030, p031;
+    byte p032, p033, p034, p035, p036, p037, p038, p039, p040, p041, p042, p043, p044, p045, p046, p047;
+    byte p048, p049, p050, p051, p052, p053, p054, p055, p056, p057, p058, p059, p060, p061, p062, p063;
+}
+
+abstract class DestinationHotFields extends DestinationLhsPadding
+{
     long timeOfLastActivityNs;
+}
+
+final class Destination extends DestinationHotFields
+{
+    byte p064, p065, p066, p067, p068, p069, p070, p071, p072, p073, p074, p075, p076, p077, p078, p079;
+    byte p080, p081, p082, p083, p084, p085, p086, p087, p088, p089, p090, p091, p092, p093, p094, p095;
+    byte p096, p097, p098, p099, p100, p101, p102, p103, p104, p105, p106, p107, p108, p109, p110, p111;
+    byte p112, p113, p114, p115, p116, p117, p118, p119, p120, p121, p122, p123, p124, p125, p126, p127;
+
+    long receiverId;
     boolean isReceiverIdValid;
     int port;
     InetSocketAddress address;

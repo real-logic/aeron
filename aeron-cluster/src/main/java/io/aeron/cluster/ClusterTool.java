280220daacc6b2122e824debf769ea5e7e473d1f
Michael Barker <mikeb01@gmail.com>
2023-05-01 11:23:58 +1200

    [Java] Remove standby snapshot functions and make some utility methods package-private.

--- aeron-cluster/src/main/java/io/aeron/cluster/ClusterTool.java
+++ aeron-cluster/src/main/java/io/aeron/cluster/ClusterTool.java
@@ -241,10 +241,6 @@ public class ClusterTool
                 exitWithErrorOnFailure(snapshot(clusterDir, System.out));
                 break;
 
-            case "standby-snapshot":
-                exitWithErrorOnFailure(standbySnapshot(clusterDir, System.out));
-                break;
-
             case "suspend":
                 exitWithErrorOnFailure(suspend(clusterDir, System.out));
                 break;
@@ -1066,24 +1062,6 @@ public class ClusterTool
     }
 
     /**
-     * Instruct the cluster to take a snapshot on appropriately configured cluster standby nodes.
-     *
-     * @param clusterDir where the consensus module is running.
-     * @param out        to print the result of the operation.
-     * @return true is the operation was successfully requested.
-     */
-    public static boolean standbySnapshot(final File clusterDir, final PrintStream out)
-    {
-        return toggleClusterState(
-            out,
-            clusterDir,
-            ConsensusModule.State.ACTIVE,
-            ClusterControl.ToggleState.STANDBY_SNAPSHOT,
-            true,
-            TimeUnit.SECONDS.toMillis(30));
-    }
-
-    /**
      * Instruct the cluster to suspend operation.
      *
      * @param clusterDir where the consensus module is running.
@@ -1186,7 +1164,7 @@ public class ClusterTool
     }
 
     @SuppressWarnings("MethodLength")
-    private static boolean toggleClusterState(
+    static boolean toggleClusterState(
         final PrintStream out,
         final File clusterDir,
         final ConsensusModule.State expectedState,
@@ -1435,7 +1413,7 @@ public class ClusterTool
         }
     }
 
-    private static void exitWithErrorOnFailure(final boolean success)
+    static void exitWithErrorOnFailure(final boolean success)
     {
         if (!success)
         {
@@ -1443,7 +1421,7 @@ public class ClusterTool
         }
     }
 
-    private static void printHelp()
+    static void printHelp()
     {
         System.out.format(
             "Usage: <cluster-dir> <command> [options]%n" +
@@ -1460,7 +1438,6 @@ public class ClusterTool
             "                     backup-query: [delay] get, or set, time of next backup query.%n" +
             "       invalidate-latest-snapshot: marks the latest snapshot as a invalid so the previous is loaded.%n" +
             "                         snapshot: triggers a snapshot on the leader.%n" +
-            "                 standby-snapshot: triggers a snapshot on cluster standby nodes.%n" +
             "                          suspend: suspends appending to the log.%n" +
             "                           resume: resumes appending to the log.%n" +
             "                         shutdown: initiates an orderly stop of the cluster with a snapshot.%n" +

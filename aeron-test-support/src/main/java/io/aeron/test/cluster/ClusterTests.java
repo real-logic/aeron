973d4f75a2daaea6ac82fe11651cde5a42829b78
Michael Barker <mikeb01@gmail.com>
2023-07-06 14:20:52 +1200

    [Java] Remove exceptions from with the termination hook that cause intermittency in the tests.

--- aeron-test-support/src/main/java/io/aeron/test/cluster/ClusterTests.java
+++ aeron-test-support/src/main/java/io/aeron/test/cluster/ClusterTests.java
@@ -21,7 +21,6 @@ import io.aeron.exceptions.AeronException;
 import org.agrona.ErrorHandler;
 import org.agrona.ExpandableArrayBuffer;
 import org.agrona.collections.MutableInteger;
-import org.agrona.concurrent.AgentTerminationException;
 import org.agrona.concurrent.IdleStrategy;
 import org.agrona.concurrent.YieldingIdleStrategy;
 
@@ -69,11 +68,7 @@ public class ClusterTests
                     {
                         hasTerminated.set(true);
                     }
-
-                    throw new ClusterTerminationException();
                 }
-
-                throw new AgentTerminationException();
             };
     }
 

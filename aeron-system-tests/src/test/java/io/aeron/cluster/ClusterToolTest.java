ecf2894be748e164cdc8443a2b21d8ad00d4f2aa
Michael Barker <mikeb01@gmail.com>
2023-07-26 03:35:56 -1100

    [Java] More Windows test fixes.

--- aeron-system-tests/src/test/java/io/aeron/cluster/ClusterToolTest.java
+++ aeron-system-tests/src/test/java/io/aeron/cluster/ClusterToolTest.java
@@ -197,7 +197,7 @@ class ClusterToolTest
         final String errorContent = stream.flushAndGetContent();
         assertThat(errorContent, containsString("This message will cause an error"));
         assertThat(errorContent, containsString("Mark file exists"));
-        final String path = markFileDir.getAbsolutePath().replace("\\", "\\\\");
+        final String path = markFileDir.getName();
         final Pattern serviceMarkFileName = Pattern.compile(
             ".*Mark file exists:.*" + path + ".*cluster-mark-service-0.dat.*", Pattern.DOTALL);
         assertThat("Tool output: " + errorContent, errorContent, matchesRegex(serviceMarkFileName));

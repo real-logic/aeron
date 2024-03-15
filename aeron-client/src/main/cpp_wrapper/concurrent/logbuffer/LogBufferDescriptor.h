38fcdab6bf8cac3da72260a74b8eb409543049de
Michael Barker <mikeb01@gmail.com>
2024-03-15 16:21:10 +1300

    [C/C++] Fix warning.

--- aeron-client/src/main/cpp_wrapper/concurrent/logbuffer/LogBufferDescriptor.h
+++ aeron-client/src/main/cpp_wrapper/concurrent/logbuffer/LogBufferDescriptor.h
@@ -27,15 +27,13 @@
 extern "C"
 {
 #include "concurrent/aeron_logbuffer_descriptor.h"
-};
+}
 
 namespace aeron { namespace concurrent { namespace logbuffer { namespace LogBufferDescriptor
 {
 
 const std::int32_t TERM_MIN_LENGTH = 64 * 1024;
 const std::int32_t TERM_MAX_LENGTH = 1024 * 1024 * 1024;
-//const std::int32_t AERON_PAGE_MIN_SIZE = 4 * 1024;
-//const std::int32_t AERON_PAGE_MAX_SIZE = 1024 * 1024 * 1024;
 
 #if defined(__GNUC__) || _MSC_VER >= 1900
 static constexpr const int PARTITION_COUNT = 3;

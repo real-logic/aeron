bc8d2ca733b33bd5b6613e3ddcda4eeddf9c90bc
Dmytro Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2024-04-02 16:25:58 +0200

    [Java] Pass AERON_DRIVER_ASYNC_EXECUTOR_THREADS to the C media driver.

--- aeron-test-support/src/main/java/io/aeron/test/driver/CTestMediaDriver.java
+++ aeron-test-support/src/main/java/io/aeron/test/driver/CTestMediaDriver.java
@@ -278,6 +278,7 @@ public final class CTestMediaDriver implements TestMediaDriver
         environment.put("AERON_DRIVER_SENDER_CYCLE_THRESHOLD", String.valueOf(context.senderCycleThresholdNs()));
         environment.put("AERON_DRIVER_RECEIVER_CYCLE_THRESHOLD", String.valueOf(context.receiverCycleThresholdNs()));
         environment.put("AERON_DRIVER_NAME_RESOLVER_THRESHOLD", String.valueOf(context.nameResolverThresholdNs()));
+        environment.put("AERON_DRIVER_ASYNC_EXECUTOR_THREADS", String.valueOf(context.asyncTaskExecutorThreads()));
         final String senderWildcardPortRange = context.senderWildcardPortRange();
         if (null != senderWildcardPortRange)
         {

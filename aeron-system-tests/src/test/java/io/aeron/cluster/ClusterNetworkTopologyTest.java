87dff9323396af26cd518e3d995981417fe2411a
Dmytro Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2024-12-18 17:25:54 +0100

    [Java] Add missing `--add-opens`.

--- aeron-system-tests/src/test/java/io/aeron/cluster/ClusterNetworkTopologyTest.java
+++ aeron-system-tests/src/test/java/io/aeron/cluster/ClusterNetworkTopologyTest.java
@@ -40,7 +40,6 @@ import org.agrona.concurrent.UnsafeBuffer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledOnOs;
-import org.junit.jupiter.api.condition.JRE;
 import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.RegisterExtension;
@@ -63,7 +62,6 @@ import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.EnumSet;
 import java.util.List;
 import java.util.Set;
 import java.util.regex.Pattern;
@@ -461,12 +459,10 @@ class ClusterNetworkTopologyTest
         IoUtil.delete(clusterDir, false);
         IoUtil.ensureDirectoryExists(clusterDir, "cluster base directory");
 
-        if (isVersionAfterJdk8())
-        {
-            command.add("--add-opens");
-            command.add("java.base/sun.nio.ch=ALL-UNNAMED");
-        }
-
+        command.add("--add-opens");
+        command.add("java.base/jdk.internal.misc=ALL-UNNAMED");
+        command.add("--add-opens");
+        command.add("java.base/java.util.zip=ALL-UNNAMED");
         command.add("-Xmx32m");
         command.add("-cp");
         command.add(FileResolveUtil.resolveAeronAllJar().getAbsolutePath());
@@ -500,10 +496,5 @@ class ClusterNetworkTopologyTest
         command.add(EchoServiceNode.class.getName());
 
         return command.toArray(new String[0]);
-    }
-
-    private static boolean isVersionAfterJdk8()
-    {
-        return EnumSet.range(JRE.JAVA_9, JRE.OTHER).contains(JRE.currentVersion());
     }
 }

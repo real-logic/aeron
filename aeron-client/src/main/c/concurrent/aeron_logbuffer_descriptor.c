9eb689f6ea6a4e935bda5ce7732413adfcc808c9
Dmytro Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2024-12-20 16:49:40 +0100

    [C] Move `_Static_assert` out of the header file.

--- aeron-client/src/main/c/concurrent/aeron_logbuffer_descriptor.c
+++ aeron-client/src/main/c/concurrent/aeron_logbuffer_descriptor.c
@@ -19,6 +19,145 @@
 #include "util/aeron_error.h"
 #include "concurrent/aeron_logbuffer_descriptor.h"
 
+#ifdef _MSC_VER
+#define _Static_assert static_assert
+#endif
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, term_tail_counters) == 0,
+    "offsetof(aeron_logbuffer_metadata_t, term_tail_counters) is wrong");
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, active_term_count) == 24,
+    "offsetof(aeron_logbuffer_metadata_t, active_term_count) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, end_of_stream_position) == 128,
+    "offsetof(aeron_logbuffer_metadata_t, end_of_stream_position) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, is_connected) == 136,
+    "offsetof(aeron_logbuffer_metadata_t, is_connected) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, active_transport_count) == 140,
+    "offsetof(aeron_logbuffer_metadata_t, active_transport_count) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, correlation_id) == 256,
+    "offsetof(aeron_logbuffer_metadata_t, correlation_id) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, initial_term_id) == 264,
+    "offsetof(aeron_logbuffer_metadata_t, initial_term_id) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, default_frame_header_length) == 268,
+    "offsetof(aeron_logbuffer_metadata_t, default_frame_header_length) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, mtu_length) == 272,
+    "offsetof(aeron_logbuffer_metadata_t, mtu_length) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, term_length) == 276,
+    "offsetof(aeron_logbuffer_metadata_t, term_length) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, page_size) == 280,
+    "offsetof(aeron_logbuffer_metadata_t, page_size) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, publication_window_length) == 284,
+    "offsetof(aeron_logbuffer_metadata_t, publication_window_length) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, receiver_window_length) == 288,
+    "offsetof(aeron_logbuffer_metadata_t, receiver_window_length) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, socket_sndbuf_length) == 292,
+    "offsetof(aeron_logbuffer_metadata_t, socket_sndbuf_length) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, socket_rcvbuf_length) == 296,
+    "offsetof(aeron_logbuffer_metadata_t, socket_rcvbuf_length) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, max_resend) == 300,
+    "offsetof(aeron_logbuffer_metadata_t, max_resend) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, entity_tag) == 304,
+    "offsetof(aeron_logbuffer_metadata_t, entity_tag) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, entity_tag) % sizeof(int64_t) == 0,
+    "offsetof(aeron_logbuffer_metadata_t, entity_tag) not aligned");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, response_correlation_id) == 312,
+    "offsetof(aeron_logbuffer_metadata_t, response_correlation_id) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, default_header) == 320,
+    "offsetof(aeron_logbuffer_metadata_t, default_header) is wrong");
+
+_Static_assert(
+    AERON_LOGBUFFER_DEFAULT_FRAME_HEADER_MAX_LENGTH >= AERON_DATA_HEADER_LENGTH,
+    "AERON_LOGBUFFER_DEFAULT_FRAME_HEADER_MAX_LENGTH < AERON_DATA_HEADER_LENGTH");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, linger_timeout_ns) == 448,
+    "offsetof(aeron_logbuffer_metadata_t, linger_timeout_ns) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, untethered_window_limit_timeout_ns) == 456,
+    "offsetof(aeron_logbuffer_metadata_t, untethered_window_limit_timeout_ns) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, untethered_resting_timeout_ns) == 464,
+    "offsetof(aeron_logbuffer_metadata_t, untethered_resting_timeout_ns) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, group) == 472,
+    "offsetof(aeron_logbuffer_metadata_t, group) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, is_response) == 473,
+    "offsetof(aeron_logbuffer_metadata_t, is_response) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, rejoin) == 474,
+    "offsetof(aeron_logbuffer_metadata_t, rejoin) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, reliable) == 475,
+    "offsetof(aeron_logbuffer_metadata_t, reliable) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, sparse) == 476,
+    "offsetof(aeron_logbuffer_metadata_t, sparse) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, signal_eos) == 477,
+    "offsetof(aeron_logbuffer_metadata_t, signal_eos) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, spies_simulate_connection) == 478,
+    "offsetof(aeron_logbuffer_metadata_t, spies_simulate_connection) is wrong");
+
+_Static_assert(
+    offsetof(aeron_logbuffer_metadata_t, tether) == 479,
+    "offsetof(aeron_logbuffer_metadata_t, tether) is wrong");
+
+_Static_assert(
+    sizeof(aeron_logbuffer_metadata_t) == 480,
+    "sizeof(aeron_logbuffer_metadata_t) is wrong");
+
+_Static_assert(
+    AERON_LOGBUFFER_META_DATA_LENGTH == AERON_PAGE_MIN_SIZE,
+    "AERON_LOGBUFFER_META_DATA_LENGTH != AERON_PAGE_MIN_SIZE");
+
 int aeron_logbuffer_check_term_length(uint64_t term_length)
 {
     if (term_length < AERON_LOGBUFFER_TERM_MIN_LENGTH)

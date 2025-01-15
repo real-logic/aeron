96cb7643b8200df0542ed4dfe70dcce3ee549fa3
guoan.chen <guoan.chen@okg.com>
2025-01-15 10:26:21 +0800

    tidy up the namespace for exception_handler_t

--- aeron-client/src/main/cpp_wrapper/concurrent/AgentRunner.h
+++ aeron-client/src/main/cpp_wrapper/concurrent/AgentRunner.h
@@ -37,7 +37,7 @@ template <typename Agent, typename IdleStrategy>
 class AgentRunner
 {
 public:
-    AgentRunner(Agent &agent, IdleStrategy &idleStrategy, logbuffer::exception_handler_t &exceptionHandler) :
+    AgentRunner(Agent &agent, IdleStrategy &idleStrategy, util::exception_handler_t &exceptionHandler) :
         m_agent(agent),
         m_idleStrategy(idleStrategy),
         m_exceptionHandler(exceptionHandler),
@@ -48,7 +48,7 @@ public:
     AgentRunner(
         Agent &agent,
         IdleStrategy &idleStrategy,
-        logbuffer::exception_handler_t &exceptionHandler,
+        util::exception_handler_t &exceptionHandler,
         const std::string &name) :
         m_agent(agent),
         m_idleStrategy(idleStrategy),
@@ -197,7 +197,7 @@ public:
 private:
     Agent &m_agent;
     IdleStrategy &m_idleStrategy;
-    logbuffer::exception_handler_t &m_exceptionHandler;
+    util::exception_handler_t &m_exceptionHandler;
     std::atomic<bool> m_isStarted = { false };
     std::atomic<bool> m_isRunning = { false };
     std::atomic<bool> m_isClosed = { false };

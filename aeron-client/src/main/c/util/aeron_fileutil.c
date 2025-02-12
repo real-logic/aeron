120ae8f6337d00b417253628f6d83731d44c09a5
Dmytro Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2025-02-12 20:54:43 +0100

    [C] Close HANDLE before returning file descriptor.

--- aeron-client/src/main/c/util/aeron_fileutil.c
+++ aeron-client/src/main/c/util/aeron_fileutil.c
@@ -260,6 +260,7 @@ int aeron_create_file(const char *path, size_t length, bool sparse_file)
         goto error;
     }
 
+    CloseHandle(hfile);
     return fd;
 
 error:
@@ -295,6 +296,7 @@ int aeron_open_file_rw(const char *path)
         return -1;
     }
 
+    CloseHandle(hfile);
     return fd;
 }
 

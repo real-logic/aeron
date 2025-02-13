291200bf66f29ad2a9e9715a2b7c475a14c041d5
Dmytro Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2025-02-13 09:43:15 +0100

    [DEBUG] Java file mapping delete test.

--- aeron-driver/src/test/java/io/aeron/driver/buffer/FileStoreLogFactoryTest.java
+++ aeron-driver/src/test/java/io/aeron/driver/buffer/FileStoreLogFactoryTest.java
@@ -15,6 +15,7 @@
  */
 package io.aeron.driver.buffer;
 
+import io.aeron.LogBuffers;
 import io.aeron.driver.Configuration;
 import io.aeron.exceptions.AeronException;
 import io.aeron.exceptions.StorageSpaceException;
@@ -37,6 +38,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileStore;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static io.aeron.logbuffer.LogBufferDescriptor.PARTITION_COUNT;
@@ -214,6 +217,25 @@ class FileStoreLogFactoryTest
                 " usable=" + usableSpace + " in " + fileStore, exception.getMessage());
         }
         verifyNoMoreInteractions(errorHandler);
+    }
+
+    @Test
+    void debugFileRetention()
+    {
+        rawLog = fileStoreLogFactory.newPublication(CREATION_ID, TERM_BUFFER_LENGTH, PRE_ZERO_LOG);
+        assertEquals(TERM_BUFFER_LENGTH, rawLog.termLength());
+        LogBufferDescriptor.termLength(rawLog.metaData(), TERM_BUFFER_LENGTH);
+        LogBufferDescriptor.pageSize(rawLog.metaData(), PAGE_SIZE);
+
+        final Path file = Paths.get(rawLog.fileName());
+        assertTrue(Files.exists(file));
+
+        final LogBuffers logBuffers = new LogBuffers(rawLog.fileName());
+
+        assertTrue(rawLog.free());
+        assertFalse(Files.exists(file));
+
+        logBuffers.close();
     }
 
     private static void assertThrowsStorageSpaceException(
